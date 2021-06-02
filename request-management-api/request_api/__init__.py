648c65ca6d9b42301698e384ad8e70656d235349
divyav-aot <divya.v@aot-technologies.com>
2021-06-02 11:39:14 -0400

    updated logging

--- request-management-api/request_api/__init__.py
+++ request-management-api/request_api/__init__.py
@@ -87,6 +87,21 @@ def create_app(run_mode=os.getenv('FLASK_ENV', 'development')):
     #register_shellcontext(app)
     
 
+    log_level = logging.INFO
+ 
+    for handler in app.logger.handlers:
+        app.logger.removeHandler(handler)
+
+    root = os.path.dirname(os.path.abspath(__file__))
+    logdir = os.path.join(root, 'logs')
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+    log_file = os.path.join(logdir, 'app.log')
+    handler = logging.FileHandler(log_file)
+    handler.setLevel(log_level)
+    app.logger.addHandler(handler) 
+    app.logger.setLevel(log_level)
+
     return app
 
 
