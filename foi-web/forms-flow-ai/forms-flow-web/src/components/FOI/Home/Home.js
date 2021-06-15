7a64b7ef1d0b4b544872c63c603cc314e1999359
divyav-aot <divya.v@aot-technologies.com>
2021-06-15 16:09:37 -0400

    style changes for home page

--- foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Home/Home.js
+++ foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Home/Home.js
@@ -3,15 +3,20 @@ import "./home.scss";
 const Home = memo((props) => { 
     return (      
         <div className="home-page">
-          <div className="rounded-rectangle">
+          {/* <div className="rounded-rectangle">
             <div className="login-container">
               <h1 className="display-5">Welcome, Sign In</h1>
               <button className="btn btn-primary active">
                 Log In
               </button>
             </div>
-          </div>
-                       
+          </div> */}
+          <div className="card rounded-rectangle">     
+            <div className="card-body login-container">
+              <h1 className="card-title">Welcome, Sign In</h1>
+              <button type="button" className="btn btn-primary login-btn">Log In</button>
+            </div>
+          </div>     
         </div>
     );
   });
