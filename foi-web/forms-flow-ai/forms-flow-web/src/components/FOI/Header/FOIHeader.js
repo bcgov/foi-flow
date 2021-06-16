612b6607fec3b1850331758c0ce61aee22993eec
divyav-aot <divya.v@aot-technologies.com>
2021-06-16 16:36:35 -0400

    Homepage style changes with Nav Bar

--- foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Header/FOIHeader.js
+++ foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Header/FOIHeader.js
@@ -6,53 +6,40 @@ import "./foiheader.scss";
 import { Container } from "@material-ui/core";
 const FOIHeader = React.memo(() => {
   //const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
-  const isAuthenticated = true;
-  //const today = new Date();
-   {/* <header>
-        <div className="offset-md-2 banner">
-        <a href="https://gov.bc.ca" alt="British Columbia">
-          <img src="FOI/assets/Images/logo-banner.png" alt="Go to the Government of British Columbia website" />
-        </a>
-        <h2>FOI</h2>        
-    </div>
-    </header> */}
+  const isAuthenticated = true;  
   return (
     <Navbar collapseOnSelect fixed="top" expand="sm" bg="#036" variant="dark">
       <Container>
-      <Nav className="ml-auto">
-    {/* <nav className="navbar navbar-expand-lg navbar-light"> */}
-        {/* <div className="offset-md-2 banner-left"> */}
+      <Nav className="ml-auto">   
           <a href="https://gov.bc.ca" alt="British Columbia">
             <img src="FOI/assets/Images/logo-banner.png" alt="Go to the Government of British Columbia website" />
           </a>
           <h2>FOI</h2>
-        {/* </div> */}
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         {isAuthenticated?
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <div className="ml-auto banner-right">       
-              <ul className="navbar-nav ">
-                  <li className="nav-item username">
-                      <span className="navbar-text"> Username </span>
-                  </li>
-                  <li className="nav-item bell-icon">
-                  <Badge color="secondary" badgeContent=" " variant="dot">
-                      <i className="fa fa-bell-o"></i>
-                  </Badge>
-                    
-                  </li>
-                  <li className="nav-item">
-                    <button type="button" className="btn btn-primary signout-btn">Sign Out</button>
-                  </li>
-              </ul>
-            </div>
-        </Nav>
-        </Navbar.Collapse>
-       
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         :null}
-           
-    {/* </nav> */}
+        {isAuthenticated?
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <div className="ml-auto banner-right">       
+                <ul className="navbar-nav ">
+                    <li className="nav-item username">
+                        <span className="navbar-text"> Username </span>
+                    </li>
+                    <li className="nav-item bell-icon">
+                    <Badge color="secondary" badgeContent=" " variant="dot">
+                        <i className="fa fa-bell-o"></i>
+                    </Badge>
+                      
+                    </li>
+                    <li className="nav-item">
+                      <button type="button" className="btn btn-primary signout-btn">Sign Out</button>
+                    </li>
+                </ul>
+              </div>
+          </Nav>
+          </Navbar.Collapse>       
+        :null}
     </Nav>
     </Container>
          </Navbar>   
