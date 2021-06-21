01106ce5da8d6f305c14311c5aea14e0412a1dcd
Abin Antony <abin.antony@aot-technologies.com>
2021-06-21 16:46:08 -0700

    #121

--- foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Dashboard/Dashboard.js
+++ foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Dashboard/Dashboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {useSelector} from "react-redux";
-import "./dashboard.scss";
+// import DashboardImage from "../../../assets/FOI/images/DashboardImage.PNG"
+
 const Dashboard = React.memo(() => {
   
   const user = useSelector((state) => state.user.userDetail);
 
      return (      
-        <div className="foidashboard">
+        <div className="dashboard">
           <span>Welcome {user.name || user.preferred_username || ""} !!!</span>
         </div>
     );
--- foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Home/Home.js
+++ foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Home/Home.js
@@ -1,9 +1,30 @@
-import React, { memo } from 'react';
-import {useDispatch} from "react-redux";
+import React,  {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 import {push} from "connected-react-router";
+import UserService from "../../../services/UserService";
+import { setUserAuth } from "../../../actions/bpmActions";
 import "./home.scss";
-const Home = memo(() => {
+const Home = React.memo((props) => {
+  let isAuth = false;
+
+  const authToken = localStorage.getItem("authToken"); 
+  
+  if(authToken !== null && authToken !== '' && authToken !== undefined) {
+    isAuth = true;
+  }
+
   const dispatch = useDispatch();
+
+  useEffect(()=>{
+    if(props.store && isAuth){
+      UserService.initKeycloak(props.store, (err, res) => {
+        dispatch(setUserAuth(res.authenticated));
+      });
+    }
+  },[props.store, dispatch]);
+
+
+  const user = useSelector((state) => state.user.userDetail);
   const login = () => {
     dispatch(push(`/Dashboard`));
 }
@@ -11,8 +32,8 @@ const Home = memo(() => {
         <div className="home-page">         
           <div className="card rounded-rectangle foiroundedrectangle">     
             <div className="card-body login-container">
-              <h1 className="card-title">Welcome, Sign In</h1>
-              <button type="button" className="btn btn-primary login-btn foiLogin" onClick={login}>Log In</button>
+            {isAuth?<h1 className="card-title">Welcome {user.name || user.preferred_username || ""}</h1> : <div> <h1 className="card-title">Welcome, Sign In</h1> <button type="button" className="btn btn-primary login-btn foiLogin" onClick={login}>Log In</button> </div>}
+              
             </div>
           </div>     
         </div>
