2a5840922ec8e93adccb79e0054f53735f53adab
divyav-aot <divya.v@aot-technologies.com>
2021-06-21 15:51:21 -0400

    design chenages

--- foi-web/forms-flow-ai/forms-flow-web/src/components/App.jsx
+++ foi-web/forms-flow-ai/forms-flow-web/src/components/App.jsx
@@ -7,12 +7,14 @@ import "../assets/styles/user-styles.css";
 import BaseRouting from "./BaseRouting";
 import {Helmet} from "react-helmet";
 import {KEYCLOAK_URL} from "../constants/constants";
+import {APPLICATION_NAME} from "../constants/constants";
 
 import FOIBaseRouting from "./FOI/FOIBaseRouting"
 
 require("typeface-nunito-sans");
 
 const App = React.memo((props) => {
+  const appName = APPLICATION_NAME;
     const { store, history } = props;
     return (
       <div>
@@ -21,8 +23,12 @@ const App = React.memo((props) => {
         </Helmet>
         <Provider store={store}>
           <ConnectedRouter history={history}>
-            {/* <BaseRouting store={store}/> */}
+            {appName === 'foi-flow'?
             <FOIBaseRouting  store={store}/>
+            :
+            <BaseRouting store={store}/>
+            }
+            
           </ConnectedRouter>
         </Provider>
       </div>
--- foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Dashboard/Dashboard.js
+++ foi-web/forms-flow-ai/forms-flow-web/src/components/FOI/Dashboard/Dashboard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {useSelector} from "react-redux";
-
+import "./dashboard.scss";
 const Dashboard = React.memo(() => {
   
   const user = useSelector((state) => state.user.userDetail);
 
      return (      
-        <div className="dashboard">
+        <div className="foidashboard">
           <span>Welcome {user.name || user.preferred_username || ""} !!!</span>
         </div>
     );
