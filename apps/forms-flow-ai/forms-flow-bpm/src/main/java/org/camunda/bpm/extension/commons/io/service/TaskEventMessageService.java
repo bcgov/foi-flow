24b15febcd8a9a287f66744113e8c96682b033ba
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2021-07-04 22:34:07 -0700

    forms-flow-bpm integration and setup of message listener for creating process instance.

---
+++ apps/forms-flow-ai/forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/io/service/TaskEventMessageService.java
@@ -0,0 +1,41 @@
+package org.camunda.bpm.extension.commons.io.service;
+
+import org.camunda.bpm.extension.commons.io.message.TaskMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import spinjar.com.fasterxml.jackson.core.JsonProcessingException;
+import spinjar.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This component publishes the message to websocket
+ *
+ * @author sumathi.thirumani@aot-technologies.com
+ */
+@Component
+public class TaskEventMessageService {
+
+    private final Logger LOGGER = Logger.getLogger(TaskEventMessageService.class.getName());
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    public void sendMessage(TaskMessage message) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            template.convertAndSend("/topic/task-event", objectMapper.writeValueAsString(message));
+            Map<String,Object> obj = new HashMap<>();
+            obj.put("id","123");
+            template.convertAndSend("/topic/foi-rawrequest", objectMapper.writeValueAsString(obj));
+        } catch (JsonProcessingException e) {
+            LOGGER.log(Level.SEVERE,"Exception Occured in preparing message", e);
+        }
+    }
+
+
+}
