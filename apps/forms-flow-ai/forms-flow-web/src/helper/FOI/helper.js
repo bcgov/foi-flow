b1f00c42df9544da0798964da0ce7435e61ed306
Adam Coard <arcoard+github@gmail.com>
2021-07-21 13:46:14 -0700

    Revert "Revert "Merge pull request #217 from bcgov/dev-AA-25""
    This reverts commit c95e0faefff62a4af18ccc7f498797527d993b4c.

--- apps/forms-flow-ai/forms-flow-web/src/helper/FOI/helper.js
+++ apps/forms-flow-ai/forms-flow-web/src/helper/FOI/helper.js
@@ -1,13 +1,13 @@
 const replaceUrl = (URL, key, value) => {
-    return URL.replace(key, value);
-  };
-  const formatDate = (d) => {
-    if(d !== "") {
-      let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-      let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
-      let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
-      return `${ye}-${mo}-${da}`;
-    }
+  return URL.replace(key, value);
+};
+const formatDate = (d) => {
+  if(d !== "") {
+    var _d= new Date(d)
+    let ye = new Intl.DateTimeFormat('en-US', { year: 'numeric' }).format(_d);
+    let mo = new Intl.DateTimeFormat('en-US', { month: '2-digit' }).format(_d);
+    let da = new Intl.DateTimeFormat('en-US', { day: '2-digit' }).format(_d);
+    return `${ye}-${mo}-${da}`;
   }
-  export { replaceUrl, formatDate };
-  
+}
+export { replaceUrl, formatDate };
