f86705ff3620aef35e941f3fd86b5c164899b162
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2021-08-09 11:12:03 -0700

    Changes to create request.

--- request-management-api/request_api/models/FOIRawRequests.py
+++ request-management-api/request_api/models/FOIRawRequests.py
@@ -64,21 +64,16 @@ class FOIRawRequest(db.Model):
             return DefaultMethodResult(False,'Requestid not exists',-1)              
 
     @classmethod
-    def updateworkflowinstancewithstatus(cls,wfinstanceid,requestid,status)->DefaultMethodResult:
+    def updateworkflowinstancewithstatus(cls,wfinstanceid,requestid,status,notes)-> DefaultMethodResult:
         updatedat = datetime.now().isoformat()
         dbquery = db.session.query(FOIRawRequest)
         _requestraqw = dbquery.filter_by(requestid=requestid).order_by(FOIRawRequest.version.desc()).first()
         requestraqw = dbquery.filter_by(requestid=requestid,version = _requestraqw.version)
         if(requestraqw.count() > 0) :
-            existingrequestswithWFid = dbquery.filter_by(wfinstanceid=wfinstanceid)               
-            if(existingrequestswithWFid.count() == 1) :
-                requestraqw.update({FOIRawRequest.wfinstanceid:wfinstanceid, FOIRawRequest.updated_at:updatedat,FOIRawRequest.notes:"Status update",FOIRawRequest.status:status}, synchronize_session = False)
-                db.session.commit()
-                return DefaultMethodResult(True,'Request updated with status',requestid)
-            else:
-                requestraqw.update({FOIRawRequest.wfinstanceid:wfinstanceid, FOIRawRequest.updated_at:updatedat,FOIRawRequest.notes:"Workflow ID update",FOIRawRequest.status:status}, synchronize_session = False)
-                db.session.commit()
-                return DefaultMethodResult(True,'Request updated with WF Instance Id',requestid)
+            existingrequestswithWFid = dbquery.filter_by(wfinstanceid=wfinstanceid)        
+            requestraqw.update({FOIRawRequest.wfinstanceid:wfinstanceid, FOIRawRequest.updated_at:updatedat,FOIRawRequest.notes:notes,FOIRawRequest.status:status}, synchronize_session = False)
+            db.session.commit()
+            return DefaultMethodResult(True,'Request updated',requestid)       
         else:
             return DefaultMethodResult(False,'Requestid not exists',-1)    
 
--- request-management-api/request_api/services/rawrequestservice.py
+++ request-management-api/request_api/services/rawrequestservice.py
@@ -19,7 +19,7 @@ class rawrequestservice:
     """
 
     def saverawrequest(requestdatajson,sourceofsubmission):
-        assignee = requestdatajson["assignedTo"] if requestdatajson.get("assignedTo") != None  else ''
+        assignee = requestdatajson["assignedTxo"] if requestdatajson.get("assignedTo") != None else None
         result = FOIRawRequest.saverawrequest(requestdatajson,sourceofsubmission,assignee)
         if result.success:
             redispubservice = RedisPublisherService()
@@ -39,8 +39,8 @@ class rawrequestservice:
         result = FOIRawRequest.updateworkflowinstance(wfinstanceid, requestid)
         return result
 
-    def updateworkflowinstancewithstatus(wfinstanceid, requestid,status):
-        result = FOIRawRequest.updateworkflowinstancewithstatus(wfinstanceid, requestid,status)
+    def updateworkflowinstancewithstatus(wfinstanceid, requestid,status,notes):
+        result = FOIRawRequest.updateworkflowinstancewithstatus(wfinstanceid,requestid,status,notes)
         return result    
 
     def getrawrequests():
