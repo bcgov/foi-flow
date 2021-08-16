67aab7e97235a4cf632bc8b176ef48cf228fd5ae
Abin Antony <abin.antony@aot-technologies.com>
2021-08-16 11:12:17 -0700

    #33 #294

--- request-management-api/tests/services/test_foirequest_services.py
+++ request-management-api/tests/services/test_foirequest_services.py
@@ -3,9 +3,9 @@ from request_api.services.requestservice import requestservice
 import json
 
 
-with open('tests/samplerequestjson/rawrequest-general.json') as f:
+with open('tests/samplerequestjson/foirequest-general.json') as f:
   generalrequestjson = json.load(f)
-with open('tests/samplerequestjson/rawrequest-personal.json') as f:
+with open('tests/samplerequestjson/foirequest-personal.json') as f:
   personalrequestjson = json.load(f)
 def pytest_namespace():
     return {'requestidtoupdate': 0}
@@ -13,9 +13,19 @@ def pytest_namespace():
 def test_save_rawrequest(session):
     response = requestservice.saverequest(generalrequestjson)
     assert response.success == True
+    requestid = response.identifier
+    for item in response.args[0]:     
+      assert item['id']
+      requestmessage = requestservice.getrequest(requestid,item['id'])
+      assert requestmessage["currentState"]
 
 def test_save_rawrequestversion(session):
     response = requestservice.saverequest(personalrequestjson)  
-    assert response.success == True  
+    assert response.success == True
+    requestid = response.identifier
+    for item in response.args[0]:     
+      assert item['id']
+      requestmessage = requestservice.getrequest(requestid,item['id'])
+      assert requestmessage["currentState"]  
 
 
