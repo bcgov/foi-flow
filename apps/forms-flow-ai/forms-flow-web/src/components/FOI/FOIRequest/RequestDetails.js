c7c072e3aa15ef7a3308ce8dd89096d6fdec67c7
divyav-aot <divya.v@aot-technologies.com>
2021-08-19 19:57:01 -0400

    Date conversion test

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/RequestDetails.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/RequestDetails.js
@@ -46,12 +46,12 @@ const RequestDetails = React.memo(({requestDetails, handleRequestDetailsValue, h
     const calculateReceivedDate = (receivedDateString) => {      
       const dateString = receivedDateString ? receivedDateString.substring(0,10): "";
       receivedDateString = receivedDateString ? new Date(receivedDateString): "";
-      console.log(`dateString = ${dateString}, receivedDateString = ${receivedDateString}`);
+      console.log(`dateString = ${dateString}, receivedDateString = ${receivedDateString}, businessDay = ${businessDay(dateString)}`);
       if (receivedDateString !== "" && ((receivedDateString.getHours() > 16 || (receivedDateString.getHours() === 16 && receivedDateString.getMinutes() > 30)) || !businessDay(dateString))) {
-        console.log(`formatDate = ${formatDate(receivedDateString)}, businessDay = ${businessDay(dateString)}`);
-        if (dateString !== formatDate(receivedDateString) || (dateString === formatDate(receivedDateString) && !businessDay(dateString))) {        
-          receivedDateString = addBusinessDays(dateString, 1);        
-        }
+        // console.log(`formatDate = ${formatDate(receivedDateString)}, businessDay = ${businessDay(dateString)}`);
+        // if (dateString !== formatDate(receivedDateString) || (dateString === formatDate(receivedDateString) && !businessDay(dateString))) {        
+          receivedDateString = addBusinessDays(receivedDateString, 1);
+        // }
       }
         console.log(`FinalReceivedDateString = ${receivedDateString}`);
         return receivedDateString;
