26f57f6285dc01d80cd2a718aae81c2a11d119f3
Abin Antony <abin.antony@aot-technologies.com>
2021-08-23 10:00:56 -0700

    migration file

---
+++ request-management-api/migrations/versions/f61413c0517d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f61413c0517d
+Revises: 490bdb384e28
+Create Date: 2021-08-23 10:00:27.399097
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f61413c0517d'
+down_revision = '490bdb384e28'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
