87a37edd85d44bb2016a4f522229f2c24b562340
Abin Antony <abin.antony@aot-technologies.com>
2021-09-07 14:55:14 -0700

    #238 #215 Application Reducer reference update

--- apps/forms-flow-ai/forms-flow-web/src/modules/index.js
+++ apps/forms-flow-ai/forms-flow-web/src/modules/index.js
@@ -1,22 +1,12 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
-
-
 import user from "./userDetailReducer";
-
-
-import applications from './applicationsReducer';
-
-
 import foiRequests from  './FOI/foiRequestsReducer';
 
 const createRootReducer = (history) =>
   combineReducers({
     user,       
-    applications,
-
-    router: connectRouter(history),
-   
+    router: connectRouter(history),   
     foiRequests
   });
 
