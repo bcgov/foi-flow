e669be06e778854c162a3a5751f0e03e542bcbd5
Abin Antony <abin.antony@aot-technologies.com>
2021-09-07 15:58:24 -0700

    #215 refactoring constansts

--- apps/forms-flow-ai/forms-flow-web/src/constants/taskConstants.js
+++
@@ -1,4 +0,0 @@
-export const TASK_FILTER_LIST_DEFAULT_PARAM = {sortBy: "created",
-    sortOrder: "desc",
-   label:"Created"
-  };
--- apps/forms-flow-ai/forms-flow-web/src/constants/userContants.js
+++
@@ -1,2 +0,0 @@
-//TODO make it dynamic from env
-export const REVIEWER_GROUP="formsflow/formsflow-reviewer";
