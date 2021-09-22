1a3077982d04065982e0e45112218e466054810b
Abin Antony <abin.antony@aot-technologies.com>
2021-09-21 18:29:38 -0700

    #470 #453 Status update on front mapping

--- apps/forms-flow-ai/forms-flow-web/src/helper/FOI/statusEnum.js
+++ apps/forms-flow-ai/forms-flow-web/src/helper/FOI/statusEnum.js
@@ -2,7 +2,9 @@ const stateList = Object.freeze({
     unopened: [{status: "Unopened", isSelected: false}, {status: "Intake in Progress", isSelected: false}],
     intakeinprogress: [{status:"Intake in Progress", isSelected: false}, {status: "Open", isSelected: false}, {status: "Closed", isSelected: false}, {status: "Redirect", isSelected: false}],
     open: [{status: "Open", isSelected: false}, {status: "Closed", isSelected: false}, {status: "Call for Records", isSelected: false}],
-    closed: [{status: "Closed", isSelected: false},{status: "Open", isSelected: false}]
+    closed: [{status: "Closed", isSelected: false},{status: "Open", isSelected: false}],
+    redirect: [{status: "Redirect", isSelected: false},{status: "Closed", isSelected: false}],
+    callforrecords: [{status: "Redirect", isSelected: false},{status: "Closed", isSelected: false}],
 });
 
 export { stateList };
