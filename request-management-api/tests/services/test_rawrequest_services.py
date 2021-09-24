4639a4ddc23291b151383e1177da2d6122ac1055
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2021-09-24 09:02:45 -0700

    unit test failure fix

--- request-management-api/tests/services/test_rawrequest_services.py
+++ request-management-api/tests/services/test_rawrequest_services.py
@@ -13,10 +13,10 @@ def pytest_namespace():
     return {'requestidtoupdate': 0}
 
 def test_save_rawrequest(session):
-    response = rawrequestservice.saverawrequest(requestjson,'onlineform')
+    response = rawrequestservice.saverawrequest(requestjson,'onlineform',None)
     requestid = response.identifier
     pytest.approxrequestidtoupdate = requestid
-    wfupdateresponse = rawrequestservice.updateworkflowinstance(str(uuid.uuid4()),requestid)
+    wfupdateresponse = rawrequestservice.updateworkflowinstance(str(uuid.uuid4()),requestid,'service-account-forms-flow-bpm')
     assert response.success == True and wfupdateresponse.success == True
 
 def test_save_rawrequestversion(session):
