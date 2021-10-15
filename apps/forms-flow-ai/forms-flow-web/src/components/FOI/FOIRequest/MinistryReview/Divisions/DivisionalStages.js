583005cba85a6012649227503babc32bc36e82cd
Abin Antony <abin.antony@aot-technologies.com>
2021-10-15 16:33:19 -0700

    #566 UI UX Div stages WIP

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/MinistryReview/Divisions/DivisionalStages.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/MinistryReview/Divisions/DivisionalStages.js
@@ -4,11 +4,13 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Input from '@material-ui/core/Input';
 import './divisionstages.scss';
 const DivisionalStages = React.memo((props) => {
+    const [minDivStages, setMinDivStages] = React.useState([]);
     const divisionList = [{ id: "0", division: "Select Division to Send CFR" }, { id: "1", division: "Divison 1" }, { id: "2", division: "Divison 2" }]
     const divisionItems = divisionList.length > 0 && divisionList.map((item) => {
 
+        let _mindivtem = minDivStages.filter(d=>d.division.toLowerCase() === item.division.toLowerCase())               
         return (
-            <MenuItem className="foi-division-menuitem" key={item.id} value={item.division} >
+            <MenuItem disabled={_mindivtem.length > 0 } className="foi-division-menuitem" key={item.id} value={item.division} >
                 <span className={`foi-menuitem-span ${item.division.toLowerCase().replace(/\s/g, '')}`} ></span>
                 {item.division}
             </MenuItem>
@@ -26,21 +28,55 @@ const DivisionalStages = React.memo((props) => {
         )
     });
 
-    const handleDivisionChange = (e)=> {
+    const handleDivisionChange = (e,id)=> {
 
         console.log('handleDivisionChange')
         console.log(e)
+        let arr = minDivStages;
+        const exists = arr.filter(st=>st.division === e.target.value).length > 0
+        if(!exists)
+        {
+            arr.push({id:id,division:e.target.value,stage:""})            
+            setMinDivStages([...arr])
+            appendstageIterator([...arr])
+        }
+        
+        console.log(minDivStages)
     }
-    const handleDivisionStageChange = (e)=> {
+    const handleDivisionStageChange = (e,id)=> {
 
+        let arr = minDivStages;
+        const exists = arr.filter(st=>st.id === id).length > 0
+        if(exists)
+        {
+            arr.filter(st=>st.id === id).forEach(item=>item.stage = e.target.value)
+
+        }
+        setMinDivStages([...arr])
         console.log('handleDivisionStageChange')    
         console.log(e)
+        console.log(minDivStages)
     }
 
-    var divisionalStagesRow = (_id) => {
+    console.log("Divstages")
+    console.log(minDivStages)
 
+    const deleteMinistryDivision = (id)=>{
+
+        let existing = stageIterator;
+        let updatedIterator = existing.filter(i=>i.id !== id);
+       
+        appendstageIterator([...updatedIterator])
+        setMinDivStages([...updatedIterator])
+       
+
+    }
+
+    var divisionalStagesRow = (row) => {
+
+        let _id = row.id
         return (
-            <div className="row foi-details-row" id={_id}>
+            <div className="row foi-details-row" id="foi-division-row"_id>
                 <div className="col-lg-5 foi-details-col">
                     <TextField
                         id="foi-division-dropdown"_id
@@ -49,11 +85,12 @@ const DivisionalStages = React.memo((props) => {
                         select
                         input={<Input />}
                         variant="outlined"
-                        onChange={handleDivisionChange}
+                        onChange={e => handleDivisionChange(e,_id)}
                         fullWidth                        
                         label="Select Divison"
+                        value={row.division}
                     >
-                        {divisionItems}
+                        {[...divisionItems]}
                     </TextField>
 
                 </div>
@@ -67,36 +104,34 @@ const DivisionalStages = React.memo((props) => {
                         variant="outlined"
                         fullWidth                        
                         label="Select Divison Stage"
-                        onChange={handleDivisionStageChange}
+                        onChange={e => handleDivisionStageChange(e,_id)}
+                        value={row.stage}
                     >
                         {divisionstageItems}
                     </TextField>
                 </div>
                 <div className="col-lg-2 foi-details-col">
-                    <i class="fa fa-trash fa-3 foi-bin" aria-hidden="true"></i>
+                    <i class="fa fa-trash fa-3 foi-bin" aria-hidden="true" onClick={e=>deleteMinistryDivision(_id)}></i>
                 </div>
             </div>
         )
 
     }
 
-    var divstageslist = []
-    divstageslist.push(1)
-    const [divstages, setdivstages] = React.useState(divstageslist);
-
-
-
-
-
+    var stageCounter = []
+    stageCounter.push({id:0,division:"",stage:""})
+    const [stageIterator, appendstageIterator] = React.useState(stageCounter);
+    
+    
     const addDivisionalStage = () => {
 
-        let existing = divstages;
-        var val = divstages[divstages.length - 1] + 1
-        if (divisionList.length >= val) {
-            existing.push(val)
-            setdivstages([...existing])
+        let existing = stageIterator;
+        var val = stageIterator[stageIterator.length - 1].id + 1
+        if (divisionList.length >= val) {           
+            existing.push({id:val,division:"",stage:""})
+            appendstageIterator([...existing])
         }
-        console.log(divstages)
+        console.log(stageIterator)
     }
 
 
@@ -105,7 +140,8 @@ const DivisionalStages = React.memo((props) => {
             <div id="divstages" >
 
                 {
-                    divstages.map((item) =>
+                    stageIterator.map((item) =>
+
                         divisionalStagesRow(item)
                     )
                 }
