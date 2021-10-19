707f8c19130d28c41b2ff56b4b797def220037cb
Richard Qi <richard.qi@aot-technologies.com>
2021-10-19 13:37:20 -0700

    closed request cannot change status

--- apps/forms-flow-ai/forms-flow-web/src/constants/FOI/statusEnum.js
+++ apps/forms-flow-ai/forms-flow-web/src/constants/FOI/statusEnum.js
@@ -2,7 +2,7 @@ const StateList = Object.freeze({
     unopened: [{status: "Unopened", isSelected: false}, {status: "Intake in Progress", isSelected: false}],
     intakeinprogress: [{status:"Intake in Progress", isSelected: false}, {status: "Open", isSelected: false}, {status: "Closed", isSelected: false}, {status: "Redirect", isSelected: false}],
     open: [{status: "Open", isSelected: false}, {status: "Closed", isSelected: false}, {status: "Call For Records", isSelected: false}],
-    closed: [{status: "Closed", isSelected: false},{status: "Open", isSelected: false}],
+    closed: [{status: "Closed", isSelected: false}],
     redirect: [{status: "Redirect", isSelected: false},{status: "Closed", isSelected: false}],
     callforrecords: [{status: "Call For Records", isSelected: false},{status: "Review", isSelected: false},{status: "Fee Assessed", isSelected: false},{status: "Open", isSelected: false},{status: "Closed", isSelected: false}],
     review: [{status: "Review", isSelected: false},{status: "Call For Records", isSelected: false},{status: "Consult", isSelected: false},{status: "Ministry Sign Off", isSelected: false},{status: "Closed", isSelected: false}],
