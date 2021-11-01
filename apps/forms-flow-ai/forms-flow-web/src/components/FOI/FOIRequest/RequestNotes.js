34fb4d4f3cc1b6518736d1da94978fbaabf0155a
Abin Antony <abin.antony@aot-technologies.com>
2021-11-01 12:46:16 -0700

    #480 Notes React component integration -WIP

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/RequestNotes.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/RequestNotes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import './requestnotes.scss';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
@@ -6,6 +6,7 @@ import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { makeStyles } from '@material-ui/core/styles';
+import {CommentSection} from '../customComponents/Comments'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,9 +20,76 @@ const useStyles = makeStyles((theme) => ({
   }));
 const RequestNotes = React.memo((props) => {
     const classes = useStyles();
+    const data = [
+      {
+        "userId": "01a",
+        "comId": "012",
+        "fullName": "Abin Antony",
+        "avatarUrl": "https://ui-avatars.com/api/name=Riya&background=random" ,
+        "text": "Hey, Loved your blog! ",
+        "replies": [
+          {
+            "userId": "02a",
+            "comId": "013",
+      
+            "fullName": "Divya Viswanath",
+            "avatarUrl": "https://ui-avatars.com/api/name=Adam&background=random" ,
+            "text": "Thanks! It took me 1 month to finish this project but I am glad it helped out someone!🥰"
+          },
+          {
+            "userId": "01a",
+            "comId": "014",
+      
+            "fullName": "Abin Antony",
+            "avatarUrl": "https://ui-avatars.com/api/name=Riya&background=random",
+            "text": "thanks!😊"
+          }
+        ]
+      },
+      {
+        "userId": "02a",
+        "comId": "07",
+        "fullName": "Divya Viswanath",
+        "text": "Follow my page for more such interesting blogs!😇",
+        "avatarUrl": "https://ui-avatars.com/api/name=Adam&background=random"
+      },
+      {
+        "userId": "02a",
+        "comId": "015",
+        "fullName": "Robert Jae",
+        "avatarUrl": "https://ui-avatars.com/api/name=Robert&background=random",
+        "text": "Woah pretty helpful! how did you solve for x?",
+        "replies": [
+          {
+            "userId": "01b",
+            "comId": "016",
+      
+            "fullName": "Divya Viswanath",
+            "text": "Thanks! refer to this link -> acs.com",
+            "avatarUrl": "https://ui-avatars.com/api/name=Adam&background=random"
+          }
+        ]
+      },
+      {
+        "userId": "02b",
+        "comId": "017",
+        "fullName": "Sumathi",
+        "text": "I have a doubt about the 4th point🤔",
+        "avatarUrl": "https://ui-avatars.com/api/name=Lily&background=random"
+      }
+    ]
+    const [comment, setComment] = useState(data)
+    const userId = "01a"
+    const avatarUrl = "https://ui-avatars.com/api/name=Riya&background=random"
+    const name = "xyz"
+    const signinUrl = "/signin"
+    const signupUrl = "/signup"
+    let count = 0
+
+  comment.map(i => { count += 1; i.replies && i.replies.map(i => count += 1) })
      return (
     <div className={classes.root}>
-      <Accordion>
+      {/* <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
@@ -30,12 +98,11 @@ const RequestNotes = React.memo((props) => {
           <Typography className={classes.heading}>REQUEST NOTES</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
+        
         </AccordionDetails>
-      </Accordion>
+      </Accordion> */}
+      <CommentSection currentUser={userId && { userId: userId, avatarUrl: avatarUrl, name: name }} commentsArray={comment}
+        setComment={setComment} signinUrl={signinUrl} signupUrl={signupUrl} />
     </div>
     );
   });
