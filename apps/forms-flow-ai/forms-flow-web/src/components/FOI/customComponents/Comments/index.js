57e7fb65aee677e97cbbca807d80fb6fdb804962
Abin Antony <abin.antony@aot-technologies.com>
2021-11-05 11:34:50 -0700

    #784 Comments Add comment button UX

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/customComponents/Comments/index.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/customComponents/Comments/index.js
@@ -14,6 +14,8 @@ export const CommentSection = ({
   customInput
 }) => {
   const [comments, setComments] = useState(commentsArray)
+  const [showaddbox, setshowaddbox] = useState(false)
+  const [addbuttontext, setaddbuttontext] = useState("Add Comment")
   useEffect(() => {
     setComments(commentsArray)
   }, [commentsArray])
@@ -28,7 +30,11 @@ export const CommentSection = ({
       customInput={customInput}
     >
       <div className="section">
-        <div className="inputBox">
+        <div className="addcommentBox">
+            <button type="button" className="btn foi-btn-create addcomment" onClick={()=>{!showaddbox ? setshowaddbox(true):setshowaddbox(false); addbuttontext == "Add Comment" ? setaddbuttontext("Hide") : setaddbuttontext("Add Comment");   }}>{addbuttontext}</button>
+        </div>
+
+        <div className="inputBox" style={ {display: showaddbox ? 'block':'none'}}>
           {<Input />}
         </div>
         <div className="displayComments">
