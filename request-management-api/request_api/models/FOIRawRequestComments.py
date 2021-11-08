9e3e09d8d26f64a4053c6672b290e2f9f3ba8c3c
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2021-11-08 08:46:48 -0800

    CRUD operations : POST, GET, PUT, DISABLE comments.

---
+++ request-management-api/request_api/models/FOIRawRequestComments.py
@@ -0,0 +1,68 @@
+from flask.app import Flask
+from sqlalchemy.sql.schema import ForeignKey
+from .db import  db, ma
+from datetime import datetime
+from sqlalchemy.orm import relationship,backref
+from .default_method_result import DefaultMethodResult
+from sqlalchemy.dialects.postgresql import JSON, UUID
+from sqlalchemy.sql.expression import distinct
+from sqlalchemy import text
+
+import json
+class FOIRawRequestComment(db.Model):
+    # Name of the table in our database
+    __tablename__ = 'FOIRawRequestComments' 
+    # Defining the columns
+    commentid = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    requestid =db.Column(db.Integer, db.ForeignKey('FOIRawRequests.requestid'))
+    version =db.Column(db.Integer, db.ForeignKey('FOIRawRequests.version'))
+    comment = db.Column(db.String(1000), unique=False, nullable=True)  
+    parentcommentid = db.Column(db.Integer, nullable=True)
+    isactive = db.Column(db.Boolean, unique=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    createdby = db.Column(db.String(120), unique=False, nullable=True)
+    updated_at = db.Column(db.DateTime, nullable=True)
+    updatedby = db.Column(db.String(120), unique=False, nullable=True)
+
+    commenttypeid = db.Column(db.Integer, unique=False, nullable=False)
+    #commenttype =  relationship("CommentType",backref=backref("CommentTypes"),uselist=False)
+    
+    @classmethod
+    def savecomment(cls, commenttypeid, foirequestcomment, version, userid)->DefaultMethodResult:   
+        parentcommentid = foirequestcomment["parentcommentid"] if 'parentcommentid' in foirequestcomment  else None
+        newcomment = FOIRawRequestComment(commenttypeid=commenttypeid, requestid=foirequestcomment["requestid"], version=version, comment=foirequestcomment["comment"], parentcommentid=parentcommentid, isactive=True, created_at=datetime.now(), createdby=userid)
+        db.session.add(newcomment)
+        db.session.commit()               
+        return DefaultMethodResult(True,'Comment added',newcomment.commentid)          
+
+    @classmethod
+    def disablecomment(cls, commentid, userid):   
+        dbquery = db.session.query(FOIRawRequestComment)
+        comment = dbquery.filter_by(commentid=commentid)
+        if(comment.count() > 0) :             
+            comment.update({FOIRawRequestComment.isactive:False, FOIRawRequestComment.updatedby:userid, FOIRawRequestComment.updated_at:datetime.now()}, synchronize_session = False)
+            db.session.commit()
+            return DefaultMethodResult(True,'Comment disabled',commentid)
+        else:
+            return DefaultMethodResult(True,'No Comment found',commentid)
+
+    @classmethod
+    def updatecomment(cls, commentid, foirequestcomment, userid):   
+        dbquery = db.session.query(FOIRawRequestComment)
+        comment = dbquery.filter_by(commentid=commentid)
+        if(comment.count() > 0) :             
+            comment.update({FOIRawRequestComment.isactive:True, FOIRawRequestComment.comment:foirequestcomment["comment"], FOIRawRequestComment.updatedby:userid, FOIRawRequestComment.updated_at:datetime.now()}, synchronize_session = False)
+            db.session.commit()
+            return DefaultMethodResult(True,'Comment updated',commentid)
+        else:
+            return DefaultMethodResult(True,'No Comment found',commentid)
+            
+    @classmethod
+    def getcomments(cls, requestid)->DefaultMethodResult:   
+        comment_schema = FOIRawRequestCommentSchema(many=True)
+        query = db.session.query(FOIRawRequestComment).filter_by(requestid=requestid, isactive = True).order_by(FOIRawRequestComment.commentid.desc()).all()
+        return comment_schema.dump(query)   
+    
+class FOIRawRequestCommentSchema(ma.Schema):
+    class Meta:
+        fields = ('commentid', 'requestid', 'parentcommentid','comment', 'commenttypeid','commenttype','isactive','created_at','createdby','updated_at','updatedby') 
---
+++ request-management-api/request_api/resources/foicomment.py
@@ -0,0 +1,163 @@
+# Copyright © 2021 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""API endpoints for managing a FOI Requests resource."""
+
+
+from flask import g, request
+from flask_restx import Namespace, Resource, cors
+from flask_expects_json import expects_json
+from request_api.auth import auth
+from request_api.auth import auth, AuthHelper
+from request_api.tracer import Tracer
+from request_api.utils.util import  cors_preflight, allowedOrigins
+from request_api.exceptions import BusinessException, Error
+from request_api.services.commentservice import commentservice
+from request_api.schemas.foicomment import  FOIRawRequestCommentSchema, FOIMinistryRequestCommentSchema
+from request_api.schemas.foicomment import  EditFOIRawRequestCommentSchema, FOIMinistryRequestCommentSchema
+import json
+from flask_cors import cross_origin
+
+
+API = Namespace('FOIComment', description='Endpoints for FOI Comment management')
+TRACER = Tracer.get_instance()  
+        
+@cors_preflight('POST,OPTIONS')
+@API.route('/foicomment/ministryrequest')
+class CreateFOIRequestComment(Resource):
+    """Resource for managing FOI requests."""
+
+       
+    @staticmethod
+    @TRACER.trace()
+    @cross_origin(origins=allowedOrigins())
+    @auth.require
+    def post():      
+        try:
+            requestjson = request.get_json() 
+            minrquescommentschema = FOIMinistryRequestCommentSchema().load(requestjson)  
+            result = commentservice().createministryrequestcomment(minrquescommentschema, AuthHelper.getUserId())
+            return {'status': result.success, 'message':result.message,'id':result.identifier} , 200 
+        except ValueError:
+            return {'status': 500, 'message':"Invalid Request Id"}, 500
+        except KeyError as err:
+            return {'status': False, 'message':err.messages}, 400        
+        except BusinessException as exception:            
+            return {'status': exception.status_code, 'message':exception.message}, 500 
+
+@cors_preflight('POST,OPTIONS')
+@API.route('/foicomment/rawrequest')
+class CreateFOIRawRequestComment(Resource):
+    """Resource for managing FOI requests."""
+
+     
+    @staticmethod
+    @TRACER.trace()
+    @cross_origin(origins=allowedOrigins())
+    @auth.require
+    def post():      
+        try:
+            requestjson = request.get_json() 
+            rawrqcommentschema = FOIRawRequestCommentSchema().load(requestjson)  
+            result = commentservice().createrawrequestcomment(rawrqcommentschema, AuthHelper.getUserId())
+            return {'status': result.success, 'message':result.message,'id':result.identifier} , 200 
+        except ValueError:
+            return {'status': 500, 'message':"Invalid Request Id"}, 500
+        except KeyError as err:
+            return {'status': False, 'message':err.messages}, 400        
+        except BusinessException as exception:            
+            return {'status': exception.status_code, 'message':exception.message}, 500 
+        
+@cors_preflight('GET,OPTIONS')
+@API.route('/foicomment/<requesttype>/<requestid>')
+class FOIComment(Resource):
+    """Resource for managing FOI requests."""
+
+       
+    @staticmethod
+    @TRACER.trace()
+    @cross_origin(origins=allowedOrigins())
+    @auth.require
+    def get(requesttype, requestid):      
+        if requesttype != "ministryrequest" and requesttype != "rawrequest":
+                return {'status': False, 'message':'Bad Request'}, 400 
+        try:
+            if requesttype == "ministryrequest":
+                result = commentservice().getministryrequestcomments(requestid)
+            else:
+                result = commentservice().getrawrequestcomments(requestid)
+            return json.dumps(result), 200
+        except ValueError:
+            return {'status': 500, 'message':"Invalid Request Id"}, 500
+        except KeyError as err:
+            return {'status': False, 'message':err.messages}, 400        
+        except BusinessException as exception:            
+            return {'status': exception.status_code, 'message':exception.message}, 500   
+        
+
+
+@cors_preflight('PUT,OPTIONS')
+@API.route('/foicomment/<requesttype>/<commentid>/disable')
+class FOIDisableComment(Resource):
+    """Resource for managing FOI requests."""
+
+      
+    @staticmethod
+    @TRACER.trace()
+    @cross_origin(origins=allowedOrigins())
+    @auth.require
+    def put(requesttype, commentid):      
+        if requesttype != "ministryrequest" and requesttype != "rawrequest":
+                return {'status': False, 'message':'Bad Request'}, 400 
+        try:
+            if requesttype == "ministryrequest":
+                result = commentservice().disableministryrequestcomment(commentid, AuthHelper.getUserId())
+            else:
+                result = commentservice().disablerawrequestcomment(commentid, AuthHelper.getUserId())
+            return {'status': result.success, 'message':result.message,'id':result.identifier} , 200 
+        except ValueError:
+            return {'status': 500, 'message':"Invalid Request Id"}, 500
+        except KeyError as err:
+            return {'status': False, 'message':err.messages}, 400        
+        except BusinessException as exception:            
+            return {'status': exception.status_code, 'message':exception.message}, 500
+        
+
+@cors_preflight('PUT,OPTIONS')
+@API.route('/foicomment/<requesttype>/<commentid>')
+class FOIUpdateComment(Resource):
+    """Resource for managing FOI requests."""
+
+      
+    @staticmethod
+    @TRACER.trace()
+    @cross_origin(origins=allowedOrigins())
+    @auth.require
+    def put(requesttype, commentid):      
+        if requesttype != "ministryrequest" and requesttype != "rawrequest":
+                return {'status': False, 'message':'Bad Request'}, 400 
+        try:
+            requestjson = request.get_json()              
+            if requesttype == "ministryrequest":
+                commentschema = FOIMinistryRequestCommentSchema().load(requestjson)
+                result = commentservice().updateministryrequestcomment(commentid, commentschema, AuthHelper.getUserId())
+            else:
+                commentschema = EditFOIRawRequestCommentSchema().load(requestjson)
+                result = commentservice().updaterawrequestcomment(commentid, commentschema, AuthHelper.getUserId())
+            return {'status': result.success, 'message':result.message,'id':result.identifier} , 200 
+        except ValueError:
+            return {'status': 500, 'message':"Invalid Request Id"}, 500
+        except KeyError as err:
+            return {'status': False, 'message':err.messages}, 400        
+        except BusinessException as exception:            
+            return {'status': exception.status_code, 'message':exception.message}, 500
