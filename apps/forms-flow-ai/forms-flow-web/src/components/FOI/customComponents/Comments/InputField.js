42b12bf2b7b2341c7f58cb427d7cfd51572808f2
Abin Antony <abin.antony@aot-technologies.com>
2021-11-12 13:59:55 -0800

    #777 post button vs add mode fix

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/customComponents/Comments/InputField.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/customComponents/Comments/InputField.js
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from 'react'
 import './comments.scss'
 import { ActionContext } from './ActionContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPaperPlane, faTimes } from '@fortawesome/free-solid-svg-icons'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
@@ -16,15 +16,15 @@ const InputField = ({ cancellor, parentId, child, value, edit, main }) => {
 
   const handleQuillChange = (htmlcontent, delta, source, editor) => {
     let _unformattedtext = editor.getText()
-    
+
     if (_unformattedtext && _unformattedtext != "" && _unformattedtext != undefined && textlength <= maxcharacterlimit) {
 
       if (_unformattedtext.length - 1 <= maxcharacterlimit) {
         setText(htmlcontent)
 
       }
-      else { 
-        setText(_unformattedtext.substring(0,maxcharacterlimit-1)) 
+      else {
+        setText(_unformattedtext.substring(0, maxcharacterlimit - 1))
       }
 
       setTextLength(maxcharacterlimit - (_unformattedtext && _unformattedtext != "" && _unformattedtext.length - 1 <= maxcharacterlimit ? _unformattedtext.length - 1 : 0))
@@ -42,11 +42,13 @@ const InputField = ({ cancellor, parentId, child, value, edit, main }) => {
   }, [value])
 
 
-  const cancel = () => {
-    setText("")
+  const cancel = (e) => {
+    setText('')
     edit
       ? actions.handleCancel(cancellor, edit)
       : actions.handleCancel(cancellor)
+
+     e.preventDefault() 
   }
 
   const post = () => {
@@ -73,21 +75,33 @@ const InputField = ({ cancellor, parentId, child, value, edit, main }) => {
         <ReactQuill theme="snow" value={text || ''} onKeyDown={handlekeydown} onChange={handleQuillChange} placeholder={"Type your comments here"} />
 
         <div className="inputActions">
-          <div className="col-lg-11">
-          <span className="characterlen">{textlength} characters remaining</span>
+          <div className={!main ? 'col-lg-10' :'col-lg-11'}>
+            <span className="characterlen">{textlength} characters remaining</span>
           </div>
-          <div className="col-lg-1">
-          <button
-            className="postBtn"
-            onClick={post}
-            type='button'
-            disabled={!text}
+          <div className="col-lg-2">
+          {(!main) ? (
+              <button
+                className="cancelBtn"
+                onClick={cancel}
+               
+              >
+                <FontAwesomeIcon icon={faTimes} size='2x' color={text === undefined || text.length === 0 || textlength === maxcharacterlimit ? '#a5a5a5' : 'darkblue'} />
+              </button>
+            ):null}
 
-          >
-            {' '}
-            <FontAwesomeIcon icon={faPaperPlane} size='2x' color={!text ? '#a5a5a5' : 'darkblue'} />
-          </button>  
-          </div>        
+            <button
+              className="postBtn"
+              onClick={post}
+              type='button'
+              disabled={!text}
+
+            >
+              {' '}
+              <FontAwesomeIcon icon={faPaperPlane} size='2x' color={ text === undefined || text.length === 0 || textlength === maxcharacterlimit ? '#a5a5a5' : 'darkblue'} />
+            </button>
+         
+            
+          </div>
         </div>
 
       </form>
