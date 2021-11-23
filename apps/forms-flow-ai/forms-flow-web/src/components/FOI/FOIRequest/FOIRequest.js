6cf5d5572a5a88951555ab3f801b51f7a9b6d47d
divyav-aot <divya.v@aot-technologies.com>
2021-11-23 18:33:07 -0500

    Removed unwanted code

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
@@ -594,24 +594,6 @@ const FOIRequest = React.memo(({userDetail}) => {
       break;      
   }
 
-  // const tabclick =(evt,param)=>{
-   
-  //   var i, tabcontent, tablinks;
-    
-  //   tabcontent = document.getElementsByClassName("tabcontent");
-  //   for (i = 0; i < tabcontent.length; i++) {
-  //     tabcontent[i].style.display = "none";
-  //   }
-   
-  //   tablinks = document.getElementsByClassName("tablinks");
-  //   for (i = 0; i < tablinks.length; i++) {
-  //     tablinks[i].className = tablinks[i].className.replace(" active", "");
-  //   }
-  //   document.getElementById(param).style.display = "block";
-  //   evt.currentTarget.className += " active";
-
-  // }
-
   const tabclick =(evt,param)=>{
    
     var i, tabcontent, tablinks;
