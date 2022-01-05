ceedfe0e7062e9fcd569c7d2636784452134ad4b
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2022-01-05 14:12:39 -0800

    formatting notification date at server side for PST only.

--- request-management-api/request_api/models/FOIRequestNotifications.py
+++ request-management-api/request_api/models/FOIRequestNotifications.py
@@ -47,7 +47,9 @@ class FOIRequestNotification(db.Model):
         rs = db.session.execute(text(sql), {'userid': userid, 'days': days})
         notifications = []
         for row in rs:
-            notifications.append({"idnumber": row["idnumber"], "notificationid": row["notificationid"], "notification": row["notification"], "notificationtype": row["notificationtype"],  "notificationusertype": row["notificationusertype"], "created_at": row["created_at"].strftime('%Y-%m-%d %H:%M:%S.%f'), "createdby": row["createdby"], "requesttype":row["requesttype"], "requestid":row["requestid"],"foirequestid":row["foirequestid"]})
+            dt = maya.parse(row["created_at"]).datetime(to_timezone='America/Vancouver', naive=False)
+            _createddate = dt
+            notifications.append({"idnumber": row["idnumber"], "notificationid": row["notificationid"], "notification": row["notification"], "notificationtype": row["notificationtype"],  "notificationusertype": row["notificationusertype"], "created_at": _createddate.strftime('%Y %b %d | %H:%M:%S %p'), "createdby": row["createdby"], "requesttype":row["requesttype"], "requestid":row["requestid"],"foirequestid":row["foirequestid"]})
         return notifications
 
     @classmethod
