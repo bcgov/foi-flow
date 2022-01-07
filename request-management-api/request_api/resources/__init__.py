c9ff20cb911f39c9eaa80fd3da7ae8444e34655f
divyav-aot <divyav-aot@github>
2022-01-07 13:01:06 -0500

    1246 - POST API - v3

--- request-management-api/request_api/resources/__init__.py
+++ request-management-api/request_api/resources/__init__.py
@@ -37,6 +37,7 @@ from .foiaudit import API as FOIAUDTI_API
 from .foiwatcher import API as FOIWATCHER_API
 from .foicomment import API as FOICOMMENT_API
 from .foidocument import API as FOIDOCUMENT_API
+from .foiextension import API as FOIEXTENSION_API
 from .fee import API as FEE_API
 
 __all__ = ('API_BLUEPRINT')
@@ -70,4 +71,5 @@ API.add_namespace(FOIAUDTI_API,'/api')
 API.add_namespace(FOIWATCHER_API,'/api')
 API.add_namespace(FOICOMMENT_API,'/api')
 API.add_namespace(FOIDOCUMENT_API,'/api')
+API.add_namespace(FOIEXTENSION_API,'/api')
 API.add_namespace(FEE_API,'/api')
