41a5fe9fd15e902aa15b3123885bc69d48cc09e7
Jad <jad.saad@aot-technologies.com>
2022-01-13 09:10:43 -0800

    Revert unnecessary changed and removing unused import

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/ExtensionDetails/index.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/ExtensionDetails/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { ActionProvider } from "./ActionContext";
 import ExtensionDetailsBox from "./ExtensionDetailsBox"
