7324bd5bce0862c3f240b1de12defb08d27f20a8
Richard Qi <richard.qi@aot-technologies.com>
2022-01-31 12:43:50 -0800

    update default sorting for ministry queue

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/Dashboard/MinistryDashboard.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/Dashboard/MinistryDashboard.js
@@ -29,7 +29,7 @@ const MinistryDashboard = ({userDetail}) => {
   const defaultRowsState = {page: 0, pageSize: 10};
   const [rowsState, setRowsState] = React.useState(defaultRowsState);
   
-  const defaultSortModel = [{field: 'currentState', sort: 'desc'}, {field: 'cfrduedate', sort: 'asc'}];
+  const defaultSortModel = [{field: 'currentState', sort: 'asc'}, {field: 'cfrduedate', sort: 'asc'}];
   const [sortModel, setSortModel] = React.useState(defaultSortModel);
   let serverSortModel;
   const [filterModel, setFilterModel] = React.useState({
