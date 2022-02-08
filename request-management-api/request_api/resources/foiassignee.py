9b050a12f438e5181f100186afb359c6ac87a3ea
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2022-02-08 12:45:28 -0800

    Code smell fixes.

--- request-management-api/request_api/resources/foiassignee.py
+++ request-management-api/request_api/resources/foiassignee.py
@@ -29,6 +29,11 @@ from flask_cors import cross_origin
 API = Namespace('FOIAssignee', description='Endpoints for FOI assignee management')
 TRACER = Tracer.get_instance()
 
+"""Custom exception messages
+"""
+EXCEPTION_MESSAGE_BAD_REQUEST='Bad Request'
+EXCEPTION_MESSAGE_NOT_FOUND='Not Found'
+
 
 @cors_preflight('GET,OPTIONS')
 @API.route('/foiassignees')
@@ -43,13 +48,13 @@ class FOIAssigneesByTypeAndStatus(Resource):
     @auth.require
     def get(requestype=None, status=None, bcgovcode=None):
         if requestype is not None and (requestype != "personal" and requestype != "general"):
-            return {'status': False, 'message':'Bad Request'}, 400   
+            return {'status': False, 'message':EXCEPTION_MESSAGE_BAD_REQUEST}, 400   
         try:
             result = assigneeservice().getgroupsandmembersbytypeandstatus(requestype, status, bcgovcode)
             if result is not None:
                 return json.dumps(result), 200
             else:
-                return {'status': False, 'message':'Not Found'}, 404   
+                return {'status': False, 'message':EXCEPTION_MESSAGE_NOT_FOUND}, 404   
         except BusinessException as exception:            
             return {'status': exception.status_code, 'message':exception.message}, 500    
         
@@ -70,7 +75,7 @@ class FOIAssigneesByTypeAndStatus(Resource):
             if result is not None:
                 return json.dumps(result), 200
             else:
-                return {'status': False, 'message':'Not Found'}, 404 
+                return {'status': False, 'message':EXCEPTION_MESSAGE_NOT_FOUND}, 404 
         except BusinessException as exception:            
             return {'status': exception.status_code, 'message':exception.message}, 500    
         
@@ -89,14 +94,14 @@ class FOIAssigneesTeams(Resource):
         """ POST Method for capturing FOI requests before processing"""
         try:
             if requestype is None:
-                return {'status': False, 'message':'Bad Request'}, 400   
+                return {'status': False, 'message':EXCEPTION_MESSAGE_BAD_REQUEST}, 400   
             if requestype is not None and (requestype != "personal" and requestype != "general"):
-                return {'status': False, 'message':'Bad Request'}, 400   
+                return {'status': False, 'message':EXCEPTION_MESSAGE_BAD_REQUEST}, 400   
             result = assigneeservice().getprocessingteamsbyrequesttype(requestype)
             if result is not None:
                 return json.dumps(result), 200
             else:
-                return {'status': False, 'message':'Not Found'}, 404 
+                return {'status': False, 'message':EXCEPTION_MESSAGE_NOT_FOUND}, 404 
         except BusinessException as exception:            
             return {'status': exception.status_code, 'message':exception.message}, 500    
         
