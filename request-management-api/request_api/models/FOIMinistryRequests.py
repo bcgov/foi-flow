038b9847a80bf4bd51809980580b63f31b8be9e7
Richard Qi <richard.qi@aot-technologies.com>
2022-02-14 15:36:41 -0800

    apply advanced search to all criterias

--- request-management-api/request_api/models/FOIMinistryRequests.py
+++ request-management-api/request_api/models/FOIMinistryRequests.py
@@ -566,11 +566,11 @@ class FOIMinistryRequest(db.Model):
             filtercondition.append(or_(*requeststatecondition))
         
         #request status: overdue || on time
-        # if(len(params['requeststatus']) == 1):
-        #     if(params['requeststatus'][0] == 'overdue'):
-        #         filtercondition.append(FOIMinistryRequest.findfield('duedate', iaoassignee, ministryassignee) < datetime.now())
-        #     else:
-        #         filtercondition.append(FOIMinistryRequest.findfield('duedate', iaoassignee, ministryassignee) >= datetime.now())
+        if(len(params['requeststatus']) == 1):
+            if(params['requeststatus'][0] == 'overdue'):
+                filtercondition.append(FOIMinistryRequest.findfield('duedate', iaoassignee, ministryassignee) < datetime.now())
+            else:
+                filtercondition.append(FOIMinistryRequest.findfield('duedate', iaoassignee, ministryassignee) >= datetime.now())
 
         #request type: personal, general
         if(len(params['requesttype']) > 0):
@@ -608,11 +608,11 @@ class FOIMinistryRequest(db.Model):
                     searchcondition.append(FOIMinistryRequest.findfield(params['search'], iaoassignee, ministryassignee).ilike('%'+keyword+'%'))
                 filtercondition.append(and_(*searchcondition))
 
-        # if(params['fromdate'] is not None):
-        #     filtercondition.append(FOIMinistryRequest.findfield('receivedDate', iaoassignee, ministryassignee) >= params['fromdate'])
+        if(params['fromdate'] is not None):
+            filtercondition.append(FOIMinistryRequest.findfield('receivedDate', iaoassignee, ministryassignee) >= params['fromdate'])
 
-        # if(params['todate'] is not None):
-        #     filtercondition.append(FOIMinistryRequest.findfield('duedate', iaoassignee, ministryassignee) <= params['todate'])
+        if(params['todate'] is not None):
+            filtercondition.append(FOIMinistryRequest.findfield('duedate', iaoassignee, ministryassignee) <= params['todate'])
         
         return filtercondition
 class FOIMinistryRequestSchema(ma.Schema):
