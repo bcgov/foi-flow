2c006f6f3e62116f7fe79073d159302670dfd7b4
Abin Antony <abin.antony@aot-technologies.com>
2022-02-16 15:27:09 -0800

    #1507 Boiler Initial Checkin with base project

---
+++ axisintegrationapi/MCS.FOI.AXISIntegration/MCS.FOI.AXISIntegrationWebAPI/Controllers/RequestSearchController.cs
@@ -0,0 +1,37 @@
+﻿using MCS.FOI.AXISIntegrationWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCS.FOI.AXISIntegrationWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestSearchController : ControllerBase
+    {
+
+        private readonly ILogger<RequestSearchController> _logger;
+
+       
+
+        public RequestSearchController(ILogger<RequestSearchController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<AXISRequest> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new AXISRequest()
+            {
+                AXISRequestID = "EDUC-2021-4524"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ axisintegrationapi/MCS.FOI.AXISIntegration/MCS.FOI.AXISIntegrationWebAPI/Models/AXISRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCS.FOI.AXISIntegrationWebAPI.Models
+{
+    public class AXISRequest
+    {
+
+        //MORE PROPERTIES TO ADD HERE. TODO
+
+        public string AXISRequestID { get; set; }
+
+        public string RequestDescription { get; set; }
+
+        public string RequestType { get; set; }
+
+        public string ReceivedDate { get; set; }
+        public string Category { get; set; }
+
+    }
+}
