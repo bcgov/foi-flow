51bc4372bb2708f66106c6f116b1ee76b4d80bdd
Abin Antony <abin.antony@aot-technologies.com>
2022-02-17 13:16:17 -0800

    #1507 DAL Unittest BoilerPlate - References

---
+++ axisintegrationapi/MCS.FOI.AXISIntegration/MCS.FOI.AXISIntegration.DAL.UnitTests/RequestDAUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MCS.FOI.AXISIntegration.DAL.UnitTests
+{
+
+    [TestClass]
+    public class RequestDAUnitTest
+    {
+
+        private Mock<ILogger> _mockLogger = new Mock<ILogger>();
+
+        [TestMethod]
+        public void GetRequestTest()
+        {
+            RequestsDA requestsDA = new RequestsDA(_mockLogger.Object);
+            requestsDA.GetAXISRequest("Sample");
+        }
+
+    }
+}
