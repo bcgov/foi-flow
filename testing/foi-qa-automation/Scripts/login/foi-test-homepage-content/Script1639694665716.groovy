ea2cc1e2219d0b24bef08a0419bc864181429fd3
unknown <nicholas.kan@aot-technologies.com>
2022-02-17 12:07:48 -0800

    try using different delay function

--- testing/foi-qa-automation/Scripts/login/foi-test-homepage-content/Script1639694665716.groovy
+++ testing/foi-qa-automation/Scripts/login/foi-test-homepage-content/Script1639694665716.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.waitForElementPresent(findTestObject('Page_foi.flow/navbar/img'), 5)
+WebUI.delay(5, FailureHandling.STOP_ON_FAILURE)
 
 WebUI.verifyElementPresent(findTestObject('Page_foi.flow/navbar/img'), 0)
 
