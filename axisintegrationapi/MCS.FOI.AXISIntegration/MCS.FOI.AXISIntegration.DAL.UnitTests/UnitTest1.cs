7e45dfba8601983d7c938aee57692badb8d3ef43
Abin Antony <abin.antony@aot-technologies.com>
2022-02-16 16:24:53 -0800

    #1507 Unit test Preoject and Reference to DAL Unit Tests

---
+++ axisintegrationapi/MCS.FOI.AXISIntegration/MCS.FOI.AXISIntegration.DAL.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCS.FOI.AXISIntegration.DAL.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
