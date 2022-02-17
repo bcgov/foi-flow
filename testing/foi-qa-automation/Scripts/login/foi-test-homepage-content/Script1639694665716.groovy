bccda61af950e63cee3421409234e04ae9098c38
unknown <nicholas.kan@aot-technologies.com>
2022-02-17 11:54:12 -0800

    try increased timeout since tests failing in github actions

--- testing/foi-qa-automation/Scripts/login/foi-test-homepage-content/Script1639694665716.groovy
+++ testing/foi-qa-automation/Scripts/login/foi-test-homepage-content/Script1639694665716.groovy
@@ -17,6 +17,8 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.waitForElementPresent(findTestObject('Page_foi.flow/navbar/img'), 5)
+
 WebUI.verifyElementPresent(findTestObject('Page_foi.flow/navbar/img'), 0)
 
 WebUI.verifyElementVisible(findTestObject('Page_foi.flow/navbar/img'))
