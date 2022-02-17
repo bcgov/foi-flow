3fa4c3c105676baba30ed48345505b1a9e49acfa
Abin Antony <abin.antony@aot-technologies.com>
2022-02-16 16:06:40 -0800

    Utility enum - #1507

---
+++ axisintegrationapi/MCS.FOI.AXISIntegration/MCS.FOI.AXISIntegration.Utilities/Types/Environments.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable once CheckNamespace
+namespace MCS.FOI.AXISIntegration.Utilities.Types
+{
+    /// <summary>
+    /// Server environments
+    /// </summary>
+    public enum Environments
+    {
+        Production,
+        Test,
+        Development
+    }
+}
