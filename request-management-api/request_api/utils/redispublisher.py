1d42cda283c28e2d5caa296b77727bed48393ae8
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2022-02-18 12:17:27 -0800

    Option to configure queue type.

--- request-management-api/request_api/utils/redispublisher.py
+++ request-management-api/request_api/utils/redispublisher.py
@@ -4,26 +4,29 @@ from request_api.exceptions import BusinessException, Error
 
 from flask import current_app
 from redis import Redis 
+from request_api.utils.redissubscriber import RedisSubscriberService
 class RedisPublisherService:
 
     foirequestqueueredishost = os.getenv('FOI_REQUESTQUEUE_REDISHOST')
     foirequestqueueredisport = os.getenv('FOI_REQUESTQUEUE_REDISPORT')
     foirequestqueueredispassword = os.getenv('FOI_REQUESTQUEUE_REDISPASSWORD')
     foirequestqueueredischannel = os.getenv('FOI_REQUESTQUEUE_REDISCHANNEL')
-    foicommentqueueredischannel = os.getenv('SOCKETIO_COMMENT_TOPIC')
+    foicommentqueueredischannel = os.getenv('SOCKETIO_REDIS_COMMENT_CHANNEL')
          
     foiredis = Redis(host=foirequestqueueredishost, port=foirequestqueueredisport,password=foirequestqueueredispassword)
-                   
+    
     async def publishrequest(self, message):  
         self.publishtoredischannel(self.foirequestqueueredischannel, message)
             
     async def publishcommment(self, message):  
         self.publishtoredischannel(self.foicommentqueueredischannel, message)
-        
-        
+              
     def publishtoredischannel(self, channel , message):  
         try: 
-            self.foiredis.publish(channel, message)            
+            if channel == os.getenv('FOI_REQUESTQUEUE_REDISCHANNEL'):
+                self.foiredis.publish(channel, message) 
+            if channel == os.getenv('SOCKETIO_REDIS_COMMENT_CHANNEL'):
+                RedisSubscriberService().foicommentredis.publish(channel, message)               
         except BusinessException as exception:            
             current_app.logger.error("%s,%s" % ('FOI request Queue REDIS Error', exception.message))
-   
+  
