b5cd3e206d34de51f7b5065ac88edc29aa3536f9
divyav-aot <divyav-aot@github>
2022-03-07 12:29:34 -0500

    Updated unit test cases

--- axisintegrationapi/MCS.FOI.AXISIntegration/MCS.FOI.AXISIntegration.DAL.UnitTests/RequestDAUnitTest.cs
+++ axisintegrationapi/MCS.FOI.AXISIntegration/MCS.FOI.AXISIntegration.DAL.UnitTests/RequestDAUnitTest.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MCS.FOI.AXISIntegration.DataModels;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Newtonsoft.Json;
 
 namespace MCS.FOI.AXISIntegration.DAL.UnitTests
 {
@@ -14,8 +16,36 @@ namespace MCS.FOI.AXISIntegration.DAL.UnitTests
         [TestMethod]
         public void GetRequestTest()
         {
-            RequestsDA requestsDA = new RequestsDA(_mockLogger.Object);
-            requestsDA.GetAXISRequestString("EDU-2015-50012");
+            RequestsDA requestsDA = new(_mockLogger.Object);
+            string jsonData = requestsDA.GetAXISRequestString("EDU-2015-50012");
+            /*
+             "axisRequestId":null,"axisSyncDate":null,"description":null
+             */
+
+            AXISRequest deserializedRequest = JsonConvert.DeserializeObject<AXISRequest>(jsonData);
+            var axisRequestId = deserializedRequest.AXISRequestID;
+            var axisSyncData = deserializedRequest.AxisSyncDate;
+            var description = deserializedRequest.RequestDescription;
+
+            Assert.AreEqual(axisRequestId, "EDU-2015-50012");
+            Assert.IsNotNull(axisSyncData);
+            Assert.IsNotNull(description);
+        }
+
+        [TestMethod]
+        public void GetNullRequestTest()
+        {
+            RequestsDA requestsDA = new(_mockLogger.Object);
+            string jsonData = requestsDA.GetAXISRequestString("IAP-2015-50012");
+
+            AXISRequest deserializedRequest = JsonConvert.DeserializeObject<AXISRequest>(jsonData);
+            var axisRequestId = deserializedRequest.AXISRequestID;
+            var axisSyncData = deserializedRequest.AxisSyncDate;
+            var description = deserializedRequest.RequestDescription;
+
+            Assert.IsNull(axisRequestId);
+            Assert.IsNull(axisSyncData);
+            Assert.IsNull(description);
         }
 
     }
