bc362a06ef359ac57b7051b5b2bf0a869347c7e8
Aparna S <aparna.s@aot-technologies.com>
2022-03-15 10:24:39 -0700

    field names file

---
+++ apps/forms-flow-ai/forms-flow-web/src/constants/FOI/axisSyncDisplayFields.js
@@ -0,0 +1,66 @@
+const AXIS_SYNC_DISPLAY_FIELDS = {
+    ASSIGNED_TO: "assignedTo",
+    MINISTRY_ASSIGNED_TO: "ministryAssignedTo",
+    APPLICANT_FIRST_NAME: "firstName",
+    APPLICANT_LAST_NAME: "lastName",
+    APPLICANT_MIDDLE_NAME: "middleName",
+    FOI_CATEGORY: "category",
+    FOI_SELECTED_CATEGORY: "selectedCategory",
+    APPLICANT_EMAIL: "email",
+    ORGANIZATION: "businessName",
+
+    CHILD_FIRST_NAME: "childFirstName",
+    CHILD_LAST_NAME: "childLastName",
+    CHILD_MIDDLE_NAME: "childMiddleName",
+    CHILD_NICKNAME: "childAlsoKnownAs",
+    CHILD_DOB: "childBirthDate",
+
+    ANOTHER_FIRST_NAME: "anotherFirstName",
+    ANOTHER_LAST_NAME: "anotherLastName",
+    ANOTHER_MIDDLE_NAME: "anotherMiddleName",
+    ANOTHER_NICKNAME: "anotherAlsoKnownAs",
+    ANOTHER_DOB: "anotherBirthDate",
+
+    HOME_PHONE: "phonePrimary",
+    MOBILE_PHONE: "phoneSecondary",
+    WORK_PHONE_PRIMARY: "workPhonePrimary",
+    WORK_PHONE_SECONDARY: "workPhoneSecondary",
+    STREET_ADDRESS_PRIMARY: "address",
+    STREET_ADDRESS_SECONDARY: "addressSecondary",
+    CITY: "city",
+    POSTALCODE: "postal",
+    PROVINCE: "province",
+    COUNTRY: "country",
+
+    FROM_DATE: "fromDate",
+    TO_DATE: "toDate",
+    DESCRIPTION: "description",
+    IS_PROGRAM_AREA_SELECTED: "isProgramAreaSelected",
+    PROGRAM_AREA_LIST: "programAreaList",
+
+    REQUEST_TYPE: "requestType",
+    RECEIVED_MODE: "receivedMode",
+    DELIVERY_MODE: "deliveryMode",
+    RECEIVED_DATE: "receivedDate",
+    RECEIVED_DATE_UF: "receivedDateUF",
+    REQUEST_START_DATE: "requestStartDate",
+    DUE_DATE: "dueDate",
+
+    PERSONAL_HEALTH_NUMBER: "personalHealthNumber",
+    CORRECTIONS_NUMBER: "correctionalServiceNumber",
+    DOB: "birthDate",
+    EMPLOYEE_NUMBER: "publicServiceEmployeeNumber",
+    IDENTITY_VERIFIED: "identityVerified",
+
+    REQUEST_TYPE_GENERAL: "general",
+    REQUEST_TYPE_PERSONAL: "personal",
+    CURRENT_STATE: "currentState",
+    XGOVE: "xgov",
+    ID_NUMBER: "idNumber",    
+    SELECTED_MINISTRIES: "selectedMinistries",
+
+    ADDITIONAL_PERSONAL_INFORMATION: "additionalPersonalInfo",
+    AXIS_REQUEST_ID:"axisRequestId"
+  };
+  
+  export default AXIS_SYNC_DISPLAY_FIELDS;
