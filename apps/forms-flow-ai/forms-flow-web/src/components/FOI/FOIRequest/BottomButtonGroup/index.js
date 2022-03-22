651b0b27c7756700a8713fc4c21b5c39a4a03f93
divyav-aot <divyav-aot@github>
2022-03-22 17:43:58 -0400

    Fixed build error

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/BottomButtonGroup/index.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/BottomButtonGroup/index.js
@@ -348,7 +348,7 @@ const BottomButtonGroup = React.memo(
         </ConditionalComponent>
 
         <div className="foi-bottom-button-group">
-          {urlIndexCreateRequest < 0 && && requestState?.toLowerCase() !== StateEnum.intakeinprogress.name.toLowerCase() &&
+          {urlIndexCreateRequest < 0 && requestState?.toLowerCase() !== StateEnum.intakeinprogress.name.toLowerCase() &&
             Object.entries(axisSyncedData)?.length !== 0 &&
             <button type="button" className="btn btn-bottom" 
             onClick={(e) => {
