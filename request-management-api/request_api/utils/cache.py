7907f25ffeb29ff733dbb8acbba621ddd4f7144d
sumathi-thirumani-aot <sumathi.thirumani@aot-technologies.com>
2022-03-30 19:04:03 -0700

    Exception handling for connection issues.

--- request-management-api/request_api/utils/cache.py
+++ request-management-api/request_api/utils/cache.py
@@ -14,14 +14,16 @@
 import os
 
 import request_api
+import redis
 
 class Config(object):
     ## type 'redis' is deprecated
-    CACHE_TYPE ='RedisCache'
+    CACHE_TYPE = 'RedisCache'
     
     CACHE_REDIS_HOST = os.getenv('FOI_REQUESTQUEUE_REDISHOST')
     CACHE_REDIS_PORT = os.getenv('FOI_REQUESTQUEUE_REDISPORT')
-    CACHE_REDIS_DB = 0
+    if os.getenv('CACHE_REDIS_DEFAULT_DB') == "Y":
+        CACHE_REDIS_DB =  0 
     CACHE_REDIS_URL = os.getenv('FOI_REQUESTQUEUE_REDISURL')
     CACHE_DEFAULT_TIMEOUT = os.getenv('CACHE_TIMEOUT')
     CACHE_KEY_PPREFIX = 'foi'
@@ -38,7 +40,7 @@ def cache_filter(*args, **kwargs):
         # Do a random get to the cache just to ping it and test its health. 
         # If redis is down exception will happen and will be caught in next line, otherwise the cache.get will just return None and will have no functional impact
         request_api.cache.get("")
-    except Exception:
+    except (redis.exceptions.ConnectionError, redis.exceptions.BusyLoadingError):
         return True
     return False
 
