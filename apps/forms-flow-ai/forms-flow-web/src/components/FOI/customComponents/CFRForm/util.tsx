21f1d723f7c3e86a50ed173a19099ddb837922d1
Richard Qi <richard.qi@aot-technologies.com>
2022-06-01 09:36:35 -0700

    simplify the code and remove debugging code

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/customComponents/CFRForm/util.tsx
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/customComponents/CFRForm/util.tsx
@@ -2,15 +2,13 @@ import foiFees from '../../../../constants/FOI/foiFees.json';
 import type { CFRFormData } from './types';
 
 export const calculateFees = (cfrForm: CFRFormData) => {
-  let k: keyof typeof cfrForm.estimates;
+  let newObj : any = {...cfrForm.estimates, ...cfrForm.actual};
+  let k: keyof typeof newObj;
+  let newFoiFees : any = foiFees;
   let totalFee = 0;
 
-  // console.log("calculateFees");
-  // console.log(foiFees);
-
-  for (k in cfrForm.estimates) {
-    let value = cfrForm.actual && cfrForm.actual[k] && cfrForm.actual[k] > 0 ? cfrForm.actual[k] : cfrForm.estimates[k];
-    totalFee += foiFees[k].type == "hour" ? calculateFeesByTime(k, value) : calculateFeesByPages(k, value);
+  for (k in newObj) {
+    totalFee += newFoiFees[k]["type"] == "hour" ? calculateFeesByTime(k, newObj[k]) : calculateFeesByPages(k, newObj[k]);
   }
 
   cfrForm.amountDue = totalFee;
@@ -19,15 +17,10 @@ export const calculateFees = (cfrForm: CFRFormData) => {
 };
 
 const calculateFeesByTime = (name: string, hours: number) => {
-  // console.log("calculateFeesByTime");
-  // console.log(name);
-  // console.log(hours);
-
   if(hours <= 0) return 0;
 
   if(foiFees[name as keyof typeof foiFees]) {
     let feeItem : any = foiFees[name as keyof typeof foiFees];
-    // console.log(feeItem);
     return (hours - feeItem["freehours"] <= 0) ? 0 : ((hours - feeItem["freehours"]) / feeItem["unit"]) * feeItem["price"];
   } else {
     return 0;
@@ -35,15 +28,10 @@ const calculateFeesByTime = (name: string, hours: number) => {
 }
 
 const calculateFeesByPages = (name: string, pages: number) => {
-  // console.log("calculateFeesByPages");
-  // console.log(name);
-  // console.log(pages);
-
   if(pages <= 0) return 0;
 
   if(foiFees[name as keyof typeof foiFees]) {
     let feeItem : any = foiFees[name as keyof typeof foiFees];
-    // console.log(feeItem);
     return (pages / feeItem["unit"]) * feeItem["price"];
   } else {
     return 0;
