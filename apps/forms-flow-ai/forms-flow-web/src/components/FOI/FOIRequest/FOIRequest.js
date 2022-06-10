7f242e54555404b033f1a13be7ab9a08b429b620
Aparna S <aparna.s@aot-technologies.com>
2022-06-10 12:39:08 -0700

    Removed fetching axis ids to FE and added check in BE

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
@@ -26,7 +26,6 @@ import {
 import {
   fetchFOIRequestDetailsWrapper,
   fetchFOIRequestDescriptionList,
-  fetchExistingAxisRequestIds,
   fetchRequestDataFromAxis
 } from "../../../apiManager/services/FOI/foiRequestServices";
 import {
@@ -175,7 +174,6 @@ const FOIRequest = React.memo(({ userDetail }) => {
     if (window.location.href.indexOf("comments") > -1) {
       tabclick("Comments");
     }
-    dispatch(fetchExistingAxisRequestIds());
   }, []);
 
   const dispatch = useDispatch();
