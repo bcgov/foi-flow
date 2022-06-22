9a16f3849c197ac3d002f27effda622432d060f5
unknown <nicholas.kan@aot-technologies.com>
2022-06-22 12:53:18 -0700

    fix code smell

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
@@ -738,19 +738,21 @@ const FOIRequest = React.memo(({ userDetail }) => {
                     }
                   >
                     <Breadcrumbs aria-label="breadcrumb" className="foi-breadcrumb">
-                      {showAdvancedSearch ? 
-                        (<Chip
+                      <ConditionalComponent condition={showAdvancedSearch}>
+                        <Chip
                           label={"Advanced Search"}
                           sx={{ backgroundColor: '#fff', border:'1px solid #038', color: '#038', height: 19, cursor: 'pointer' }}
                           onClick={() => dispatch(push(`/foi/dashboard`))}
-                        />) :
-                        (<Chip
+                        />
+                        <ConditionalComponent condition={!showAdvancedSearch}>
+                        </ConditionalComponent>
+                        <Chip
                           icon={<HomeIcon fontSize="small" sx={{color: '#038 !important'}}/>}
                           label={"Request Queue"}
                           sx={{ backgroundColor: '#fff', border:'1px solid #038', color: '#038', height: 19, cursor: 'pointer' }}
                           onClick={() => dispatch(push(`/foi/dashboard`))}
-                        />)
-                      }
+                        />
+                      </ConditionalComponent>
                       <Chip
                         label={headerText}
                         sx={{ backgroundColor: '#fff', border:'1px solid #038', color: '#038', height: 19 }}
