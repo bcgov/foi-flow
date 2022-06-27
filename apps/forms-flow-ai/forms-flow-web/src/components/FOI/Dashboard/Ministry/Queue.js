60d22b6fc3c3e7f5a74034c3fc72c86c5445db86
unknown <nicholas.kan@aot-technologies.com>
2022-06-27 13:48:27 -0700

    fix code smell

--- apps/forms-flow-ai/forms-flow-web/src/components/FOI/Dashboard/Ministry/Queue.js
+++ apps/forms-flow-ai/forms-flow-web/src/components/FOI/Dashboard/Ministry/Queue.js
@@ -48,7 +48,7 @@ const Queue = ({ userDetail, tableInfo }) => {
   const requestFilter = useSelector((state) => state.foiRequests.queueFilter);
 
   // update sortModel for records due, ldd & assignedTo
-  const updateSortModel = (sortModel) => {
+  const updateSortModel = () => {
     let smodel = JSON.parse(JSON.stringify(sortModel));
     if (smodel) {
       smodel.map((row) => {
@@ -72,7 +72,7 @@ const Queue = ({ userDetail, tableInfo }) => {
   };
 
   useEffect(() => {
-    serverSortModel = updateSortModel(sortModel);
+    serverSortModel = updateSortModel();
     // page+1 here, because initial page value is 0 for mui-data-grid
     dispatch(
       fetchFOIMinistryRequestListByPage(
