dce944f98b2a750445b0b55955835540c009fd19
Aparna S <aparna.s@aot-technologies.com>
2022-07-12 11:54:14 -0700

    template service

---
+++ request-management-api/request_api/services/external/templateservice.py
@@ -0,0 +1,25 @@
+from jinja2 import Template
+from request_api.services.external.storageservice import storageservice
+import json
+
+class templateservice:
+    """This class is reserved for jinja templating services integration.
+    """
+    
+   
+    def generatetemplate(self, emailtemplatename, dynamictemplatevalues):        
+        
+        print("Values::",dynamictemplatevalues)
+        emailtemplatehtml= storageservice().download(emailtemplatename)
+        if(emailtemplatehtml is None):
+            raise ValueError('No template found')
+
+        if(dynamictemplatevalues is None):
+            raise ValueError('Values not found')
+
+        template = Template(emailtemplatehtml)
+        dynamicvalues = dict(dynamictemplatevalues)
+        print(dynamicvalues)
+        templatedhtml = template.render(dynamicvalues)
+        print("<<<>>>",templatedhtml)
+        return templatedhtml
