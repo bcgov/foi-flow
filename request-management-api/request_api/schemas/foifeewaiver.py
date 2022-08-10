fcec0d294c4a90f5f04cf0a67d4074bc79d3a6ed
unknown <nicholas.kan@aot-technologies.com>
2022-08-10 11:17:43 -0700

    add missing field, add validation for required fields

--- request-management-api/request_api/schemas/foifeewaiver.py
+++ request-management-api/request_api/schemas/foifeewaiver.py
@@ -20,23 +20,23 @@ class FOIFeeWaiverPublicDataSchema(Schema):
     class Meta:  # pylint: disable=too-few-public-methods
         """Exclude unknown fields in the deserialized output."""
 
-    debate = fields.Bool(data_key="debate")
-    environment = fields.Bool(data_key="environment")
-    disclosing = fields.Bool(data_key="disclosing")
-    understanding = fields.Bool(data_key="understanding")
-    newpolicy = fields.Bool(data_key="newpolicy")
-    financing = fields.Bool(data_key="financing")
+    debate = fields.Bool(data_key="debate", required=True)
+    environment = fields.Bool(data_key="environment", required=True)
+    disclosing = fields.Bool(data_key="disclosing", required=True)
+    understanding = fields.Bool(data_key="understanding", required=True)
+    newpolicy = fields.Bool(data_key="newpolicy", required=True)
+    financing = fields.Bool(data_key="financing", required=True)
     other = fields.Str(data_key="other")
-    analysis = fields.Str(data_key="analysis", validate=validate.OneOf(['partial', 'yes', 'no'])) #partial, yes or no
+    analysis = fields.Str(data_key="analysis", validate=validate.OneOf(['partial', 'yes', 'no']), required=True) #partial, yes or no
     description = fields.Str(data_key="description")
 
 class FOIFeeWaiverRecommendationSchema(Schema):
     class Meta:  # pylint: disable=too-few-public-methods
         """Exclude unknown fields in the deserialized output."""
 
-    waive = fields.Str(data_key="waive", validate=validate.OneOf(['partial', 'yes', 'no'])) #partial, yes or no
-    summary = fields.Str(data_key="summary")
-    amount = fields.Float(data_key="amount")
+    waive = fields.Str(data_key="waive", validate=validate.OneOf(['partial', 'yes', 'no']), required=True) #partial, yes or no
+    summary = fields.Str(data_key="summary", required=True)
+    amount = fields.Float(data_key="amount", required=True)
 
 class FOIFeeWaiverDecisionSchema(Schema):
     class Meta:  # pylint: disable=too-few-public-methods
@@ -49,19 +49,20 @@ class FOIFeeWaiverFormDataSchema(Schema):
     class Meta:  # pylint: disable=too-few-public-methods
         """Exclude unknown fields in the deserialized output."""
 
-    requesteddate = fields.Str(data_key="requesteddate")
+    requesteddate = fields.Str(data_key="requesteddate", required=True)
     receiveddate = fields.Str(data_key="receiveddate")
-    summary = fields.Str(data_key="summary")
+    summary = fields.Str(data_key="summary", required=True)
     recordsdescription = fields.Str(data_key="recordsdescription")
-    type = fields.Str(data_key="type", validate=validate.OneOf(['public', 'inability']))
+    type = fields.Str(data_key="type", validate=validate.OneOf(['public', 'inability']), required=True)
     inability = fields.Nested(FOIFeeWaiverInabilityDataSchema)
     public = fields.Nested(FOIFeeWaiverPublicDataSchema)
-    disseminate = fields.Bool()
-    abletodisseminate = fields.Bool()
-    narrow = fields.Bool()
-    exceed = fields.Bool()
-    timelines = fields.Bool()
-    previous = fields.Bool()
+    disseminate = fields.Bool(required=True)
+    abletodisseminate = fields.Bool(required=True)
+    narrow = fields.Bool(required=True)
+    exceed = fields.Bool(required=True)
+    timelines = fields.Bool(required=True)
+    previous = fields.Bool(required=True)
+    reason = fields.Str()
     recommendation = fields.Nested(FOIFeeWaiverRecommendationSchema, allow_none=False)
 
 class FOIFeeWaiverMinistryFormDataSchema(Schema):
