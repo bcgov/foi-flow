c18f879ff4c7232cc5578828af445f8b16235e0e
Abin Antony <abin.antony@aot-technologies.com>
2022-08-11 11:49:43 -0700

    #2471 Ministry Request Comment Push update

--- request-management-api/request_api/resources/foirequest.py
+++ request-management-api/request_api/resources/foirequest.py
@@ -88,7 +88,10 @@ class FOIRequests(Resource):
             assignedtomiddlename = request_json["assignedToMiddleName"] if request_json.get("assignedToMiddleName") != None else None
             assignedtolastname = request_json["assignedToLastName"] if request_json.get("assignedToLastName") != None else None
             rawresult = rawrequestservice().saverawrequestversion(request_json,request_json['id'],assignedgroup,assignedto,"Archived",AuthHelper.getuserid(), assignedtofirstname,assignedtomiddlename,assignedtolastname)               
+
+
             eventservice().posteventsync(request_json['id'],"rawrequest",AuthHelper.getuserid(), AuthHelper.getusername(), AuthHelper.isministrymember())
+
             if rawresult.success == True:   
                 result = requestservice().saverequest(foirequestschema,AuthHelper.getuserid())
                 if result.success == True:
@@ -120,7 +123,7 @@ class FOIRequestsById(Resource):
             foirequestschema = FOIRequestWrapperSchema().load(request_json)                                    
             result = requestservice().saverequestversion(foirequestschema, foirequestid, foiministryrequestid,AuthHelper.getuserid())
             if result.success == True:
-                asyncio.ensure_future(eventservice().postevent(foiministryrequestid,"ministryrequest",AuthHelper.getuserid(),AuthHelper.getusername(),AuthHelper.isministrymember()))
+                asyncio.ensure_future((foiministryrequestid,"ministryrequest",AuthHelper.getuserid(),AuthHelper.getusername(),AuthHelper.isministrymember()))
                 metadata = json.dumps({"id": result.identifier, "ministries": result.args[0]})               
                 asyncio.ensure_future(requestservice().posteventtoworkflow(foiministryrequestid,  result.args[1], foirequestschema, json.loads(metadata),"iao"))
                 return {'status': result.success, 'message':result.message,'id':result.identifier, 'ministryRequests': result.args[0]} , 200
@@ -149,13 +152,24 @@ class FOIRequestsByIdAndType(Resource):
             if usertype != "ministry" and actiontype != "assignee":
                 return {'status': False, 'message':'Bad Request'}, 400
             request_json = request.get_json()
+            assigneename =''
             if actiontype == "assignee":
-                ministryrequestschema = FOIRequestAssigneeSchema().load(request_json)
+                ministryrequestschema = FOIRequestAssigneeSchema().load(request_json)                
+                if(usertype == "iao"):
+                    if(ministryrequestschema['assignedToFirstName'] != '' and ministryrequestschema['assignedToLastName'] != ''):
+                        assigneename = '{0}, {1}'.format(ministryrequestschema['assignedToLastName'],ministryrequestschema['assignedToFirstName'])
+                    elif (ministryrequestschema['assignedgroup'] != '' and ministryrequestschema['assignedToFirstName'] ==  '' and ministryrequestschema['assignedToLastName'] ==  ''):
+                        assigneename = ministryrequestschema['assignedgroup']
+                elif(usertype == "ministry"):
+                    if (ministryrequestschema['assignedministrypersonLastName'] != '' and ministryrequestschema['assignedministrypersonFirstName'] != '' ):
+                        assigneename = '{0}, {1}'.format(ministryrequestschema['assignedministrypersonLastName'],ministryrequestschema['assignedministrypersonFirstName'])    
+                    elif (ministryrequestschema['assignedministrygroup'] != ''  and ministryrequestschema['assignedministrypersonLastName'] == '' and ministryrequestschema['assignedministrypersonFirstName'] == ''):
+                        assigneename = ministryrequestschema['assignedministrygroup']               
             else:
                 ministryrequestschema = FOIRequestMinistrySchema().load(request_json)
             result = requestservice().saveministryrequestversion(ministryrequestschema, foirequestid, foiministryrequestid,AuthHelper.getuserid(), usertype)
             if result.success == True:
-                asyncio.ensure_future(eventservice().postevent(foiministryrequestid,"ministryrequest",AuthHelper.getuserid(),AuthHelper.getusername(),AuthHelper.isministrymember()))
+                asyncio.ensure_future(eventservice().postevent(foiministryrequestid,"ministryrequest",AuthHelper.getuserid(),AuthHelper.getusername(),AuthHelper.isministrymember(),assigneename))
                 metadata = json.dumps({"id": result.identifier, "ministries": result.args[0]})
                 asyncio.ensure_future(requestservice().posteventtoworkflow(foiministryrequestid, result.args[1], ministryrequestschema, json.loads(metadata),"ministry"))
                 return {'status': result.success, 'message':result.message,'id':result.identifier, 'ministryRequests': result.args[0]} , 200
@@ -167,7 +181,9 @@ class FOIRequestsByIdAndType(Resource):
             return {'status': False, 'message':err.messages}, 400
         except BusinessException as exception:            
             return {'status': exception.status_code, 'message':exception.message}, 500
-    
+        
+
+
 @cors_preflight('GET,POST,PUT,OPTIONS')
 @API.route('/foirequests/<int:foirequestid>')
 class FOIRequestUpdateById(Resource): 
