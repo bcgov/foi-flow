6302a1031507ea4cc5927f757db16448e4c0cfac
Abin Antony <abin.antony@aot-technologies.com>
2022-09-07 10:56:45 -0700

    #2687CorrespondenceTemplates migration script

---
+++ request-management-api/migrations/versions/30c5b9c88d33_.py
@@ -0,0 +1,34 @@
+"""empty message
+
+Revision ID: 30c5b9c88d33
+Revises: 2ccfbfe3a195
+Create Date: 2022-09-06 15:36:45.347950
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from datetime import datetime
+
+# revision identifiers, used by Alembic.
+revision = '30c5b9c88d33'
+down_revision = '2ccfbfe3a195'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('ApplicantCorrespondenceTemplates',
+    sa.Column('templateid', sa.Integer(), autoincrement=True, nullable=False),
+    sa.Column('documenturipath', sa.Text, nullable=False),
+    sa.Column('name', sa.String(length=500), nullable=False),
+    sa.Column('description', sa.String(length=1000) ),
+    sa.Column('active', sa.Boolean, nullable=False),
+    sa.Column('version', sa.Integer(), nullable=False),
+    sa.Column('created_at', sa.DateTime(), nullable=True, default=datetime.now()),
+    sa.Column('createdby', sa.String(length=120), nullable=True),       
+    sa.PrimaryKeyConstraint('templateid')
+    )
+
+
+def downgrade():
+    op.drop_table('ApplicantCorrespondenceTemplates')
