69f6b5e1cac10ed4a6b25f8bf69070f04cc3fda7
Aparna S <aparna.s@aot-technologies.com>
2022-09-08 12:27:13 -0700

    Query updated for deleting existing extension comment

--- request-management-api/request_api/services/events/extension.py
+++ request-management-api/request_api/services/events/extension.py
@@ -42,7 +42,6 @@ class extensionevent:
 
     def deleteexistingrelatedevents(self, ministryrequestid):
         try:
-            print("Inside deleteexistingrelatedevents")
             notificationids = FOIRequestNotification().getextensionnotificationidsbyministry(ministryrequestid)
             if notificationids:
                 self.__deleteaxisextensionnotifications(notificationids)
