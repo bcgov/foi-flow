706a4e46f071a2dca55bc9e4f2115df9bb8e6745
divyav-aot <divyav-aot@github>
2022-09-16 14:24:25 -0400

    reverted BE development run changes

--- request-management-api/entrypoint.sh
+++ request-management-api/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 
-[ ! -f .env ] || export $(grep -v '^#' .env | xargs)
-
-if [[ $FLASK_ENV="development" ]]
-then
-    python manage.py db && flask run -h 0.0.0.0 -p 5000
-else
-    python manage.py db upgrade && python wsgi.py
-fi
+python manage.py db upgrade && python wsgi.py
