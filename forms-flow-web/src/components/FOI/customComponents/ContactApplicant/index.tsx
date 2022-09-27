c9062f3a2681c7bdad6489431cfa48e03553146b
Richard Qi <richard.qi@aot-technologies.com>
2022-09-26 17:10:07 -0700

    big fox

--- forms-flow-web/src/components/FOI/customComponents/ContactApplicant/index.tsx
+++ forms-flow-web/src/components/FOI/customComponents/ContactApplicant/index.tsx
@@ -368,6 +368,7 @@ export const ContactApplicant = ({
             existingDocuments={files}
             attachment={{}}
             handleRename={undefined}
+            isMinistryCoordinator={false}
           />
         </div>
         <div id="correspondence-editor-ql-toolbar" className="ql-toolbar ql-snow">
