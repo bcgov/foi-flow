6ccec57001123d98ca0e59283d21b2542e11afc3
divyav-aot <divyav-aot@github>
2022-10-03 14:40:31 -0400

    migration script to update FOIReuqestTeam for Businessteam

---
+++ request-management-api/migrations/versions/c92673c110fe_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: c92673c110fe
+Revises: 7959ccaa7f9d
+Create Date: 2022-10-03 13:29:49.177590
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c92673c110fe'
+down_revision = '7959ccaa7f9d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute('UPDATE public."FOIRequestTeams" set teamid = (select teamid from public."OperatingTeams" where name = \'Central Team\') where teamid = (select teamid from public."OperatingTeams" where name = \'Business Team\') and programareaid in (select programareaid from public."ProgramAreas" where iaocode in (\'JER\', \'LBR\', \'MMA\', \'TAC\', \'TIC\', \'TRA\'));')
+
+def downgrade():    
+    op.execute('UPDATE public."FOIRequestTeams" set teamid = (select teamid from public."OperatingTeams" where name = \'Business Team\') where teamid = (select teamid from public."OperatingTeams" where name = \'Central Team\') and programareaid in (select programareaid from public."ProgramAreas" where iaocode in (\'JER\', \'LBR\', \'MMA\', \'TA\', \'TIC\', \'TRA\'));')
+    
+    
