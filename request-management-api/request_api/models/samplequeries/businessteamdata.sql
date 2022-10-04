1fbd3a8cee88ef21ef95b7aaf4ba7942b9905dfa
divyav-aot <divyav-aot@github>
2022-10-04 13:27:18 -0400

    query changed to update only the latest version of FOIMinistryRequests table

--- request-management-api/request_api/models/samplequeries/businessteamdata.sql
+++ request-management-api/request_api/models/samplequeries/businessteamdata.sql
@@ -3,14 +3,27 @@ startdate, duedate, assignedto, created_at, updated_at, createdby, updatedby, pr
 foirequest_id, foirequestversion_id, assignedgroup, assignedministryperson, assignedministrygroup, cfrduedate, closedate, closereasonid, axisrequestid, axissyncdate, requestpagecount
 	FROM public."FOIMinistryRequests" where assignedgroup = 'Business Team';
 
+--get latest version of all ministry ids where assignedgroup = 'Business Team'
+SELECT foiministryrequestid, version FROM
+public."FOIMinistryRequests" JOIN (
+  SELECT foiministryrequestid, MAX(version) version
+  FROM public."FOIMinistryRequests" where assignedgroup = 'Business Team'
+  GROUP BY foiministryrequestid
+  ) max_version USING (foiministryrequestid, version)
+
+
 select * from public."FOIRawRequestWatchers" where watchedbygroup = 'Business Team';
 select * from public."FOIRequestWatchers" where watchedbygroup = 'Business Team';
-select * from public."FOIRequestComments" where comment like '%Business Team%';
 
 
 
--- UPDATE public."FOIMinistryRequests" SET assignedgroup = 'Central Team' WHERE assignedgroup = 'Business Team';
--- UPDATE public."FOIRequestWatchers" SET watchedbygroup = 'Central Team' WHERE watchedbygroup = 'Business Team';
+-- only the latest version will get updated.
+UPDATE public."FOIMinistryRequests" ministryrequests SET assignedgroup = 'Central Team' FROM (
+  SELECT foiministryrequestid, MAX(version) version
+  FROM public."FOIMinistryRequests" where assignedgroup = 'Business Team'
+  GROUP BY foiministryrequestid
+  ) max_version WHERE ministryrequests.foiministryrequestid = max_version.foiministryrequestid and  ministryrequests.version = max_version.version
+UPDATE public."FOIRequestWatchers" SET watchedbygroup = 'Central Team' WHERE watchedbygroup = 'Business Team';
 
 
 --CAMUNDA DB ---
