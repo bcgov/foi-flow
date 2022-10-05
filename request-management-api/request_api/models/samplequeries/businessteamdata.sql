039d20f2d16994d944d565676b670eaf25d4b290
divyav-aot <divyav-aot@github>
2022-10-05 11:04:11 -0400

    update to the sample query

--- request-management-api/request_api/models/samplequeries/businessteamdata.sql
+++ request-management-api/request_api/models/samplequeries/businessteamdata.sql
@@ -1,15 +1,14 @@
-SELECT foiministryrequestid, version, isactive, filenumber, description, recordsearchfromdate, recordsearchtodate, 
-startdate, duedate, assignedto, created_at, updated_at, createdby, updatedby, programareaid, requeststatusid, 
-foirequest_id, foirequestversion_id, assignedgroup, assignedministryperson, assignedministrygroup, cfrduedate, closedate, closereasonid, axisrequestid, axissyncdate, requestpagecount
-	FROM public."FOIMinistryRequests" where assignedgroup = 'Business Team';
-
 --get latest version of all ministry ids where assignedgroup = 'Business Team'
 SELECT foiministryrequestid, version FROM
 public."FOIMinistryRequests" JOIN (
-  SELECT foiministryrequestid, MAX(version) version
+  SELECT foiministryrequestid, MAX(version) as version
   FROM public."FOIMinistryRequests" where assignedgroup = 'Business Team'
   GROUP BY foiministryrequestid
   ) max_version USING (foiministryrequestid, version);
+  
+
+select * from public."FOIMinistryRequests" where assignedgroup = 'Business Team' order by foiministryrequestid, version desc
+select assignedgroup,* from public."FOIMinistryRequests" where foiministryrequestid in (5214,5215,5216,5255,5257,5258,5260) order by foiministryrequestid, version desc 
 
 
 select * from public."FOIRawRequestWatchers" where watchedbygroup = 'Business Team';
@@ -19,12 +18,13 @@ select * from public."FOIRequestWatchers" where watchedbygroup = 'Business Team'
 
 -- only the latest version will get updated.
 UPDATE public."FOIMinistryRequests" ministryrequests SET assignedgroup = 'Central Team' FROM (
-  SELECT foiministryrequestid, MAX(version) version
+  SELECT foiministryrequestid, MAX(version) as version
   FROM public."FOIMinistryRequests" where assignedgroup = 'Business Team'
   GROUP BY foiministryrequestid
   ) max_version WHERE ministryrequests.foiministryrequestid = max_version.foiministryrequestid and  ministryrequests.version = max_version.version;
 
 UPDATE public."FOIRequestWatchers" SET watchedbygroup = 'Central Team' WHERE watchedbygroup = 'Business Team';
+UPDATE public."FOIRawRequestWatchers" SET watchedbygroup = 'Central Team' WHERE watchedbygroup = 'Business Team';
 
 
 --CAMUNDA DB ---
