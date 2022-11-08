1c961817aa689649918becf9e605c6eb914581d4
divyav-aot <79858022+divyav-aot@users.noreply.github.com>
2022-11-08 16:05:40 -0500

    null check added

--- apps/forms-flow-ai/forms-flow-ai-web/src/components/Form/Item/Submission/Item/PublicEdit.js
+++ apps/forms-flow-ai/forms-flow-ai-web/src/components/Form/Item/Submission/Item/PublicEdit.js
@@ -46,7 +46,7 @@ const PublicEdit = React.memo((props) => {
   return (
       <div className="container overflow-y-auto">
         <div className={`main-header ${classes.publicstyle}`}>
-          <SubmissionError modalOpen={errors.includes("Token Expired")}
+          <SubmissionError modalOpen={errors?.includes("Token Expired")}
             errors={errors}
             onConfirm={props.onConfirm}
           >
