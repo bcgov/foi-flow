bd4b5e7e37100daa546b7ccea129fb58df04dc72
Aparna S <aparna.s@aot-technologies.com>
2022-12-20 22:12:35 -0800

    Initial F.E commit

--- forms-flow-web/src/components/FOI/FOIRequest/FOIRequestHeader/index.js
+++ forms-flow-web/src/components/FOI/FOIRequest/FOIRequestHeader/index.js
@@ -17,6 +17,7 @@ import {
 } from "../../../../apiManager/services/FOI/foiAssigneeServices";
 import { toast } from "react-toastify";
 import _ from 'lodash';
+import RequestRestriction from "../../customComponents/RequestRestriction";
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
@@ -171,14 +172,51 @@ const FOIRequestHeader = React.memo(
     const ministryAssignedTo = getMinistryAssignedTo();
     const watcherList = assignedToList.filter(assignedTo => assignedTo.type === 'iao');
     return (
+      <>
       <div className="foi-request-review-header-row1">
-        <div className="foi-request-review-header-col1">
           <div className="foi-request-review-header-col1-row">
             <Link href="#" onClick={preventDefault}>
               <h3 className="foi-review-request-text">{headerText}</h3>
             </Link>
           </div>
-          {window.location.href.indexOf(FOI_COMPONENT_CONSTANTS.ADDREQUEST) ===
+          <div className="foi-assigned-to-container">
+            <div className="foi-assigned-to-inner-container">
+              <TextField
+                id="assignedTo"
+                label={showMinistryAssignedTo ? "IAO Assigned To" : "Assigned To"}
+                inputProps={{ "aria-labelledby": "assignedTo-label"}}
+                InputLabelProps={{ shrink: true }}
+                select
+                value={selectedAssignedTo}
+                onChange={saveAssigneeDetails}
+                input={<Input />}
+                variant="outlined"
+                fullWidth
+                required
+                disabled={disableInput}
+                error={selectedAssignedTo.toLowerCase().includes("unassigned")}
+              >
+                {menuItems}
+              </TextField>
+            </div>
+
+            {showMinistryAssignedTo && (
+              <>
+              <TextField
+                  id="ministryAssignedTotxt"
+                  label="Ministry Assigned To"
+                  InputLabelProps={{ shrink: true }}
+                  value={ministryAssignedTo}
+                  variant="outlined"
+                  fullWidth
+                  disabled={true}
+                />
+              </>
+            )}
+          </div>
+      </div>
+      <div className="foi-request-review-header-row1">
+        {window.location.href.indexOf(FOI_COMPONENT_CONSTANTS.ADDREQUEST) ===
             -1 && (
             <div
               className="foi-request-review-header-col1-row"
@@ -193,44 +231,12 @@ const FOIRequestHeader = React.memo(
               />
             </div>
           )}
-        </div>
-
-        <div className="foi-assigned-to-container">
-          <div className="foi-assigned-to-inner-container">
-            <TextField
-              id="assignedTo"
-              label={showMinistryAssignedTo ? "IAO Assigned To" : "Assigned To"}
-              inputProps={{ "aria-labelledby": "assignedTo-label"}}
-              InputLabelProps={{ shrink: true }}
-              select
-              value={selectedAssignedTo}
-              onChange={saveAssigneeDetails}
-              input={<Input />}
-              variant="outlined"
-              fullWidth
-              required
-              disabled={disableInput}
-              error={selectedAssignedTo.toLowerCase().includes("unassigned")}
-            >
-              {menuItems}
-            </TextField>
-          </div>
-
-          {showMinistryAssignedTo && (
-            <>
-            <TextField
-                id="ministryAssignedTotxt"
-                label="Ministry Assigned To"
-                InputLabelProps={{ shrink: true }}
-                value={ministryAssignedTo}
-                variant="outlined"
-                fullWidth
-                disabled={true}
-              />
-            </>
-          )}
-        </div>
+        {!isAddRequest && requestDetails?.currentState?.toLowerCase() !== 'unopened' &&
+         <RequestRestriction 
+          isiaorestricted= {requestDetails?.isiaorestricted} />
+        }
       </div>
+      </>
     );
   }
 );
---
+++ forms-flow-web/src/components/FOI/customComponents/RequestRestriction.js
@@ -0,0 +1,161 @@
+import React, { useEffect, useState }  from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFlag } from '@fortawesome/free-regular-svg-icons'; 
+import { faFlag  as faSolidFlag} from '@fortawesome/free-solid-svg-icons'; 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from '@mui/material/TextField';
+import './requestrestriction.scss';
+
+
+const RequestRestriction= ({isiaorestricted}) =>{ 
+
+    const [restrictionType, setRestrictionType] = useState("unrestricted");
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalMessage, setModalMessage] = useState(<></>);    
+    const [modalDescription, setModalDescription] = useState(<></>);
+
+    useEffect(() => {
+        if(isiaorestricted)
+            setRestrictionType("restricted");
+        else
+            setRestrictionType("unrestricted");
+    }, [isiaorestricted]);
+
+
+    const restriction = [
+        {
+          value: 'unrestricted',
+          label: 'Unrestricted',
+          disabled: false // To Do : Add condition
+        },
+        {
+          value: 'restricted',
+          label: 'Restricted',
+          disabled: false,
+        }
+    ];
+
+    const handleValueChange = (e) => {
+        setRestrictionType(e.target.value);
+        if(e.target.value?.toLowerCase() == 'restricted'){
+            console.log("Restricted!!");
+            setModalOpen(true);
+            setModalMessage("Are you sure you want to flag this as a restricted file ?");
+            setModalDescription("If you change this to be a restrcited file only the Intake Manager and any user assigned or selected as watchers will be able to view this request content.");
+        }
+        else {
+            setModalOpen(true);
+            setModalMessage("Are you sure you want to remove the restricted file flag on this request ?");
+            setModalDescription("If you restrcit this file only all IAO users will be able to search and find the request, and all users "+
+            "on the respective Ministry will be able to see this request.");
+        }
+        // setModalMessage("Only the Intake Manager can remove the restricted flag on a request ?");
+        // setModalDescription("If you would like to have this request restricted please contact the Intake Manager"+
+        // " as they are the original user who flagged this as a restricted request.");
+        // setModalMessage("A request can only be restricted when it is assigned to one team member, not a team queue.");
+    }
+
+    const handleSave = () => {
+        setModalOpen(false);
+        //save();
+    };
+
+    const handleClose = () => {
+        setModalOpen(false);
+    };
+
+
+
+    return (  
+        <>
+        <div className="requestRestriction">
+            <div className="restrict-dropdown-all">
+                <div className="restrict-select" style={{background: restrictionType == 'restricted'? 'linear-gradient(to right, rgba(160,25,47,0.32) 80%, #a0192f 0%)' : 'linear-gradient(to right, #fff 80%, #a0192f 0%)'}}>
+                    { restrictionType?.toLowerCase() == 'restricted'?
+                        <FontAwesomeIcon icon={faSolidFlag} size='2x' className='restrict-icon' />:
+                        <FontAwesomeIcon icon={faFlag} size='2x' className='restrict-icon' />      
+                    }       
+                    {/* <InputLabel id="restrict-dropdown-label">
+                    Unrestricted
+                    </InputLabel> */}
+                    <TextField
+                    id="restrict-dropdown"
+                    className="restrict-dropdown"
+                    select
+                    value={restrictionType}
+                    onChange={handleValueChange}
+                    inputProps={{'aria-labelledby': 'restrict-dropdown-label'}}
+                    input={<OutlinedInput label="Tag" />}
+                    disabled = {false}
+                    >
+                    {restriction.map((option) => (
+                        <MenuItem
+                          key={option.value}
+                          value={option.value}
+                          disabled={option.disabled}
+                        >
+                          {option.label}
+                        </MenuItem>
+                        ))}
+                    </TextField>
+                </div>
+            </div>
+        </div> 
+        <div className="state-change-dialog">
+            <Dialog
+            open={modalOpen}
+            onClose={handleClose}
+            aria-labelledby="state-change-dialog-title"
+            aria-describedby="restricted-modal-text"
+            maxWidth={'md'}
+            fullWidth={true}
+            // id="state-change-dialog"
+            >
+            <DialogTitle disableTypography id="state-change-dialog-title">
+                <h2 className="state-change-header">Restricted File</h2>
+                <IconButton className="title-col3" onClick={handleClose}>
+                    <i className="dialog-close-button">Close</i>
+                    <CloseIcon />
+                </IconButton>
+                </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="restricted-modal-text" component={'span'}>
+                <div className="modal-msg">
+                    <div className="confirmation-message">
+                        {modalMessage}
+                    </div>
+                    <div className='modal-msg-description'>
+                        <i>{modalDescription}</i>
+                    </div>
+                </div>
+            </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <button
+                className={`btn-bottom btn-save btn`}
+                onClick={handleSave}
+                >
+                Save Change
+                </button>
+                <button className="btn-bottom btn-cancel" onClick={handleClose}>
+                Cancel
+                </button>
+            </DialogActions>
+            </Dialog>
+        </div>
+        </>
+    );
+};
+
+export default RequestRestriction;
