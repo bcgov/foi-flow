43d0766fb16bcabb41c27e06d7c4198ee70a1bdd
unknown <nicholas.kan@aot-technologies.com>
2023-01-23 16:31:01 -0800

    add missing fields to schema to be saved to reviewer db

--- request-management-api/request_api/schemas/foirecord.py
+++ request-management-api/request_api/schemas/foirecord.py
@@ -54,6 +54,8 @@ class RetryRecordAttributeSchema(Schema):
     lastmodified = fields.Str(data_key="lastmodified",allow_none=False, validate=[validate.Length(max=120, error=MAX_EXCEPTION_MESSAGE)])
     filesize = fields.Int(data_key="filesize", allow_none=False)
     batch = fields.Str(data_key="batch", allow_none=False, validate=validate.Length(min=1), required=True)
+    incompatible = fields.Boolean(required=True,allow_none=False)
+    extension = fields.Str(validate=validate.Length(min=1, max=10), required=True,allow_none=False)
 
 class FOIRequestRetryRecordSchema(Schema):
     class Meta:  # pylint: disable=too-few-public-methods
