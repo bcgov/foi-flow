9834a0cd7f99d3e18e625c778a3e5cd0d296b1a7
leodube-aot <leo.dube@aot-technologies.com>
2023-02-16 16:48:47 -0800

    Scaffold admin page

---
+++ forms-flow-web/src/components/FOI/Admin/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./admin.scss";
+
+const Admin = React.memo(() => {
+    return (
+        <div className="admin-page">
+            <p>Test Admin</p>
+        </div>
+    );
+});
+
+export default Admin;
