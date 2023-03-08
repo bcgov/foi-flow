a9ff40f0cb290733c9a50cc93e7b714ce0d4da1d
nimya-aot <nimya.john@aot-technologies.com>
2023-03-08 12:46:02 -0800

    updated, saved fixed conflits

--- testing/foi-qa-automation/Object Repository/Page_foi.flow/form/watch/button_Watch.rs
+++ testing/foi-qa-automation/Object Repository/Page_foi.flow/form/watch/button_Watch.rs
@@ -30,11 +30,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>button</value>
-<<<<<<< HEAD
       <webElementGuid>ac0da7fb-62a6-46b5-8466-757670270202</webElementGuid>
-=======
-      <webElementGuid>01d8852f-9ac4-49b4-835f-279458a7e0e7</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -42,11 +38,7 @@
       <name>class</name>
       <type>Main</type>
       <value>foi-eye-container</value>
-<<<<<<< HEAD
       <webElementGuid>c65e0bbf-fb30-4882-b882-d1601aea913c</webElementGuid>
-=======
-      <webElementGuid>109fba45-da0a-4934-aebb-3a21c0bfaf40</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -54,63 +46,39 @@
       <name>text</name>
       <type>Main</type>
       <value>  Watch</value>
-<<<<<<< HEAD
       <webElementGuid>ba4f7927-629e-4ccd-86a0-cf3044930e08</webElementGuid>
-=======
-      <webElementGuid>caec4bff-48df-4be5-b89e-96dcfe4a482f</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-<<<<<<< HEAD
       <value>id(&quot;Request&quot;)/div[@class=&quot;container foi-review-request-container&quot;]/div[@class=&quot;foi-review-container&quot;]/form[@class=&quot;jss142 foi-request-form&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-lg-8&quot;]/div[@class=&quot;foi-request-review-header-col1-row&quot;]/div[@class=&quot;foi-request-review-header-col1-row&quot;]/div[1]/div[@class=&quot;foi-watcher-all&quot;]/button[@class=&quot;foi-eye-container&quot;]</value>
       <webElementGuid>139a735e-558f-4b75-a7f2-7271526a78f4</webElementGuid>
-=======
-      <value>id(&quot;Request&quot;)/div[@class=&quot;container foi-review-request-container&quot;]/div[@class=&quot;foi-review-container&quot;]/form[@class=&quot;jss5 foi-request-form&quot;]/div[@class=&quot;foi-request-review-header-row1&quot;]/div[@class=&quot;foi-request-review-header-col1&quot;]/div[@class=&quot;foi-request-review-header-col1-row&quot;]/div[1]/div[@class=&quot;foi-watcher-all&quot;]/button[@class=&quot;foi-eye-container&quot;]</value>
-      <webElementGuid>87835955-3c8d-4154-9def-9d57be60ccbe</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:idRelative</name>
       <type>Main</type>
-<<<<<<< HEAD
       <value>//div[@id='Request']/div/div/form/div[2]/div/div/div/div/div/button</value>
       <webElementGuid>4bfdc2b1-b997-4f6c-9633-a996883148fb</webElementGuid>
-=======
-      <value>//div[@id='Request']/div/div/form/div/div/div[2]/div/div/button</value>
-      <webElementGuid>1ace6571-6e5f-4046-acb3-dabf499972ef</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-<<<<<<< HEAD
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Intake, FOI'])[1]/following::button[1]</value>
       <webElementGuid>7c23d579-ed8d-4099-b848-df41a57e7f18</webElementGuid>
-=======
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='Review Request'])[1]/following::button[1]</value>
-      <webElementGuid>a712b29f-8291-484f-a012-325bc11b9632</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-<<<<<<< HEAD
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Watchers'])[1]/preceding::button[1]</value>
       <webElementGuid>6a38e9f9-8ba9-49e6-bdae-b51138e88724</webElementGuid>
-=======
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='Option 4'])[1]/following::button[1]</value>
-      <webElementGuid>90356f03-2b3b-44a6-9d31-cbfc4f747e44</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -118,11 +86,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Tag'])[1]/preceding::button[1]</value>
-<<<<<<< HEAD
       <webElementGuid>ea46645a-067a-455b-8365-22d48074858b</webElementGuid>
-=======
-      <webElementGuid>2019895f-c4f3-4c20-ba06-c78e75dd37fd</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -130,18 +94,13 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>//*/text()[normalize-space(.)='Watch']/parent::*</value>
-<<<<<<< HEAD
       <webElementGuid>a4f63af2-4ca0-4182-9d71-0689149f1985</webElementGuid>
-=======
-      <webElementGuid>3c601ab4-2bcc-4b02-b34a-94a01fe69e98</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:position</name>
       <type>Main</type>
-<<<<<<< HEAD
       <value>//form/div[2]/div/div/div/div/div/button</value>
       <webElementGuid>35f8671e-1f91-4950-998d-3ffd13a2dffc</webElementGuid>
    </webElementXpaths>
@@ -152,9 +111,5 @@
       <type>Main</type>
       <value>//button[(text() = '  Watch' or . = '  Watch')]</value>
       <webElementGuid>c31ae8c0-9a0f-45d4-b16e-64b64cec74a9</webElementGuid>
-=======
-      <value>//div[2]/div/div/button</value>
-      <webElementGuid>3ab7ff4b-cf9f-425b-9499-6e43a4e5b43b</webElementGuid>
->>>>>>> dev-automationscripts
    </webElementXpaths>
 </WebElementEntity>
