509fec0a476cb3bc7d7ddc6a660a66ed56f7c09c
leodube-aot <leo.dube@aot-technologies.com>
2023-03-29 09:34:23 -0700

    Implement create, edit, and disable division in web services

--- forms-flow-web/src/apiManager/services/FOI/foiAdminServices.js
+++ forms-flow-web/src/apiManager/services/FOI/foiAdminServices.js
@@ -10,11 +10,11 @@ import {
   setFOIProgramAreaDivisionsList,
 } from "../../../actions/FOI/foiRequestActions";
 import { replaceUrl, errorToast } from "../../../helper/FOI/helper";
-import { catchError } from './foiServicesUtil';
+import { catchError } from "./foiServicesUtil";
 
 export const fetchProgramAreaDivisions = () => {
   return (dispatch) => {
-    httpGETRequest(API.FOI_GET_PROGRAMAREADIVISIONS_API)
+    httpGETRequest(API.FOI_GET_PROGRAMAREADIVISIONS)
       .then((res) => {
         if (res.data) {
           dispatch(setFOIProgramAreaDivisionsList(res.data));
@@ -26,7 +26,74 @@ export const fetchProgramAreaDivisions = () => {
         }
       })
       .catch((error) => {
-        catchError(error, dispatch)
+        catchError(error, dispatch);
+      });
+  };
+};
+
+export const createProgramAreaDivision = (data) => {
+  return (dispatch) => {
+    httpPOSTRequest(API.FOI_POST_PROGRAMAREADIVISION, data)
+      .then((res) => {
+        dispatch(fetchProgramAreaDivisions());
+        if (res.data) {
+          console.log("Create program area division successfully!");
+        } else {
+          dispatch(serviceActionError(res));
+          console.log("Error creating program area division");
+        }
       })
-  }
-}
+      .catch((error) => {
+        dispatch(serviceActionError(error));
+        console.log("Error creating program area division");
+      });
+  };
+};
+
+export const editProgramAreaDivision = (data, divisionid) => {
+  let apiUrl = replaceUrl(
+    replaceUrl(API.FOI_PUT_PROGRAMAREADIVISIONS),
+    "<divisionid>",
+    divisionid
+  );
+  return (dispatch) => {
+    httpPUTRequest(apiUrl, data)
+      .then((res) => {
+        dispatch(fetchProgramAreaDivisions());
+        if (res.data) {
+          console.log("Edit program area division successfully!");
+        } else {
+          dispatch(serviceActionError(res));
+          console.log("Error editing program area division");
+        }
+      })
+      .catch((error) => {
+        dispatch(serviceActionError(error));
+        console.log("Error editing program area division");
+      });
+  };
+};
+
+export const disableProgramAreaDivision = (divisionid) => {
+  let apiUrl = replaceUrl(
+    replaceUrl(API.FOI_DELETE_PROGRAMAREADIVISIONS),
+    "<divisionid>",
+    divisionid
+  );
+  return (dispatch) => {
+    httpPUTRequest(apiUrl)
+      .then((res) => {
+        dispatch(fetchProgramAreaDivisions());
+        if (res.data) {
+          console.log("Disable program area division successfully!");
+        } else {
+          dispatch(serviceActionError(res));
+          console.log("Error disabling program area division");
+        }
+      })
+      .catch((error) => {
+        dispatch(serviceActionError(error));
+        console.log("Error disabling program area division");
+      });
+  };
+};
