649da25c94ad21d5132c0cd8af9c244df0bc75b5
sumathi.thirumani <sumathi.thirumani@aot-technologies.com>
2023-04-10 10:41:45 -0700

    Comment print statement

--- request-management-api/request_api/resources/foirecord.py
+++ request-management-api/request_api/resources/foirecord.py
@@ -191,7 +191,7 @@ class FOIRequestRecordsChanged(Resource):
     def get(requestid, ministryrequestid, recordstype):
         try:
             result = recordservice().isrecordschanged(ministryrequestid, recordstype.lower())
-            print("records changed == ", result)
+            #print("records changed == ", result)
             return result, 200
         except KeyError as err:
             print("KeyError == ", err.messages)
