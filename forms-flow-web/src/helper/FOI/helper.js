cf189e7d30a32032dc3eee36f505145bc6a5e9bb
Richard Qi <richard.qi@aot-technologies.com>
2023-05-11 11:16:25 -0700

    add missing function

--- forms-flow-web/src/helper/FOI/helper.js
+++ forms-flow-web/src/helper/FOI/helper.js
@@ -438,6 +438,22 @@ const isrecordtimeout = (createDate, days) => {
   return diffhrs >= days;
 };
 
+const readUploadedFileAsBytes = (inputFile) => {
+  const temporaryFileReader = new FileReader();
+
+  return new Promise((resolve, reject) => {
+    temporaryFileReader.onerror = () => {
+      temporaryFileReader.abort();
+      reject(new DOMException("Problem parsing input file."));
+    };
+
+    temporaryFileReader.onload = () => {
+      resolve(temporaryFileReader.result);
+    };
+    temporaryFileReader.readAsArrayBuffer(inputFile);
+  });
+};
+
 export {
   replaceUrl,
   formatDate,
@@ -469,5 +485,6 @@ export {
   isRequestMinistryRestricted,
   getMinistryRestrictedTagList,
   isrecordtimeout,
-  isFoiAdmin
+  isFoiAdmin,
+  readUploadedFileAsBytes
 };
