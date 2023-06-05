6ecfa075c2ba4828891fad64bfaab429005fa8a7
divyav-aot <divyav-aot@github>
2023-06-05 12:50:33 -0400

    download harms attachment sort

--- forms-flow-web/src/components/FOI/customComponents/Records/util.js
+++ forms-flow-web/src/components/FOI/customComponents/Records/util.js
@@ -1,6 +1,73 @@
-export const removeDuplicateFiles = (recordList) => {
-    return recordList.filter(record => !record.isduplicate);    
-}
+//Remove duplicate records
+export const removeDuplicateFiles = (recordList) => (
+    recordList.filter(({isduplicate}) => !isduplicate)
+  );
+  
+  // Helper function to sort files by lastmodified date
+export const sortByLastModified = (files) => (
+    files.sort((a, b) => new Date(a.lastmodified) - new Date(b.lastmodified))
+  );
+  
+export const getPDFFilePath = (item) => {
+ 
+    let pdffilepath = item.s3uripath
+    let pdffilename = item.filename
+ 
+    if (item.isredactionready && ['.doc','.docx','.xls','.xlsx', '.ics', '.msg'].includes(item.attributes?.extension?.toLowerCase())) {
+        pdffilepath = pdffilepath.substr(0, pdffilepath.lastIndexOf(".")) + ".pdf";
+        pdffilename += ".pdf";
+    }
+    return [pdffilepath, pdffilename];
+ }
+ 
+ // Get records with only necessary fields
+ export const getUpdatedRecords = (_records, isattachment=false) =>{
+    return _records.map(_record => {
+        const [filepath, filename] = getPDFFilePath(_record)
+        const deduplicatedAttachments = _record?.attachments?.length > 0 ? removeDuplicateFiles(_record.attachments): []
+        const _recordObj =
+            {
+                recordid: _record.recordid,
+                filename: filename,
+                s3uripath: filepath,
+                filesize: _record.attributes.convertedfilesize || _record.attributes.filesize,
+                lastmodified: _record.attributes.lastmodified,
+                isduplicate: _record.isduplicate,
+                divisions: _record.attributes.divisions,
+                divisionids: _record.attributes.divisions.map(d => d.divisionid),
+                attachments: !isattachment ? sortByLastModified(getUpdatedRecords(deduplicatedAttachments, true)) : undefined
+            }
+        return _recordObj
+        
+    })
+ }
+ 
+ //Get files for specified divisionid with necessary fields
+ export const getFiles = (_records, _divisionid) => {
+     return _records.filter(_record => _record.divisionids.includes(_divisionid)).map(r => {
+         return ({
+             recordid: r.recordid,
+             filename: r.filename,
+             s3uripath: r.s3uripath,
+             filesize: r.filesize,
+             lastmodified: r.lastmodified
+         })
+     })
+ }
+ 
+ // calculate divisional total file size
+ export const calculateDivisionFileSize = (_files) => {
+     return _files.reduce((total, _file) => {
+         return total + +_file.filesize;
+       }, 0);
+ }
+ 
+ // calculate final file size
+export const calculateTotalFileSize = (divisions) => {
+     return divisions.reduce((total, division) => {
+       return total + division.divisionfilesize;
+     }, 0);
+   }
 
 export const addDeduplicatedAttachmentsToRecords = (exporting) => {
     for (let record of exporting) {
@@ -11,17 +78,6 @@ export const addDeduplicatedAttachmentsToRecords = (exporting) => {
     return exporting;
 }
 
-export const getPDFFilePath = (item) => {
-
-    let filepath = item.s3uripath
-    let filename = item.filename
-
-    if (item.isredactionready && ['.doc','.docx','.xls','.xlsx', '.ics', '.msg'].includes(item.attributes?.extension?.toLowerCase())) {
-        filepath = filepath.substr(0, filepath.lastIndexOf(".")) + ".pdf";
-        filename += ".pdf";
-    }
-    return [filepath, filename];
-}
 
 export const sortDivisionalFiles = (divisionMap) => {
     return Array.from(divisionMap.values()).map(({ divisionid, divisionname, files, divisionfilesize }) => ({
@@ -32,12 +88,6 @@ export const sortDivisionalFiles = (divisionMap) => {
     }));
 }
 
-
-export const calculateTotalFileSize = (divisions) => {
-    return divisions.reduce((total, division) => {
-      return total + division.divisionfilesize;
-    }, 0);
-  }
 
 export const calculateTotalUploadedFileSizeInKB = (records) => {
     return records.reduce((total, record) => {
