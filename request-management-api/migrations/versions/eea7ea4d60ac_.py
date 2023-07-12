e0fafe0dc6601049cf2f5e4ef2e83d09462b257d
divyav-aot <divyav-aot@github>
2023-07-12 18:10:42 -0400

    migration script added

---
+++ request-management-api/migrations/versions/eea7ea4d60ac_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: eea7ea4d60ac
+Revises: d64df04da9df
+Create Date: 2023-07-12 18:09:59.244266
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eea7ea4d60ac'
+down_revision = 'd64df04da9df'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
