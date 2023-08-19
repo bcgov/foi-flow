cd53e0021af6434d80f06ea536b68f18cba93248
Abin Antony <abin.antony@aot-technologies.com>
2023-08-18 17:10:37 -0700

    #4141 appsettting for main app.

--- datamigrations/FOIMOD.CFD.ConsoleApp.DocMigration/FOIMOD.CFD.ConsoleApp.DocMigration/Program.cs
+++ datamigrations/FOIMOD.CFD.ConsoleApp.DocMigration/FOIMOD.CFD.ConsoleApp.DocMigration/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using FOIMOD.CFD.DocMigration.Models;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Starting, CFD Document Migration!");
+var configurationbuilder = new ConfigurationBuilder()
+                       .AddJsonFile($"appsettings.json", true, true)
+                       .AddEnvironmentVariables().Build();
+
+SystemSettings.S3_AccessKey = configurationbuilder.GetSection("S3Configuration:AWS_accesskey").Value;
+SystemSettings.S3_SecretKey = configurationbuilder.GetSection("S3Configuration:AWS_secret").Value;
+SystemSettings.S3_EndPoint = configurationbuilder.GetSection("S3Configuration:AWS_S3_Url").Value;
+SystemSettings.AXISConnectionString = configurationbuilder.GetSection("AXISConfiguration:SQLConnectionString").Value;
+SystemSettings.RequestToMigrate = configurationbuilder.GetSection("AXISConfiguration:RequestToMigrate").Value;
