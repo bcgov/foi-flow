db1e5f8794cdfd6581626b43b1a934ad16bfe9f3
Aman-Hundal <aman.hundal@gov.bc.ca>
2023-09-13 17:19:43 -0700

    Fixed sorting bug in get division stages. there was a bug if programareadivisions returned if some divisions had a sort order and others sort order was none, this lead to a type error in the sort function.

--- request-management-api/request_api/services/divisionstageservice.py
+++ request-management-api/request_api/services/divisionstageservice.py
@@ -9,7 +9,7 @@ class divisionstageservice:
         divisionstages = []
         programarea = ProgramArea.getprogramarea(bcgovcode)
         divisions = ProgramAreaDivision.getprogramareadivisions(programarea['programareaid'])
-        divisions.sort(key=lambda item: (item['sortorder'], item['name']))        
+        divisions.sort(key=lambda item: (item["sortorder"] is None, item["name"]))        
         for division in divisions:
             divisionstages.append({"divisionid": division['divisionid'], "name": self.escapestr(division['name'])})
         return {"divisions": divisionstages, "stages": self.getstages()}
