0752fff7090e8a6797f973083ce8ad2252b44eab
Aman-Hundal <aman.hundal@gov.bc.ca>
2023-09-27 12:59:12 -0700

    Testing of state changes with Section 5 Pending completed. Adjust migraiton file as well too build on top of ticket 4368

--- request-management-api/migrations/versions/a79cd809e85e_.py
+++ request-management-api/migrations/versions/a79cd809e85e_.py
@@ -1,7 +1,7 @@
 """Adding Section 5 Pending FOI Status to FOIRequestStatuses Table
 
 Revision ID: a79cd809e85e
-Revises: aacdbca19a47
+Revises: 1491b3126887
 Create Date: 2023-09-25 14:37:29.208839
 
 """
@@ -10,15 +10,15 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'a79cd809e85e'
-down_revision = 'aacdbca19a47'
+down_revision = '1491b3126887'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.execute('INSERT INTO public."FOIRequestStatuses"(requeststatusid, name, description, isactive) VALUES (19, \'Section 5 Pending\', \'Section 5 Pending (Personal)\', true);commit;')
+    op.execute('INSERT INTO public."FOIRequestStatuses"(requeststatusid, name, description, isactive) VALUES (20, \'Section 5 Pending\', \'Section 5 Pending (Personal)\', true);commit;')
     
 
 
 def downgrade():
-    op.execute('DELETE FROM public."FOIRequestStatuses" WHERE requeststatusid = 19;commit;')
+    op.execute('DELETE FROM public."FOIRequestStatuses" WHERE requeststatusid = 20;commit;')
