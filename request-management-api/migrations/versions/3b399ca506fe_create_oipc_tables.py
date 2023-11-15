92e416b92498e9032370597eab6c725f1f9b2ace
Milos Despotovic <milosmdes@gmail.com>
2023-11-15 12:24:12 -0800

    Update migration file

--- request-management-api/migrations/versions/3b399ca506fe_create_oipc_tables.py
+++ request-management-api/migrations/versions/3b399ca506fe_create_oipc_tables.py
@@ -20,56 +20,90 @@ depends_on = None
 
 
 def upgrade():
-    subjectcodestable = op.create_table('OIPCReviewTypes',
+    reviewtypestable = op.create_table('OIPCReviewTypes',
     sa.Column('reviewtypeid', sa.Integer(), autoincrement=True, nullable=False),
-    sa.Column('created_at', sa.DateTime(), nullable=True),
-    sa.Column('createdby', sa.String(length=120), nullable=True),
-    sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.Column('updatedby', sa.String(length=120), nullable=True),
-    sa.Column('isactive', sa.Boolean(), nullable=False),
+    sa.Column('name', sa.String(length=100), unique=False, nullable=False),
+    sa.Column('isactive', sa.Boolean(), unique=False, nullable=False),
     sa.PrimaryKeyConstraint('reviewtypeid')
     )
 
     reasonstable = op.create_table('OIPCReasons',
     sa.Column('reasonid', sa.Integer(), autoincrement=True, nullable=False),
-    sa.Column('created_at', sa.DateTime(), nullable=True),
-    sa.Column('createdby', sa.String(length=120), nullable=True),
-    sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.Column('updatedby', sa.String(length=120), nullable=True),
-    sa.Column('isactive', sa.Boolean(), nullable=False),
+    sa.Column('name', sa.String(length=100), unique=False, nullable=False),
+    sa.Column('isactive', sa.Boolean(), unique=False, nullable=False),
     sa.PrimaryKeyConstraint('reasonid')
     )
 
     statusestable = op.create_table('OIPCStatuses',
     sa.Column('statusid', sa.Integer(), autoincrement=True, nullable=False),
-    sa.Column('created_at', sa.DateTime(), nullable=True),
-    sa.Column('createdby', sa.String(length=120), nullable=True),
-    sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.Column('updatedby', sa.String(length=120), nullable=True),
-    sa.Column('isactive', sa.Boolean(), nullable=False),
+    sa.Column('name', sa.String(length=100), unique=False, nullable=False),
+    sa.Column('isactive', sa.Boolean(), unique=False, nullable=False),
     sa.PrimaryKeyConstraint('statusid')
     )
 
     outcomestable = op.create_table('OIPCOutcomes',
     sa.Column('outcomeid', sa.Integer(), autoincrement=True, nullable=False),
-    sa.Column('created_at', sa.DateTime(), nullable=True),
-    sa.Column('createdby', sa.String(length=120), nullable=True),
-    sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.Column('updatedby', sa.String(length=120), nullable=True),
-    sa.Column('isactive', sa.Boolean(), nullable=False),
+    sa.Column('name', sa.String(length=100), unique=False, nullable=False),
+    sa.Column('isactive', sa.Boolean(), unique=False, nullable=False),
     sa.PrimaryKeyConstraint('outcomeid')
     )
-    # op.execute('''CREATE TABLE OIPCReviewTypes (
-    #            reviewtypeid integer PRIMARY KEY,
-    #            created_at datetime,
-    #            created_by varchar(120),
-    #            updated_at datetime,
-    #            updated_by varchar(120),
-    #            isactive boolean NOT NULL
-    # )''')
+
+    inquiryoutcomestable = op.create_table('OIPCInquiryOutcomes',
+    sa.Column('inquiryoutcomeid', sa.Integer(), autoincrement=True, nullable=False),
+    sa.Column('name', sa.String(length=100), unique=False, nullable=False),
+    sa.Column('isactive', sa.Boolean(), unique=False, nullable=False),
+    sa.PrimaryKeyConstraint('inquiryoutcomeid')
+    )
+
+    op.execute('''INSERT INTO public."OIPCReviewTypes" (name, isactive)
+               VALUES
+                ('Complaint', True),
+                ('Review', True),
+                ('Investigation', True);commit;''')
+
+    op.execute('''INSERT INTO public."OIPCReasons" (name, isactive)
+               VALUES
+                ('Adequate Search', True),
+                ('Application of Exceptions', True),
+                ('Deemed Refusal', True),
+                ('Extension', True),
+                ('Fee Amount', True),
+                ('Fee Waiver', True),
+                ('Records do not Exist', True),
+                ('Duty to Assist', True),
+                ('TPN - 22', True),
+                ('TPN - 21', True),
+                ('TPN - 18.1', True),
+                ('Reg 3', True),
+                ('Reg 4', True),
+                ('Reg 5', True),
+                ('s. 43', True),
+                ('Other', True);commit;''')
+
+    op.execute('''INSERT INTO public."OIPCStatuses" (name, isactive)
+               VALUES
+                ('Mediation', True),
+                ('Investigation', True),
+                ('Inquiry', True),
+                ('Awaiting Order', True),
+                ('Closed', True);commit;''')
+
+    op.execute('''INSERT INTO public."OIPCOutcomes" (name, isactive)
+               VALUES
+                ('Abandoned', True),
+                ('Withdrawn', True),
+                ('Resolved in Mediation', True),
+                ('Closed', True);commit;''')
+
+    op.execute('''INSERT INTO public."OIPCInquiryOutcomes" (name, isactive)
+               VALUES
+                ('Decision Upheld', True),
+                ('Decision Partially Upheld', True),
+                ('Decision Overturned', True);commit;''')
 
 def downgrade():
     op.drop_table('OIPCReviewTypes')
     op.drop_table('OIPCReasons')
     op.drop_table('OIPCStatuses')
     op.drop_table('OIPCOutcomes')
+    op.drop_table('OIPCInquiryOutcomes')
