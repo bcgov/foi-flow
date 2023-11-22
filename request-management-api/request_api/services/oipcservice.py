54f582a1c89fddb3996b26fa3aaf7f461f762569
Milos Despotovic <milosmdes@gmail.com>
2023-11-22 12:04:59 -0800

    Add OIPCReviewTypesReasons table and update endpoint

--- request-management-api/request_api/services/oipcservice.py
+++ request-management-api/request_api/services/oipcservice.py
@@ -3,12 +3,16 @@ from request_api.models.OIPCStatuses import OIPCStatuses
 from request_api.models.OIPCReasons import OIPCReasons
 from request_api.models.OIPCOutcomes import OIPCOutcomes
 from request_api.models.OIPCInquiryOutcomes import OIPCInquiryOutcomes
+from request_api.models.OIPCReviewTypesReasons import OIPCReviewTypesReasons
 
 class oipcservice:
     """ OIPC service
     """
     def getreviewtypes(self):
         return OIPCReviewTypes.getreviewtypes()
+
+    def getreviewtypeswithreasons(self):
+        return OIPCReviewTypesReasons.getreviewtypeswithreasons()
 
     def getreasons(self):
         return OIPCReasons.getreasons()
