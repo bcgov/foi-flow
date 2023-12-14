d69d72f83d15fda9b86acae853ceec7f5db25269
Aman-Hundal <aman.hundal@gov.bc.ca>
2023-12-13 16:51:59 -0800

    Reworked api endpoint - confirm with team to see which one is preferred

--- request-management-api/request_api/resources/foirequest.py
+++ request-management-api/request_api/resources/foirequest.py
@@ -126,6 +126,7 @@ class FOIRequests(Resource):
         
 @cors_preflight('GET,POST,PUT,OPTIONS')
 @API.route('/foirequests/<int:foirequestid>/ministryrequest/<int:foiministryrequestid>')
+@API.route('/foirequests/<int:foirequestid>/ministryrequest/<int:foiministryrequestid>/<string:actiontype>')
 class FOIRequestsById(Resource):
     """Creates a new version of foi request for iao updates"""
 
@@ -133,11 +134,17 @@ class FOIRequestsById(Resource):
     @TRACER.trace()
     @cross_origin(origins=allowedorigins())
     @auth.require
-    def post(foirequestid,foiministryrequestid):
+    def post(foirequestid,foiministryrequestid,actiontype=None):
         """ POST Method for capturing FOI requests before processing"""
         try:
-            request_json = request.get_json() 
-            foirequestschema = FOIRequestWrapperSchema().load(request_json)  
+            if actiontype == "deleteoipc":
+                current_foirequest = requestservice().getrequest(foirequestid, foiministryrequestid)
+                current_foirequest['isoipcreview'] = False
+                current_foirequest['oipcdetails'] = []
+                foirequestschema = FOIRequestWrapperSchema().load(current_foirequest)  
+            else:
+                request_json = request.get_json() 
+                foirequestschema = FOIRequestWrapperSchema().load(request_json)  
             result = requestservice().saverequestversion(foirequestschema, foirequestid, foiministryrequestid,AuthHelper.getuserid())
             if result.success == True:
                 asyncio.ensure_future(eventservice().postevent(foiministryrequestid,"ministryrequest",AuthHelper.getuserid(),AuthHelper.getusername(),AuthHelper.isministrymember()))
@@ -292,31 +299,31 @@ class FOIRequestByMinistryId(Resource):
         except BusinessException as exception:            
             return {'status': exception.status_code, 'message':exception.message}, 500
 
-@cors_preflight('POST, DELETE, OPTIONS')
-@API.route('/foirequests/<int:foirequestid>/ministryrequest/<int:foiministryrequestid>/deleteoipc')
-class FOIRequestsById(Resource):
-    """Creates a new version of foi request for iao users deleting all oipc details"""
-    @staticmethod
-    @TRACER.trace()
-    @cross_origin(origins=allowedorigins())
-    @auth.require
-    def post(foirequestid,foiministryrequestid):
-        try:
-            current_foirequest = requestservice().getrequest(foirequestid, foiministryrequestid)
-            current_foirequest['isoipcreview'] = False
-            current_foirequest['oipcdetails'] = []
-            foirequestschema = FOIRequestWrapperSchema().load(current_foirequest)  
-            result = requestservice().saverequestversion(foirequestschema, foirequestid, foiministryrequestid,AuthHelper.getuserid())
-            if result.success == True:
-                asyncio.ensure_future(eventservice().postevent(foiministryrequestid,"ministryrequest",AuthHelper.getuserid(),AuthHelper.getusername(),AuthHelper.isministrymember()))
-                metadata = json.dumps({"id": result.identifier, "ministries": result.args[0]})
-                requestservice().posteventtoworkflow(foiministryrequestid,  foirequestschema, json.loads(metadata),"iao")
-                return {'status': result.success, 'message':result.message,'id':result.identifier, 'ministryRequests': result.args[0]} , 200
-            else:
-                 return {'status': False, 'message':EXCEPTION_MESSAGE_NOTFOUND_REQUEST,'id':foirequestid} , 404
-        except ValidationError as err:
-            return {'status': False, 'message': str(err)}, 400
-        except KeyError as error:
-            return {'status': False, 'message': CUSTOM_KEYERROR_MESSAGE + str(error)}, 400    
-        except BusinessException as exception:            
-            return {'status': exception.status_code, 'message':exception.message}, 500 
+# @cors_preflight('POST, DELETE, OPTIONS')
+# @API.route('/foirequests/<int:foirequestid>/ministryrequest/<int:foiministryrequestid>/deleteoipc')
+# class FOIRequestsById(Resource):
+#     """Creates a new version of foi request for iao users deleting all oipc details"""
+#     @staticmethod
+#     @TRACER.trace()
+#     @cross_origin(origins=allowedorigins())
+#     @auth.require
+#     def post(foirequestid,foiministryrequestid):
+#         try:
+#             current_foirequest = requestservice().getrequest(foirequestid, foiministryrequestid)
+#             current_foirequest['isoipcreview'] = False
+#             current_foirequest['oipcdetails'] = []
+#             foirequestschema = FOIRequestWrapperSchema().load(current_foirequest)  
+#             result = requestservice().saverequestversion(foirequestschema, foirequestid, foiministryrequestid,AuthHelper.getuserid())
+#             if result.success == True:
+#                 asyncio.ensure_future(eventservice().postevent(foiministryrequestid,"ministryrequest",AuthHelper.getuserid(),AuthHelper.getusername(),AuthHelper.isministrymember()))
+#                 metadata = json.dumps({"id": result.identifier, "ministries": result.args[0]})
+#                 requestservice().posteventtoworkflow(foiministryrequestid,  foirequestschema, json.loads(metadata),"iao")
+#                 return {'status': result.success, 'message':result.message,'id':result.identifier, 'ministryRequests': result.args[0]} , 200
+#             else:
+#                  return {'status': False, 'message':EXCEPTION_MESSAGE_NOTFOUND_REQUEST,'id':foirequestid} , 404
+#         except ValidationError as err:
+#             return {'status': False, 'message': str(err)}, 400
+#         except KeyError as error:
+#             return {'status': False, 'message': CUSTOM_KEYERROR_MESSAGE + str(error)}, 400    
+#         except BusinessException as exception:            
+#             return {'status': exception.status_code, 'message':exception.message}, 500 
