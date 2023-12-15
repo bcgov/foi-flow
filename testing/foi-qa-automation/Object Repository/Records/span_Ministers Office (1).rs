5dbc101eda7e69f392baf9fdfdd1c7f2b324f75f
nimya-aot <nimya.john@aot-technologies.com>
2023-12-15 13:17:10 -0800

    #4849 fixed scripts

--- testing/foi-qa-automation/Object Repository/Records/span_Ministers Office (1).rs
+++ testing/foi-qa-automation/Object Repository/Records/span_Ministers Office (1).rs
@@ -6,15 +6,15 @@
    <elementGuidId>81038e8b-fd78-4609-ba85-b07ac247732d</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>XPATH</key>
+         <value>//div[@id='Records']/div/div[2]/div[6]/div[5]/div/div/span</value>
+      </entry>
+      <entry>
          <key>BASIC</key>
          <value>//*[(text() = concat(&quot;Minister&quot; , &quot;'&quot; , &quot;s Office&quot;) or . = concat(&quot;Minister&quot; , &quot;'&quot; , &quot;s Office&quot;))]</value>
       </entry>
       <entry>
          <key>CSS</key>
-      </entry>
-      <entry>
-         <key>XPATH</key>
-         <value>//div[@id='Records']/div/div[2]/div[6]/div[5]/div/div/span</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -25,7 +25,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>span</value>
-      <webElementGuid>3514bfe7-1977-419e-b203-b908c8cb706b</webElementGuid>
+      <webElementGuid>30655841-5218-43b1-922d-962332c6497f</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -33,7 +33,7 @@
       <name>class</name>
       <type>Main</type>
       <value>MuiChip-label MuiChip-labelSmall</value>
-      <webElementGuid>33cc80f7-4f95-44ab-88db-8c9b6e10a484</webElementGuid>
+      <webElementGuid>ad85adaf-aecd-4aa4-9f0e-cd094ed40b3e</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -41,7 +41,7 @@
       <name>text</name>
       <type>Main</type>
       <value>Minister's Office</value>
-      <webElementGuid>71875de2-0915-4acc-ae37-a2b0d4c5fbe8</webElementGuid>
+      <webElementGuid>264d27e4-09c6-4972-917c-5eafe6ecb000</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -49,7 +49,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;Records&quot;)/div[@class=&quot;jss54&quot;]/div[@class=&quot;MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-1 MuiGrid-align-items-xs-flex-start&quot;]/div[@class=&quot;MuiGrid-root jss56 MuiGrid-container MuiGrid-item MuiGrid-align-items-xs-flex-start MuiGrid-grid-xs-12&quot;]/div[@class=&quot;MuiGrid-root MuiGrid-container MuiGrid-item MuiGrid-align-items-xs-flex-start MuiGrid-grid-xs-auto&quot;]/div[@class=&quot;MuiGrid-root MuiGrid-item MuiGrid-grid-xs-6&quot;]/div[@class=&quot;MuiChip-root jss51 jss52 MuiChip-sizeSmall&quot;]/span[@class=&quot;MuiChip-label MuiChip-labelSmall&quot;]</value>
-      <webElementGuid>c2c3895b-4160-49da-902e-e73a6dc12e23</webElementGuid>
+      <webElementGuid>d6ceccc2-62b1-4d34-8f20-d3399971ef9f</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -57,7 +57,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//div[@id='Records']/div/div[2]/div[6]/div[5]/div/div/span</value>
-      <webElementGuid>6c340ab0-45e0-4ee1-a791-d227041d0dc5</webElementGuid>
+      <webElementGuid>a439046f-b1b0-46df-ab8b-ff2dfb3f77ac</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -65,7 +65,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Ready for Redaction'])[2]/following::span[3]</value>
-      <webElementGuid>86cfbdc5-0dcb-46bd-8bd1-1ce69a0fa30b</webElementGuid>
+      <webElementGuid>2a071e7e-937d-4890-9e0e-baea992ebcdc</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -73,7 +73,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='corrupted.pdf'])[1]/following::span[5]</value>
-      <webElementGuid>b34ed176-7e06-4805-8e23-93ba69ca095b</webElementGuid>
+      <webElementGuid>6f9aecbe-f036-49c3-be0c-322695977709</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -81,7 +81,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='foiedu, foiedu'])[3]/preceding::span[1]</value>
-      <webElementGuid>cad4c010-99c2-494b-951e-57a368219f0b</webElementGuid>
+      <webElementGuid>c26e070a-7714-44ff-aa37-e6fba07c3715</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -89,7 +89,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='test2.docx'])[1]/preceding::span[1]</value>
-      <webElementGuid>ca67618d-c6e0-46ac-b7dc-691f98cb5cb0</webElementGuid>
+      <webElementGuid>f65179be-762e-4aa2-983f-e499075ba5f9</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -97,7 +97,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//div[5]/div/div/span</value>
-      <webElementGuid>0a45f9a2-c7d5-419d-8d14-b35b16f5d8b4</webElementGuid>
+      <webElementGuid>a43ed301-dd25-4319-b37b-3e6c57ef5ecb</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -105,6 +105,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//span[(text() = concat(&quot;Minister&quot; , &quot;'&quot; , &quot;s Office&quot;) or . = concat(&quot;Minister&quot; , &quot;'&quot; , &quot;s Office&quot;))]</value>
-      <webElementGuid>4dda12fe-cca1-47aa-b247-cc0e5fdb7421</webElementGuid>
+      <webElementGuid>8bbb0cef-e682-44b9-901e-447d12f212cb</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
