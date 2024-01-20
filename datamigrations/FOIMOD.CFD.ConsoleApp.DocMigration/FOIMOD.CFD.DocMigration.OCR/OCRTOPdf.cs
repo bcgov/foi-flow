45ef5026e4bbe1e2af328d185104b8fdd4eb7ec7
Abin Antony <abin.antony@aot-technologies.com>
2024-01-19 16:47:35 -0800

    baseclass for OCR doc migration #4337

---
+++ datamigrations/FOIMOD.CFD.ConsoleApp.DocMigration/FOIMOD.CFD.DocMigration.OCR/OCRTOPdf.cs
@@ -0,0 +1,37 @@
+﻿
+using Syncfusion.OCRProcessor;
+using Syncfusion.Pdf.Parsing;
+
+namespace MCS.FOI.OCRtoPDF
+{
+    public  static class OCRTOPdf
+    {
+
+        public static Stream ConvertToSearchablePDF(FileStream inputPdfFilestream)
+        {
+            using (OCRProcessor processor = new OCRProcessor())
+            {
+                using FileStream fileStream = inputPdfFilestream;
+                //Load a PDF document.
+                using PdfLoadedDocument lDoc = new PdfLoadedDocument(fileStream);
+                //Set OCR language to process.
+                processor.Settings.Language = Languages.English;
+                //Process OCR by providing the PDF document.
+                processor.PerformOCR(lDoc);
+
+
+                //Create memory stream.
+                MemoryStream stream = new MemoryStream();
+                //Save the document to memory stream.
+                lDoc.Save(stream);
+                lDoc.Close();
+                //Set the position as '0'.
+                stream.Position = 0;
+                return stream;
+               
+            }
+
+        }
+
+    }
+}
