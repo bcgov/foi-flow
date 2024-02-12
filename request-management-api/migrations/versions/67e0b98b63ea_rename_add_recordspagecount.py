f3ca1d3c23fdbebcc5af7d8672e439807ee3f721
divyav-aot <divyav-aot@github>
2024-02-12 13:51:16 -0500

    fixed multiple head issue

--- request-management-api/migrations/versions/67e0b98b63ea_rename_add_recordspagecount.py
+++ request-management-api/migrations/versions/67e0b98b63ea_rename_add_recordspagecount.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '67e0b98b63ea'
-down_revision = 'b4da31675bd0'
+down_revision = '6646acda32fe'
 branch_labels = None
 depends_on = None
 
