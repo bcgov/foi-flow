73509747bb6331968fd29c1ae1508bb32765450c
abin-aot <abin.antony@aot-technologies.com>
2024-02-13 17:38:58 -0800

    Mapping POCO class

---
+++ datamigrations/FOIMOD.CFD.ConsoleApp.DocMigration/FOIMOD.CFD.DocMigration.Models/FolderToSectionMapper.cs
@@ -0,0 +1,8 @@
+﻿namespace FOIMOD.CFD.DocMigration.Models
+{
+    public class FolderToSectionMapper
+    {
+        public string AXISFolder { get; set; }
+        public string MODSECTION {  get; set; }
+    }
+}
