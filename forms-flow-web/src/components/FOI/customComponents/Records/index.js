3b04a38070295c88976d71e30797aefdaf237536
Richard Qi <richard.qi@aot-technologies.com>
2024-05-21 14:43:49 -0700

    bug fix missing comparing trackingid

--- forms-flow-web/src/components/FOI/customComponents/Records/index.js
+++ forms-flow-web/src/components/FOI/customComponents/Records/index.js
@@ -1806,7 +1806,9 @@ export const RecordsLog = ({
 
   const comparePersonalAttributes = (a, b) => {
     return a?.person === b?.person && a?.volume === b?.volume
-              && a?.filetype === b?.filetype && a?.personaltag === b?.personaltag;
+              && a?.filetype === b?.filetype
+              && a?.personaltag === b?.personaltag
+              && a?.trackingid === b?.trackingid;
   };
 
   const updatePersonalAttributes = (_all = false) => {
