2a24a48a92128d468ef55b7ca068f16d7a122182
nkan-aot2 <156717133+nkan-aot2@users.noreply.github.com>
2024-05-29 15:44:33 -0700

    correct previous correction

--- forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
+++ forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
@@ -1307,7 +1307,7 @@ const FOIRequest = React.memo(({ userDetail, openApplicantProfileModal }) => {
                           handleContactDetailsInitialValue
                         }
                         handleContanctDetailsValue={handleContanctDetailsValue}
-                        disableInput={disableInput || /*requestDetails?.axisApplicantID /* requestDetails?.foiRequestApplicantID > 0 comment back in after axis decommission*/}
+                        disableInput={disableInput /* || requestDetails?.axisApplicantID /* requestDetails?.foiRequestApplicantID > 0 comment back in after axis decommission*/}
                         handleEmailValidation={handleEmailValidation}
                         defaultExpanded={!closeContactInfo(userDetail,requestDetails)}
                         moreInfoAction={openApplicantProfileModal}
@@ -1355,7 +1355,7 @@ const FOIRequest = React.memo(({ userDetail, openApplicantProfileModal }) => {
                         <AdditionalApplicantDetails
                           requestDetails={requestDetails}
                           createSaveRequestObject={createSaveRequestObject}
-                          disableInput={disableInput || /*requestDetails?.axisApplicantID /* requestDetails?.foiRequestApplicantID > 0 comment back in after axis decommission*/}
+                          disableInput={disableInput /* || requestDetails?.axisApplicantID /* requestDetails?.foiRequestApplicantID > 0 comment back in after axis decommission*/}
                           defaultExpanded={true}
                         />
                       )}
