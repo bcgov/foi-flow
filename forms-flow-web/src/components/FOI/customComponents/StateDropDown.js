12e2f7258134f36ee026e6ff282c04794806c892
JieunSon96 <plmn855000@gmail.com>
2024-06-11 16:32:33 -0700

    Added filter to remove RRfR for non-MCF users

--- forms-flow-web/src/components/FOI/customComponents/StateDropDown.js
+++ forms-flow-web/src/components/FOI/customComponents/StateDropDown.js
@@ -200,6 +200,9 @@ const StateDropDown = ({
         if (personalIAO) return _stateList.feeassessedforpersonal;
         return _stateList.feeassessed;
       case StateEnum.deduplication.name.toLowerCase():
+        if (!isMCFMinistryTeam) {
+          return _stateList.deduplication.filter(_state => _state.status.toLowerCase() !== StateEnum.recordsreadyforreview.name.toLowerCase());
+        }
         return _stateList.deduplication;
       case StateEnum.harms.name.toLowerCase():
         return _stateList.harms;
