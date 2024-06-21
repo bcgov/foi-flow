c6488644fa2c44d69b801c7261b754ebf9595dd1
Son <Sarah.Son@gov.bc.ca>
2024-06-20 22:56:09 -0700

    fixed alembic revision issue

--- request-management-api/migrations/versions/7583b5428c87_.py
+++ request-management-api/migrations/versions/7583b5428c87_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8487688c2e35
+Revision ID: 7583b5428c87
 Revises: c3e447d3d94d
-Create Date: 2024-06-20 17:16:09.447577
+Create Date: 2024-06-19 15:29:08.420315
 
 """
 from alembic import op
@@ -13,8 +13,8 @@ from sqlalchemy.sql.sqltypes import Boolean, String,Text,Integer,DateTime
 
 
 # revision identifiers, used by Alembic.
-revision = '8487688c2e35'
-down_revision = '7583b5428c87'
+revision = '7583b5428c87'
+down_revision = 'c3e447d3d94d'
 branch_labels = None
 depends_on = None
 
@@ -32,7 +32,7 @@ def upgrade():
                                    )
     op.bulk_insert(
         applicant_correspondence_templates,
-        [{'name': 'OIPCAPPLICANTCONSENTEXTENSION',
+        {'name': 'OIPCAPPLICANTCONSENTEXTENSION',
              'description': 'OIPC Applicant Consent Time Extension',
              'active': True,
              'display': True,
@@ -40,7 +40,7 @@ def upgrade():
              'documenturipath': '/TEMPLATES/EMAILS/oipc_applicant_consent_time_extension.html',
              'created_at': datetime.now(),
              'createdby': 'system'},
-        {'name': 'OIPCFIRSTTIMEEXTENSION',
+             {'name': 'OIPCFIRSTTIMEEXTENSION',
              'description': 'OIPC Applicant First Time Extension',
              'active': True,
              'display': True,
@@ -48,21 +48,18 @@ def upgrade():
              'documenturipath': '/TEMPLATES/EMAILS/oipc_first_time_extension.html',
              'created_at': datetime.now(),
              'createdby': 'system'},
-        {'name': 'OIPCSUBSEQUENTTIMEEXTENSION',
+             {'name': 'OIPCSUBSEQUENTTIMEEXTENSION',
              'description': 'OIPC Subsequent Time Extension',
              'active': True,
              'display': True,
              'version': 1,
              'documenturipath': '/TEMPLATES/EMAILS/oipc_subsequent_time_extension.html',
              'created_at': datetime.now(),
-             'createdby': 'system'}]
+             'createdby': 'system'},
     )
 
 
 def downgrade():
     op.execute(
-        """
-        DELETE FROM "ApplicantCorrespondenceTemplates"
-        WHERE name IN ('OIPCAPPLICANTCONSENTEXTENSION', 'OIPCFIRSTTIMEEXTENSION', 'OIPCSUBSEQUENTTIMEEXTENSION')
-        """
+        "DELETE FROM ApplicantCorrespondenceTemplates WHERE name = 'OIPCAPPLICANTCONSENTEXTENSION'"
     )
