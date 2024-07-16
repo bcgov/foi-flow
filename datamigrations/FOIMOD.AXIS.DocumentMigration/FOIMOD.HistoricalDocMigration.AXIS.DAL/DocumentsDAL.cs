2ee2beec79571678234cfa5dc4e2ba919dddd017
abin-aot <abin.antony@aot-technologies.com>
2024-07-16 16:12:40 -0700

    #FOIMOD-3188 adding closig for S3 Folder creation

--- datamigrations/FOIMOD.AXIS.DocumentMigration/FOIMOD.HistoricalDocMigration.AXIS.DAL/DocumentsDAL.cs
+++ datamigrations/FOIMOD.AXIS.DocumentMigration/FOIMOD.HistoricalDocMigration.AXIS.DAL/DocumentsDAL.cs
@@ -23,31 +23,39 @@ namespace FOIMOD.HistoricalDocMigration.AXIS.DAL
                                             ,C.vcEmail
                                             ,C.vcFromEmail
                                             , CAST(C.vcBody as NVARCHAR(max)) as emailbody
-                                            , STRING_AGG(CAST((C.vcFilePath +' * ' +C.vcFileName) as nvarchar(max)),' | ') as attachments  
+                                            , STRING_AGG(CAST((C.vcFilePath +' * ' +C.vcFileName) as nvarchar(max)),' | ') as attachments
+                                            , R.sdtClosedDate as ClosingDate
 
                                             FROM tblCorrespondence C JOIN tblRequests R on C.iRequestID = R.iRequestID
                                             WHERE R.vcVisibleRequestID in ({0})
 
-                                            GROUP BY R.vcVisibleRequestID,C.vcSubject,C.sdtMailedDate,C.vcEmail,C.vcFromEmail,CAST(C.vcBody as NVARCHAR(max))";
+                                            GROUP BY R.vcVisibleRequestID,C.vcSubject,C.sdtMailedDate,C.vcEmail,C.vcFromEmail,CAST(C.vcBody as NVARCHAR(max)),R.sdtClosedDate";
 
 
         private string recordsbyrequestid = @"
-                        DECLARE @SectionList VARCHAR(MAX);
+                        
+                    DECLARE @SectionList VARCHAR(MAX);
                         DECLARE @irequestid INT
+	                    DECLARE @closingdate DATE
+
                         SET @irequestid=(SELECT TOP 1 iRequestID FROM tblRequests WHERE vcVisibleRequestID = '{0}')
-                    SET @SectionList = NULL;
+	                    SET  @closingdate=(SELECT sdtClosedDate FROM tblRequests WHERE vcVisibleRequestID = '{1}')
+	                    SET @SectionList = NULL;
                     SELECT
                         @SectionList = COALESCE(@SectionList+':', '')+vcSectionList
                     FROM [dbo].[tblDocumentReviewLog] WHERE iRequestID =@irequestid
 
-                    SELECT DISTINCT D.iDocID,D.vcDocName as FolderName,(SELECT vcDocName FROM tblDocuments where iDocID =D.iParentDocID) as ParentFolderName ,D.tiSections,vcFileName as FilePath,REVERSE(SUBSTRING(REVERSE(vcFileName),1,4)) as FileType,D.siFolderID,D.siPageCount ,p.siPageNum ,(SELECT vcInternalName FROM tblDocReviewFlags WHERE tiDocReviewFlagID = PRF.tiDocReviewFlagID ) as PageReviewFlag FROM tblPages P inner join tblDocuments D on P.iDocID=D.iDocID 
-					LEFT JOIN tblPageReviewFlags PRF ON P.iPageID = PRF.iPageID
-					WHERE  D.iDocID in(                 
+                    SELECT DISTINCT D.iDocID,D.vcDocName as FolderName,(SELECT vcDocName FROM tblDocuments where iDocID =D.iParentDocID) as ParentFolderName ,D.tiSections,vcFileName as FilePath,REVERSE(SUBSTRING(REVERSE(vcFileName),1,4)) as FileType,D.siFolderID,D.siPageCount ,p.siPageNum ,(SELECT vcInternalName FROM tblDocReviewFlags WHERE tiDocReviewFlagID = PRF.tiDocReviewFlagID ) as PageReviewFlag, @closingdate as ClosingDate FROM tblPages P inner join tblDocuments D on P.iDocID=D.iDocID 
+                    LEFT JOIN tblPageReviewFlags PRF ON P.iPageID = PRF.iPageID
+                    WHERE  D.iDocID in(                 
                     SELECT iDocID FROM tblDocuments d with(nolock) where iDocID IN (SELECT Data FROM [dbo].[AFX_Splitter](@SectionList, ':'))
                     union
                     --- Request Folder Documents    
                     select iDocID from tblRedactionLayers where irequestid=@irequestid AND iDeliveryID is NULL
-                    ) ORDER BY D.iDocID, p.siPageNum";
+                    )  
+
+                    ORDER BY D.iDocID, p.siPageNum
+                ";
 
         private string getQueryByType(DocumentTypeFromAXIS documentTypeFromAXIS, string requestnumber="")
         {
@@ -58,7 +66,7 @@ namespace FOIMOD.HistoricalDocMigration.AXIS.DAL
                     query =string.Format(correspondencelog,requestnumber);
                     break;
                 case DocumentTypeFromAXIS.RequestRecords:
-                    query = string.Format(recordsbyrequestid, requestnumber);
+                    query = string.Format(recordsbyrequestid, requestnumber, requestnumber);
                     break;
                 default:
                     break;
@@ -93,6 +101,7 @@ namespace FOIMOD.HistoricalDocMigration.AXIS.DAL
                                 EmailDate = Convert.ToString(row["sdtMailedDate"]),
                                 EmailAttachmentDelimitedString = Convert.ToString(row["attachments"]),
                                 AXISRequestNumber = Convert.ToString(row["vcVisibleRequestID"]),
+                                ClosingDate = Convert.ToDateTime(row["ClosingDate"])
                             });
 
                         }
@@ -144,7 +153,8 @@ namespace FOIMOD.HistoricalDocMigration.AXIS.DAL
                                 FolderName = Convert.ToString(row["FolderName"]),
                                 FileType = Convert.ToString(row["FileType"]),
                                 ParentFolderName = Convert.ToString(row["ParentFolderName"]),
-                                ReviewFlag = Convert.ToString(row["PageReviewFlag"])
+                                ReviewFlag = Convert.ToString(row["PageReviewFlag"]),
+                                ClosingDate = Convert.ToDateTime(row["ClosingDate"])
                             });
 
                         }
