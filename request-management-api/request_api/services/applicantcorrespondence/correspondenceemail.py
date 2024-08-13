1b7d73e389d7c17ca1ac1de0970686ea7b75c62c
Milos Despotovic <milosmdes@gmail.com>
2024-08-13 12:05:03 -0700

    Handle saving raw request correspondence emails

--- request-management-api/request_api/services/applicantcorrespondence/correspondenceemail.py
+++ request-management-api/request_api/services/applicantcorrespondence/correspondenceemail.py
@@ -1,5 +1,8 @@
 from request_api.models.FOICorrespondenceEmails import FOICorrespondenceEmail
+from request_api.models.FOICorrespondenceEmailsRawRequests import FOICorrespondenceEmailRawRequest
+from request_api.models.FOIApplicantCorrespondenceEmailsRawRequests import FOIApplicantCorrespondenceEmailRawRequest
 from request_api.models.FOIMinistryRequests import FOIMinistryRequest
+from request_api.models.FOIRawRequests import FOIRawRequest
 
 import maya
 import json
@@ -7,22 +10,33 @@ import html
 from datetime import datetime
 class correspondenceemailservice:
 
-    def savecorrespondenceemail(self, ministryrequestid, data, userid):
+    def savecorrespondenceemail(self, ministryrequestid, rawrequestid, data, userid):
         """ Saves the email against the request information
         """
-        correspondenceemail = FOICorrespondenceEmail()
-        correspondenceemail.email = data['email']
-        correspondenceemail.foiministryrequest_id = ministryrequestid
-        correspondenceemail.foiministryrequestversion_id =FOIMinistryRequest.getversionforrequest(ministryrequestid=ministryrequestid)
-        correspondenceemail.createdby = userid
-        correspondenceemail.created_at = datetime.now()
-        return FOICorrespondenceEmail.savecorrespondenceemail(correspondenceemail)
+        if rawrequestid != 'None':
+            correspondenceemail = FOICorrespondenceEmailRawRequest()
+            correspondenceemail.email = data['email']
+            correspondenceemail.foirawrequest_id = rawrequestid
+            correspondenceemail.foirawrequestversion_id =FOIRawRequest.getversionforrequest(requestid=rawrequestid)
+            correspondenceemail.createdby = userid
+            correspondenceemail.created_at = datetime.now()
+            return FOICorrespondenceEmailRawRequest.savecorrespondenceemail(correspondenceemail)
+        else:
+            correspondenceemail = FOICorrespondenceEmail()
+            correspondenceemail.email = data['email']
+            correspondenceemail.foiministryrequest_id = ministryrequestid
+            correspondenceemail.foiministryrequestversion_id =FOIMinistryRequest.getversionforrequest(ministryrequestid=ministryrequestid)
+            correspondenceemail.createdby = userid
+            correspondenceemail.created_at = datetime.now()
+            return FOICorrespondenceEmail.savecorrespondenceemail(correspondenceemail)
     
 
-    def getcorrespondenceemails(self, ministryrequestid):
+    def getcorrespondenceemails(self, ministryrequestid, rawrequestid):
         """ Saves the email against the request information
         """
-        correspondenceemails = FOICorrespondenceEmail.getcorrespondenceemails(ministryrequestid)
+        correspondenceemails = FOICorrespondenceEmailRawRequest.getcorrespondenceemails(rawrequestid)
+        if ministryrequestid != 'None':
+            correspondenceemails.extend(FOICorrespondenceEmail.getcorrespondenceemails(ministryrequestid))
         
         return correspondenceemails
     
