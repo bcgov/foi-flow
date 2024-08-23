57049e9247ef4f00a4f3ad98167c6b73f14b78b3
Aparna <aparna.s@aot-technologies.com>
2024-08-23 11:15:52 -0700

    FOIMOD-2982: Comments Tab - Type of Comments (Peer Review, Internal, General)
    FOIMOD-2982 : Minor Styling update related to reply comment post button

--- forms-flow-web/src/components/FOI/customComponents/Comments/InputField.js
+++ forms-flow-web/src/components/FOI/customComponents/Comments/InputField.js
@@ -339,7 +339,7 @@ const InputField = ({ cancellor, parentId, child, inputvalue, edit, main, add, f
           plugins={plugins}
           spellCheck={true}
         />
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent:'space-between' }}>
         <Toolbar>
           {
             (externalProps) => (
@@ -356,6 +356,20 @@ const InputField = ({ cancellor, parentId, child, inputvalue, edit, main, add, f
         <div className={'col-lg-9'}>
           <span className={textlength > 25 ? "characterlen" : "characterlen textred"}>{textlength} characters remaining</span>
         </div>
+        <div className="col-lg-1 paperplanecontainer inputActions">
+          <button
+            className="postBtn"
+            onClick={post}
+            type='button'
+            disabled={uftext.trim().length === 0}
+          >
+            {' '}
+            <svg aria-hidden="true" aria-describedby="postComment" focusable="false" data-prefix="fas" data-icon="paper-plane" class="svg-inline--fa fa-paper-plane fa-w-16 fa-2x " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" disabled={uftext.trim().length === 0} color={uftext.trim().length === 0 ? '#a5a5a5' : 'darkblue'}>
+              <title id="postComment" style={{display: 'none'}}>Post Comment</title>
+              <path fill="currentColor" d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"></path>
+            </svg>
+          </button>
+        </div>
         </div>
         <MentionSuggestions
           open={open}
@@ -369,7 +383,7 @@ const InputField = ({ cancellor, parentId, child, inputvalue, edit, main, add, f
         />
 
       </form>
-      <div className="inputActions">
+      {/* <div className="inputActions">
 
         <div className="col-lg-1 paperplanecontainer">
           <button
@@ -385,7 +399,7 @@ const InputField = ({ cancellor, parentId, child, inputvalue, edit, main, add, f
             </svg>
           </button>
         </div>
-      </div>
+      </div> */}
 
     </>
   )
