0ecd0a44f0eac88c990c29d1ac2bdeb456cb3def
Aman-Hundal <aman.hundal@gov.bc.ca>
2024-08-26 14:11:26 -0700

    version 1 of foioirequest transactional migration completed

--- request-management-api/migrations/versions/98cd70bd07f0_.py
+++ request-management-api/migrations/versions/98cd70bd07f0_.py
@@ -20,9 +20,25 @@ def upgrade():
     op.create_table('FOIOpenInformationRequests',
     sa.Column('foiopeninforequestid', sa.Integer(), autoincrement=True, nullable=False),
     sa.PrimaryKeyConstraint('foiopeninforequestid'),
+    sa.Column('foiministryrequest_id', sa.Integer(), nullable=False),
+    sa.Column('foiministryrequestversion_id', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['foiministryrequest_id', 'foiministryrequestversion_id'], ['FOIMinistryRequests.foiministryrequestid', 'FOIMinistryRequests.version']),
     sa.Column('oiexemption_id', sa.Integer(), sa.ForeignKey('OpenInfoExemptions.oiexemptionid'), nullable=True),
+    sa.Column('oipublicationstatus_id', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['oipublicationstatus_id'], ['OpenInfoPublicationStatuses.oipublicationstatusid']),
+
+    sa.Column('oiassignee', sa.String(), nullable=False), #THIS A FK OR SHOULD BE SET UP DIFF?
+    sa.Column('pagereference', sa.String()), #json ojec or array to hold array of data
+    sa.Column('oiexemptionapproved', sa.Boolean()),
+    sa.Column('iaorationale', sa.String()),
+    sa.Column('oifeedback', sa.String()),
+    sa.Column('publicationdate', sa.DateTime()),
+    sa.Column('version', sa.Integer(), nullabe=False), #NEEDED?
+
+    sa.Column('created_at', sa.DateTime(), nullable=False),
+    sa.Column('createdby', sa.String(length=120), nullable=False),
+    sa.Column('updated_at', sa.DateTime(), nullable=True),
+    sa.Column('updatedby', sa.String(length=120), nullable=True),
     )
 
     op.add_column('FOIMinistryRequests', sa.Column('oistatus_id', sa.Integer(), nullable=True, default=False))
