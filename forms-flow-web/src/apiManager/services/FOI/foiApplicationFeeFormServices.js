73b31f5391c31651876e520a6821369abdcbf921
Milos Despotovic <milosmdes@gmail.com>
2024-11-14 15:19:46 -0800

    Create app fee form services on frontend

---
+++ forms-flow-web/src/apiManager/services/FOI/foiApplicationFeeFormServices.js
@@ -0,0 +1,112 @@
+import { httpGETRequest, httpPOSTRequest } from "../../httpRequestHandler";
+  import UserService from "../../../services/UserService";
+import API from "../../endpoints";
+  import {
+    serviceActionError,
+    setRequestApplicationFeeForm
+  } from "../../../actions/FOI/foiRequestActions";
+  import { replaceUrl } from "../../../helper/FOI/helper";
+  import { catchError } from "./foiServicesUtil";
+  import _ from 'lodash';
+
+export const fetchApplicationFeeForm = async (
+  ministryId,
+  requestId,
+  dispatch,
+  errorCallback = null
+) => {
+  let apiUrl = replaceUrl(
+      API.FOI_GET_APPLICATION_FEES_FORM,
+      "<ministryrequestid>",
+      ministryId
+    )
+  
+  apiUrl = replaceUrl(apiUrl, "<requestid>", requestId);
+
+  httpGETRequest(apiUrl, {}, UserService.getToken())
+    .then((res) => {
+      if (res.data) {
+        if (!_.isEmpty(res.data)) {
+          dispatch(setRequestApplicationFeeForm(res.data));
+        }
+      } else {
+        console.log("Error in fetching fees data", res);
+        dispatch(serviceActionError(res));
+      }
+    })
+    .catch((error) => {
+      console.log("Error in fetching fees data", error);
+      dispatch(serviceActionError(error));
+      if (errorCallback) {
+        errorCallback("An error occured while trying to save fees form data");
+      }
+    });
+};
+
+export const saveApplicationFeeForm = (
+  data,
+  ministryId,
+  requestId,
+  dispatch,
+  callback,
+  errorCallback,
+) => {
+  let baseUrl = API.FOI_POST_APPLICATION_FEES_FORM;
+
+  const apiUrl = replaceUrl(replaceUrl(
+    baseUrl,
+    "<requestid>",
+    requestId
+  ), "<ministryrequestid>", ministryId);
+  
+  httpPOSTRequest(apiUrl, data)
+    .then((res) => {
+      if (res.data) {
+        if (callback) {
+          callback(res.data);
+        }
+      } else {
+        dispatch(serviceActionError(res));
+        throw new Error();
+      }
+    })
+    .catch((error) => {
+      console.log("An error occured while trying to save CFR form data", error);
+      catchError(error, dispatch);
+      if (errorCallback) {
+        errorCallback("An error occured while trying to save CFR form data");
+      }
+    });
+};
+
+export const generateReceiptFromOnlinePayment = (
+  data,
+  requestId,
+  paymentId,
+  dispatch,
+  callback,
+  errorCallback,
+) => {
+  let baseUrl = API.FOI_POST_RAWREQUEST_PAYMENT_RECEIPT;
+  let apiUrl = replaceUrl(baseUrl, "<request_id>", requestId);
+  apiUrl = replaceUrl(apiUrl, "<payment_id>", paymentId);
+  
+  httpPOSTRequest(apiUrl, data)
+    .then((res) => {
+      if (res.data) {
+        if (callback) {
+          callback(res.data);
+        }
+      } else {
+        dispatch(serviceActionError(res));
+        throw new Error();
+      }
+    })
+    .catch((error) => {
+      console.log("An error occured while trying to generate the application fee receipt", error);
+      catchError(error, dispatch);
+      if (errorCallback) {
+        errorCallback("An error occured while trying to generate the application fee receipt");
+      }
+    });
+};
