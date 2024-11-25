aa94469066939120b9c194eabca6a23464ea3414
Milos Despotovic <milosmdes@gmail.com>
2024-11-25 14:20:47 -0800

    Handle receipts

--- request-management-api/request_api/services/applicationfeeservice.py
+++ request-management-api/request_api/services/applicationfeeservice.py
@@ -1,4 +1,5 @@
 from request_api.models.FOIRequestApplicationFees import FOIRequestApplicationFee
+from request_api.models.FOIRequestApplicationFeeReceipts import FOIRequestApplicationFeeReceipt
 import maya
 from datetime import date
 from datetime import datetime
@@ -17,6 +18,9 @@ class applicationfeeservice:
         applicationfee = FOIRequestApplicationFee.getapplicationfee(requestid)
         # Check for raw requests (from online form) with IGE status
         payments = Payment.find_application_fee_payments_by_requestid(requestid)
+        receipts = FOIRequestApplicationFeeReceipt.getapplicationfeereceipts(applicationfee.get('applicationfeeid'))
+        if receipts != []:
+            applicationfee['receipts'] = receipts
         if applicationfee == {}:
             request = FOIRawRequest.get_request(requestid)
             payments = Payment.find_application_fee_payments_by_requestid(requestid)
@@ -37,6 +41,18 @@ class applicationfeeservice:
                 self.saveapplicationfee(requestid, applicationfee, 'system')
         return self.__formatapplicationfee(applicationfee)
     
+    def saveapplicationfeereceipt(self, applicationfeereceipt):
+        receipt = FOIRequestApplicationFeeReceipt()
+        receipt.createdby = applicationfeereceipt.get('createdby', None)
+        receipt.receiptfilename = applicationfeereceipt.get('receiptfilename', None)
+        receipt.receiptfilepath = applicationfeereceipt.get('receiptfilepath', None)
+        receipt.applicationfeeid = applicationfeereceipt.get('applicationfeeid', None)
+        receipt.isactive = applicationfeereceipt.get('isactive', None)
+        return FOIRequestApplicationFeeReceipt.saveapplicationfeereceipt(receipt)
+    
+    def deactivateapplicationfeereceipt(self, applicationfeereceiptid, userid):
+        return FOIRequestApplicationFeeReceipt.deactivateapplicationfeereceipt(applicationfeereceiptid, userid)
+
     def __prepareapplicationfee(self, requestid, data={}, getprevious=True):
         applicationfee = FOIRequestApplicationFee()
         lkupapplicationfee = self.getapplicationfee(requestid) if getprevious else None
@@ -59,15 +75,20 @@ class applicationfeeservice:
             applicationfee.paymentdate = datetime_object
         applicationfee.orderid = data.get('orderid', None)
         applicationfee.transactionnumber = data.get('transactionnumber', None)
+        applicationfee.refundamount = data.get('refundamount', None)
+        if applicationfee.refunddate and isinstance(applicationfee.refunddate, str) and len(applicationfee.refunddate) < 11 and applicationfee.refunddate.count('-') == 2:
+            parseddateobject = applicationfee.refunddate.split('-')
+            datetime_object = datetime(int(parseddateobject[0]), int(parseddateobject[1]), int(parseddateobject[2]), 17, 0, 0)
+            applicationfee.refunddate = datetime_object
         applicationfee.reasonforrefund = data.get('reasonforrefund', None)
-        applicationfee.receiptfilename = data.get('receiptfilename', None)
-        applicationfee.receiptfilepath = data.get('receiptfilepath', None)
         return applicationfee
 
     def __formatapplicationfee(self,applicationfee):
         if applicationfee is not None and applicationfee != {}:
             if applicationfee.get('paymentdate') is not None:
                 applicationfee['paymentdate'] = self.__pstformat(applicationfee['paymentdate'])
+            if applicationfee.get('refunddate') is not None:
+                applicationfee['refunddate'] = self.__pstformat(applicationfee['refunddate'])
             return applicationfee 
         else:
             return {}
