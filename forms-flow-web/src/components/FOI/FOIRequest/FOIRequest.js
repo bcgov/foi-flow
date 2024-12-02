1836bf58efb27212470d91f21c35113a1dbdf30e
Milos Despotovic <milosmdes@gmail.com>
2024-12-02 10:31:08 -0800

    Add changes from dev-MD-intake-3493

--- forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
+++ forms-flow-web/src/components/FOI/FOIRequest/FOIRequest.js
@@ -92,7 +92,8 @@ import {
   formatDate,
   isRequestRestricted,
   convertSTRToDate,
-  getCommentTypeIdByName
+  getCommentTypeIdByName,
+  isMinistryLogin
 } from "../../../helper/FOI/helper";
 import DivisionalTracking from "./DivisionalTracking";
 import RedactionSummary from "./RedactionSummary";
@@ -111,6 +112,7 @@ import OIPCDetails from "./OIPCDetails/Index";
 import useOIPCHook from "./OIPCDetails/oipcHook";
 import MANDATORY_FOI_REQUEST_FIELDS from "../../../constants/FOI/mandatoryFOIRequestFields";
 import RequestHistorySection from "../customComponents/RequestHistory";
+import { Fees } from "../customComponents/Fees";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -196,6 +198,11 @@ const FOIRequest = React.memo(({ userDetail, openApplicantProfileModal }) => {
   const [unsavedMessage, setUnsavedMessage] = useState(<></>);
   const commentTypes = useSelector((state) => state.foiRequests.foiCommentTypes); 
 
+  let isMinistry = false;
+  if (Object.entries(userDetail).length !== 0) {
+    const userGroups = userDetail && userDetail.groups.map(group => group.slice(1));
+    isMinistry = isMinistryLogin(userGroups);
+  }
 
   const handleUnsavedContinue = () => {
     window.removeEventListener("popstate", handleOnHashChange);
@@ -242,7 +249,7 @@ const FOIRequest = React.memo(({ userDetail, openApplicantProfileModal }) => {
       display: false,
       active: false,
     },
-    CFRForm: {
+    Fees: {
       display: false,
       active: false,
     },
@@ -1066,22 +1073,26 @@ const FOIRequest = React.memo(({ userDetail, openApplicantProfileModal }) => {
     return false;
   };
 
-  const showCFRTab = () => {
-    return (
-      requestState !== StateEnum.intakeinprogress.name &&
-      requestState !== StateEnum.unopened.name &&
-      requestState !== StateEnum.open.name &&
-      requestDetails?.requestType ===
-        FOI_COMPONENT_CONSTANTS.REQUEST_TYPE_GENERAL
-    );
+  const showFeesTab = () => {
+    if (isMinistry) {
+      return (
+        requestState !== StateEnum.intakeinprogress.name &&
+        requestState !== StateEnum.unopened.name &&
+        requestState !== StateEnum.open.name &&
+        requestDetails?.requestType ===
+          FOI_COMPONENT_CONSTANTS.REQUEST_TYPE_GENERAL
+      );
+    } else {
+      return true;
+    }
   };
 
   const showContactApplicantTab = () => {
     return (
-      // requestState !== StateEnum.intakeinprogress.name &&
-      // requestState !== StateEnum.unopened.name &&
-      // requestState !== StateEnum.open.name &&
-      // requestState !== StateEnum.appfeeowing.name &&
+      requestState !== StateEnum.intakeinprogress.name &&
+      requestState !== StateEnum.unopened.name &&
+      requestState !== StateEnum.open.name &&
+      requestState !== StateEnum.appfeeowing.name &&
       requestDetails?.requestType === FOI_COMPONENT_CONSTANTS.REQUEST_TYPE_GENERAL)
   }
 
@@ -1158,15 +1169,15 @@ const FOIRequest = React.memo(({ userDetail, openApplicantProfileModal }) => {
             </div>
             {!isAddRequest && (
               <>
-                {showCFRTab() && (
+                {showFeesTab() && (
                   <div
                     className={clsx("tablinks", {
-                      active: tabLinksStatuses.CFRForm.active,
+                      active: tabLinksStatuses.Fees.active,
                     })}
-                    name="CFRForm"
-                    onClick={() => tabclick("CFRForm")}
+                    name="Fees"
+                    onClick={() => tabclick("Fees")}
                   >
-                    CFR Form
+                    Fees
                     {CFRFormHistoryLength > 0
                       ? ` (${CFRFormHistoryLength})`
                       : ""}
@@ -1528,22 +1539,24 @@ const FOIRequest = React.memo(({ userDetail, openApplicantProfileModal }) => {
               <Loading />
             )}
           </div>
-          {showCFRTab() && (
+          {showFeesTab() && (
             <div
-              id="CFRForm"
+              id="Fees"
               className={clsx("tabcontent", {
-                active: tabLinksStatuses.CFRForm.active,
-                [classes.displayed]: tabLinksStatuses.CFRForm?.display,
-                [classes.hidden]: !tabLinksStatuses.CFRForm?.display,
+                active: tabLinksStatuses.Fees.active,
+                [classes.displayed]: tabLinksStatuses.Fees?.display,
+                [classes.hidden]: !tabLinksStatuses.Fees?.display,
               })}
             >
-              <CFRForm
+              <Fees
                 requestNumber={requestNumber}
                 requestState={requestState}
+                requestDetails={requestDetails}
                 userDetail={userDetail}
                 ministryId={ministryId}
                 requestId={requestId}
                 setCFRUnsaved={setCFRUnsaved}
+                handleStateChange={handleStateChange}
               />
             </div>
           )}
