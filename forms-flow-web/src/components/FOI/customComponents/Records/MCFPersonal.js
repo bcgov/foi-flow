5ea9290fb3b085f08326f440da4503567d9ac501
Milos Despotovic <milosmdes@gmail.com>
2024-12-24 10:46:20 -0800

    Remove accidental useRef import

--- forms-flow-web/src/components/FOI/customComponents/Records/MCFPersonal.js
+++ forms-flow-web/src/components/FOI/customComponents/Records/MCFPersonal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import {
   ClickableChip  
