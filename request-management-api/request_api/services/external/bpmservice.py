dc108b6217b8a7f7753898baad61a56ec81ef1f6
Jocelyn Cabildo <Jocelyn.Cabildo@gov.bc.ca>
2025-01-08 14:00:39 -0800

    Added Print

--- request-management-api/request_api/services/external/bpmservice.py
+++ request-management-api/request_api/services/external/bpmservice.py
@@ -173,12 +173,9 @@ class bpmservice(camundaservice):
         return auth_response.json().get('access_token')
     
     def _getHeaders_(self, token):
-        print("Bpmtoken", token)
         """Generate headers."""
         if token is None:
-            print("Bpmtoken1", token)
             token = self._getserviceaccounttoken_()
-            print("Bpmtoken2", token)
         return {
             "Authorization": "Bearer " + token,
             "Content-Type": "application/json",
