1b0b07570bc7fe1406aee4df1d5c345712fbe7e5
Jocelyn Cabildo <Jocelyn.Cabildo@gov.bc.ca>
2025-01-13 16:46:33 -0800

    Revert to original

--- request-management-api/request_api/services/emailservice.py
+++ request-management-api/request_api/services/emailservice.py
@@ -35,7 +35,7 @@ class emailservice:
             _messageattachmentlist = self.__get_attachments(ministryrequestid, emailschema, servicename)
             self.__pre_send_correspondence_audit(requestid, ministryrequestid,emailschema, content, templateconfig().isnotreceipt(servicename), _messageattachmentlist)
             subject = templateconfig().getsubject(servicename, requestjson)
-            return senderservice().send(subject, _messagepart, _messageattachmentlist, requestjson.get("email"))
+            return senderservice().send(subject, _messagepart, _messageattachmentlist, requestjson)
         except Exception as ex:
             logging.exception(ex)
 
