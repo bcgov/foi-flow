4b331c9bc3c8c27eebebf857df6a6d9826b3d0c5
Richard Qi <richard.qi@aot-technologies.com>
2025-02-27 12:56:59 -0800

    remove doc editor server (already intergrated into other api)

--- documenteditor-server/gulpfile.js
+++
@@ -1,8 +0,0 @@
-require("@syncfusion/ej2-staging");
-
-const gulp = require('gulp');
-const shelljs = require('shelljs');
-
-gulp.task('copy-webconfig', function() {
-    shelljs.cp('-r', './web.config', './src/deploy')
-});
