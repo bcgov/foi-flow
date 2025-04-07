371414efe64e07279e8996559785347af1685045
unknown <nicholas.kan@aot-technologies.com>
2025-04-07 10:21:49 -0700

    fix advanced search columns for iao

--- forms-flow-web/src/components/FOI/Dashboard/IAO/AdvancedSearch/DataGridAdvancedSearch.js
+++ forms-flow-web/src/components/FOI/Dashboard/IAO/AdvancedSearch/DataGridAdvancedSearch.js
@@ -450,7 +450,7 @@ const DataGridAdvancedSearch = ({ userDetail }) => {
   
 
   const getTableInfo = (userGroups) => {
-    let tableInfo = defaultTableInfo;
+    var tableInfo = defaultTableInfo;
     if (!userGroups || isIntakeTeam(userGroups)) {
       tableInfo = defaultTableInfo;
     }
