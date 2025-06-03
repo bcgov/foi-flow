53424637813fc7d81eac3e0bb777724a7731f9bc
Aman-Hundal <aman.hundal@gov.bc.ca>
2025-06-03 13:19:47 -0700

    small adj to migration script

--- request-management-api/migrations/versions/bf7e7298d642_.py
+++ request-management-api/migrations/versions/bf7e7298d642_.py
@@ -7,7 +7,6 @@ Create Date: 2024-08-21 11:59:07.999420
 """
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'bf7e7298d642'
