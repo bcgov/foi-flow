e7ef1c4c7df5d35d6f2bacc6a232cea20217c811
Milos Despotovic <milos.despotovic@gov.bc.ca>
2025-06-11 09:58:24 -0700

    Fix download draft bug

--- forms-flow-web/src/components/FOI/customComponents/ContactApplicant/CommunicationStructure.jsx
+++ forms-flow-web/src/components/FOI/customComponents/ContactApplicant/CommunicationStructure.jsx
@@ -296,13 +296,13 @@ const CommunicationStructure = ({
         if (variable.name == "{{secondaryAddress}}") secondaryAddress = variable.value
         if (variable.name == "{{postal}}") postal = variable.value
       })
-      let addressStartIndex = element.indexOf(address)
+      let addressStartIndex = element.indexOf(address) // -1 if undefined
       let secondaryAddressStartIndex = element.indexOf(secondaryAddress)
       let postalStartIndex = element.indexOf(postal)
       let endIndex;
-      if (postalStartIndex != -1) endIndex = postalStartIndex + postal.length // if postal code exists, it is end of address string
-      if (secondaryAddressStartIndex != -1) endIndex = secondaryAddressStartIndex + secondaryAddress.length // if secondary address
-      if (postalStartIndex == -1 && secondaryAddressStartIndex == -1) endIndex = addressStartIndex + address.length // if only primary
+      if (postalStartIndex != -1 && postal) endIndex = postalStartIndex + postal.length // if postal code exists, it is end of address string
+      if (secondaryAddressStartIndex != -1 && secondaryAddress) endIndex = secondaryAddressStartIndex + secondaryAddress.length // if secondary address
+      if (postalStartIndex == -1 && secondaryAddressStartIndex == -1 && addressStartIndex != -1 && address) endIndex = addressStartIndex + address.length // if only primary
       // slice must be done first so indexes don't change
       if (addressStartIndex && addressStartIndex != -1 && endIndex && endIndex != -1) {
         element = element.slice(0, addressStartIndex) + 'ADDRESS REDACTED' + element.slice(endIndex)
@@ -323,7 +323,7 @@ const CommunicationStructure = ({
     if (correspondence?.emailsubject) return correspondence.emailsubject
     if (!correspondence?.sentby && correspondence?.templatename) return templateList.find((obj)=> obj.fileName == correspondence.templatename)?.templateName
     if (correspondence.category === "response") return "Applicant Response"
-    return `Your FOI Request [${requestNumber}]`
+    return `Your FOI Request ${requestNumber}`
     // if(correspondence.templatename) {
     //   return correspondence.templatename;
     // } else {
