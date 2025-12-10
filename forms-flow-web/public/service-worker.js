967c587ad27b022bff6c6a78e21875eaa62606ac
Hundal <aman.hundal@gov.bc.ca>
2025-12-10 12:41:00 -0800

    adjusted serviceworker usage in app to fix the blank page bug in EDGE browser

--- forms-flow-web/public/service-worker.js
+++ forms-flow-web/public/service-worker.js
@@ -1,57 +1,69 @@
-const cacheName = "FORMS-FLOW-AI-WEB-CACHE-V2";
+// const cacheName = "FORMS-FLOW-AI-WEB-CACHE-V2";
 
-const urlsToCache = [
-  "/manifest.json",
-  "/favicon.ico",
-  "/spinner.gif",
-  "/logo.svg"
-];
+// const urlsToCache = [
+//   "/manifest.json",
+//   "/favicon.ico",
+//   "/spinner.gif",
+//   "/logo.svg"
+// ];
 
-// Install a service worker
-self.addEventListener('install', event => {
- // Perform install steps
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+// // Install a service worker
+// self.addEventListener('install', event => {
+//  // Perform install steps
+//   event.waitUntil(
+//     caches.open(cacheName)
+//       .then(cache => cache.addAll(urlsToCache))
+//   );
+// });
+
+// self.addEventListener('fetch', (event)=>{
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then((response)=>{
+//       // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       })
+//   );
+// });
+
+// //Delete outdate cache
+// self.addEventListener("activate", event => {
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//          cacheNames.filter((name)=>{
+//           // Return true if you want to remove this cache,
+//           // but remember that caches are shared across
+//           // the whole origin
+//           return cacheName!==name
+
+//         }).map(name => {
+//             return caches.delete(name);
+//           })
+//       );
+//     })
+//   );
+// });
+
+// self.addEventListener('message', (event)=>{
+//   if(event.origin === self.location.origin){
+//     if (event.data.action === 'skipWaiting') {
+//       self.skipWaiting();
+//     }
+//   }
+// });
+
+// --- No-op service worker (disables all caching and behavior safely) --- Bug Fix to prevent issue where users would encounter random blank pages in EDGE (NGINX.conf adjust as well)
+
+self.addEventListener("install", () => {
+  // Ensure this SW activates immediately
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event)=>{
-  event.respondWith(
-    caches.match(event.request)
-      .then((response)=>{
-      // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-//Delete outdate cache
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-         cacheNames.filter((name)=>{
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-          return cacheName!==name
-
-        }).map(name => {
-            return caches.delete(name);
-          })
-      );
-    })
-  );
-});
-
-self.addEventListener('message', (event)=>{
-  if(event.origin === self.location.origin){
-    if (event.data.action === 'skipWaiting') {
-      self.skipWaiting();
-    }
-  }
+self.addEventListener("activate", () => {
+  // Ensure pages immediately use this (non-caching) SW
+  clients.claim();
 });
