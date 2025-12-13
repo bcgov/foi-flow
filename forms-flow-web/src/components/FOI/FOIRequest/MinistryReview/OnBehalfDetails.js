a60bdb53d912d65d3e324d18d4ee2bf71fdc03fc
Marcos Alves Cunha <alvesfc@users.noreply.github.com>
2025-12-12 16:30:23 -0800

    Dev into main (#5951)
    * Release 21.7 (#5949)
    
    * FOIMOD-3814-Alsoknownas field added & other related updates
    
    * FOIMOD-3817-Subject Code - Clear Field
    
    * FOIMOD-3814-Delivery modes update
    
    * Fix in ministry view
    
    ---------
    
    Co-authored-by: Aparna <aparna.s@aot-technologies.com>
    
    * chore: remove Alembic migration execution from entrypoint
    
    The entrypoint was running  automatically, which caused
    failures due to conflicting migration heads and cyclic revision history.
    Migration is now disabled at startup and must be triggered manually
    after revisions have been cleaned up.
    
    * feat(db): merge Alembic heads and update migration filenames
    
    Added merge migration 8f16afbd3bae_merge_heads_3a4f7b2c6d9e_and_.py to resolve diverging Alembic heads (3a4f7b2c6d9e and cfe019598f8b).
    
    Renamed migration file 4ad3724b593b_.py → 35ac2fc5754e_delivery_mode_update.py for clarity and consistency.
    
    Updated entrypoint.sh to align with the latest migration workflow.
    
    This commit consolidates the migration history into a single linear chain and ensures clearer naming and execution of database revisions.
    
    * Fixes for Release 21.7 - Remove AXIS Dependencies
    
    Fixes for tickets:
    FOIMOD-3808
    FOIMOD-3814
    FOIMOD-3817
    
    * styling fix
    
    ---------
    
    Co-authored-by: Aparna <aparna.s@aot-technologies.com>
    Co-authored-by: Aparna <71481254+aparna-aot@users.noreply.github.com>

--- forms-flow-web/src/components/FOI/FOIRequest/MinistryReview/OnBehalfDetails.js
+++ forms-flow-web/src/components/FOI/FOIRequest/MinistryReview/OnBehalfDetails.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles(() => ({
+  rowMargin: {
+    marginBottom: "1em",
+  },
+}));
 
 const OnBehalfDetails = React.memo((requestDetails) => {
+  const classes = useStyles();
+
   const _requestDetails = requestDetails.requestDetails;
   const requestType = _requestDetails.requestType
     ? _requestDetails.requestType.replace(/^./, (str) => str.toUpperCase())
@@ -12,6 +23,8 @@ const OnBehalfDetails = React.memo((requestDetails) => {
     firstName: _requestDetails?.additionalPersonalInfo?.anotherFirstName,
     middleName: _requestDetails?.additionalPersonalInfo?.anotherMiddleName,
     lastName: _requestDetails?.additionalPersonalInfo?.anotherLastName,
+    alsoKnownAs: _requestDetails?.additionalPersonalInfo?.anotherAlsoKnownAs,
+    DOB: _requestDetails?.additionalPersonalInfo?.anotherBirthDate
   };
 
   const onBehalfDetailsEntered = Object.values(onBehalfDetails).some(
@@ -26,29 +39,47 @@ const OnBehalfDetails = React.memo((requestDetails) => {
     <Card id="onBehalfDetailsMinistry" className="foi-details-card">
       <label className="foi-details-label">On Behalf Details</label>
       <CardContent>
+        <div className={clsx("row", "foi-details-row", classes.rowMargin)}>
+            <div className="col-lg-3 foi-details-col">
+              <div>
+                <b>First Name</b>
+              </div>
+              <div>
+                <span>{onBehalfDetails.firstName}</span>
+              </div>
+            </div>
+            <div className="col-lg-3 foi-details-col">
+              <div>
+                <b>Middle Name</b>
+              </div>
+              <div>
+                <span>{onBehalfDetails.middleName}</span>
+              </div>
+            </div>
+            <div className="col-lg-3 foi-details-col">
+              <div>
+                <b>Last Name</b>
+              </div>
+              <div>
+                <span>{onBehalfDetails.lastName}</span>
+              </div>
+            </div>
+        </div>
         <div className="row foi-details-row">
           <div className="col-lg-3 foi-details-col">
             <div>
-              <b>First Name</b>
+              <b>Date of Birth</b>
             </div>
             <div>
-              <span>{onBehalfDetails.firstName}</span>
-            </div>
-          </div>
-          <div className="col-lg-3 foi-details-col">
-            <div>
-              <b>Middle Name</b>
-            </div>
-            <div>
-              <span>{onBehalfDetails.middleName}</span>
+              <span>{onBehalfDetails.DOB}</span>
             </div>
           </div>
           <div className="col-lg-3 foi-details-col">
             <div>
-              <b>Last Name</b>
+              <b>Also Known As</b>
             </div>
             <div>
-              <span>{onBehalfDetails.lastName}</span>
+              <span>{onBehalfDetails.alsoKnownAs}</span>
             </div>
           </div>
         </div>
