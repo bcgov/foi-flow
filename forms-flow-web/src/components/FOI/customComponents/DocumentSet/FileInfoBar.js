044294b3fbec495238212383eb50409185d34284
Marcos Alves Cunha <alvesfc@users.noreply.github.com>
2026-01-26 09:43:27 -0800

    FOIMOD-4583 : Fixing CSS and general observation (#6011)

--- forms-flow-web/src/components/FOI/customComponents/DocumentSet/FileInfoBar.js
+++ forms-flow-web/src/components/FOI/customComponents/DocumentSet/FileInfoBar.js
@@ -2,13 +2,9 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import FolderIcon from "@material-ui/icons/Folder";
+import {formatBytes} from "../Records/util";
 
 export default function FileInfoBar({ size }) {
-  // If size is a number (bytes), convert to MB
-  const formattedSize =
-    typeof size === "number"
-      ? `${(size / 1024).toFixed(2)} KB`
-      : size; // assume it's already formatted
 
   return (
     <Grid
@@ -28,7 +24,7 @@ export default function FileInfoBar({ size }) {
           </Grid>
           <Grid item>
             <Typography variant="body2">
-              <strong>Files size:</strong> {formattedSize}
+              <strong>Total size:</strong> {formatBytes(size ?? 0)}
             </Typography>
           </Grid>
         </Grid>
