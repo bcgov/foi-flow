ce43b73fd97c26c424d07aca4c865f3c720c1eb7
Milos Despotovic <milos.despotovic@gov.bc.ca>
2026-02-06 13:01:15 -0800

    Remove alsoKnownAs field from ApplicantDetails

--- forms-flow-web/src/components/FOI/FOIRequest/ApplicantDetails.js
+++ forms-flow-web/src/components/FOI/FOIRequest/ApplicantDetails.js
@@ -120,9 +120,7 @@ const ApplicantDetails = React.memo(
         defaultValue: "Select Category",
       })
     );
-    const [alsoKnownAsText, setAlsoKnownAs] = React.useState(
-      validateFields(requestDetails?.additionalPersonalInfo, FOI_COMPONENT_CONSTANTS.ALSO_KNOWN_AS)
-    );
+
     const [otherNotes, setOtherNotes] = React.useState(
       validateFields(requestDetails, FOI_COMPONENT_CONSTANTS.OTHER_NOTES)
     );
@@ -242,14 +240,6 @@ const ApplicantDetails = React.memo(
       );
       createSaveRequestObject(
         FOI_COMPONENT_CONSTANTS.FOI_CATEGORY,
-        e.target.value
-      );
-    };
-
-    const handleAlsoKnownAsChange = (e) => {
-      setAlsoKnownAs(e.target.value);
-      createSaveRequestObject(
-        FOI_COMPONENT_CONSTANTS.ALSO_KNOWN_AS,
         e.target.value
       );
     };
@@ -405,24 +395,12 @@ const ApplicantDetails = React.memo(
               >
                 {menuItems}
               </TextField>
-              {showHistory && <TextField
-                id="alsoKnownAs"
-                label="Also Known As"
-                inputProps={{ "aria-labelledby": "alsoKnownAs-label"}}
-                InputLabelProps={{ shrink: true }}
-                value={alsoKnownAsText}
-                variant="outlined"
-                className={warning && warning(FOI_COMPONENT_CONSTANTS.ALSO_KNOWN_AS) && classes.warning}
-                fullWidth
-                disabled={disableInput}
-                onChange={handleAlsoKnownAsChange}
-              />}
             </div>
             <div className="col-lg-12 foi-details-col">
               {displayOtherNotes && <TextField
                 id="otherNotes"
                 label="Other Applicant Notes"
-                inputProps={{ "aria-labelledby": "alsoKnownAs-label"}}
+                inputProps={{ "aria-labelledby": "otherNotes-label"}}
                 InputLabelProps={{ shrink: true }}
                 value={otherNotes}
                 variant="outlined"
