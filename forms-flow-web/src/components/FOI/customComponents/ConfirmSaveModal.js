c201a0fb2a06331a8d605031e7d318c18f92478b
Aparna <aparna.s@aot-technologies.com>
2026-02-26 10:03:10 -0800

    Removed commented out code

--- forms-flow-web/src/components/FOI/customComponents/ConfirmSaveModal.js
+++ forms-flow-web/src/components/FOI/customComponents/ConfirmSaveModal.js
@@ -1,89 +1,3 @@
-// import React, { useEffect, useState }  from 'react';
-// import Dialog from '@material-ui/core/Dialog';
-// import DialogActions from '@material-ui/core/DialogActions';
-// import DialogContent from '@material-ui/core/DialogContent';
-// import DialogContentText from '@material-ui/core/DialogContentText';
-// import DialogTitle from '@material-ui/core/DialogTitle';
-// import CloseIcon from '@material-ui/icons/Close';
-// import IconButton from '@material-ui/core/IconButton';
-// import TextField from '@mui/material/TextField';
-
-// const ConfirmSaveModal= ({
-//     modalMessage,
-//     modalDescription,
-//     showModal,
-//     saveAssigneeDetails,
-//     assigneeVal,
-//     assigneeName,
-//     resetModal
-// }) =>{
-
-//     const [modalOpen, setModalOpen] = useState(showModal);
-
-//     const handleSave = () => {
-//         setModalOpen(false);
-//         saveAssigneeDetails(assigneeVal,assigneeName);
-//         resetModal();
-//     };
-
-//     const handleClose = () => {
-//         setModalOpen(false);
-//         resetModal();
-//     };
-
-//     useEffect(() => {
-//         setModalOpen(showModal);
-//       }, [showModal]);
-
-//     return (
-//         <div className="state-change-dialog">
-//             <Dialog
-//             open={modalOpen}
-//             onClose={handleClose}
-//             aria-labelledby="state-change-dialog-title"
-//             aria-describedby="restricted-modal-text"
-//             maxWidth={'md'}
-//             fullWidth={true}
-//             >
-//             <DialogTitle disableTypography id="state-change-dialog-title">
-//                 <h2 className="state-change-header">Confirm and Proceed</h2>
-//                 <h6 className="state-change-header">You have selected {26} ministries and public bodies.</h6>
-//                 <IconButton className="title-col3" onClick={handleClose}>
-//                     <i className="dialog-close-button">Close</i>
-//                     <CloseIcon />
-//                 </IconButton>
-//                 </DialogTitle>
-//             <DialogContent>
-//                 <DialogContentText id="restricted-modal-text" component={'span'}>
-//                 <div className="modal-msg">
-//                     <div className="confirmation-message">
-//                         {modalMessage}
-//                     </div>
-//                     <div className='modal-msg-description'>
-//                         <i>{modalDescription}</i>
-//                     </div>
-//                 </div>
-//             </DialogContentText>
-//             </DialogContent>
-//             <DialogActions>
-//                 <button
-//                 className={`btn-bottom btn-save btn`}
-//                 onClick={handleSave}
-//                 //disabled={!isIAORestrictedFileManager || isRequestAssignedToTeam()}
-//                 >
-//                 Proceed
-//                 </button>
-//                 <button className="btn-bottom btn-cancel" onClick={handleClose}>
-//                 Cancel
-//                 </button>
-//             </DialogActions>
-//             </Dialog>
-//         </div>
-//     );
-// };
-
-// export default ConfirmSaveModal;
-
 import React, { useEffect, useState } from "react";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -105,7 +19,6 @@ const useStyles = makeStyles((theme) => ({
   },
   dialogTitle: {
     padding: "24px 24px 16px",
-    //borderBottom: '1px solid #e0e0e0',
   },
   titleHeader: {
     display: "flex",
@@ -134,12 +47,10 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "-8px",
   },
   dialogContent: {
-    //padding: "24px",
     color: "#2D2D2D",
   },
   selectAllContainer: {
     marginBottom: "16px",
-    //borderBottom: '1px solid #e0e0e0',
     paddingBottom: "12px",
     color: "#2D2D2D",
   },
@@ -157,8 +68,6 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   warningBox: {
-    // display: "flex",
-    // alignItems: "flex-start",
     backgroundColor: "#FEF1D8",
     border: "1px solid #F8BB47",
     borderRadius: "4px",
