5c910bdeaeeeb109809287907b4cdb0991ac4e8e
Hundal <aman.hundal@gov.bc.ca>
2026-03-07 13:14:32 -0800

    delink process completed. WIP testing and bulk add process

--- request-management-api/request_api/services/linkedrequestservice.py
+++ request-management-api/request_api/services/linkedrequestservice.py
@@ -36,7 +36,6 @@ class linkedrequestservice:
             foirawrequest_ids = [linkedrequest["rawrequestid"] for linkedrequest in linkedrequests if "rawrequestid" in linkedrequest and linkedrequest["foiministryrequestid"] is None] #axis ids from linked requests where indicaotr is FALSE
             FOIRawRequest().bulkupdate_linkedrequests(foirawrequest_ids, new_linkedrequest, userid)
             FOIMinistryRequest().bulkupdate_linkedrequests(foiministryrequest_ids, new_linkedrequest, userid)
-
             db.session.commit()
         except Exception as e:
             db.session.rollback()
@@ -48,36 +47,45 @@ class linkedrequestservice:
         try:
             foiministryrequestid = linkedrequest_a["foiministryrequestid"]
             rawrequestid = linkedrequest_a["rawrequestid"]
-            if foiministryrequestid is not None:
-                removal_result = FOIMinistryRequest().update_linkedrequests(foiministryrequestid, json.dumps(updated_linkedrequests), user)
+            current_linkedrequests = self.__get_current_linkedrequests(rawrequestid, foiministryrequestid)
+            if self.__valid_udpate(current_linkedrequests, updated_linkedrequests):
+                if foiministryrequestid is not None:
+                    FOIMinistryRequest().update_linkedrequests(foiministryrequestid, json.dumps(updated_linkedrequests), user)
+                else:
+                    FOIRawRequest().update_linkedrequests(rawrequestid, json.dumps(updated_linkedrequests), user)
+                # Delink linkedrequest_a from linkedrequest_b
+                self.__remove_two_way_link(linkedrequest_a, linkedrequest_b, user)
+                db.session.commit()
             else:
-                removal_result = FOIRawRequest().update_linkedrequests(rawrequestid, json.dumps(updated_linkedrequests), user)
-            delink_result = self.__remove_two_way_link(linkedrequest_a, linkedrequest_b, user)
-            db.session.commit()
+                db.session.close()
             return DefaultMethodResult(True,'Linkedrequest data updated', linkedrequest_a["axisrequestid"], 201)
         except Exception as e:
             db.session.rollback()
             raise Exception("Error when delinking removed linkedrequests", e) from e
 
     def __remove_two_way_link(self, linkedrequest_a, linkedrequest_b, user):
-        # Delink linkedrequest_a from linkedrequest_b
-        print("parent", linkedrequest_a)
-        print("child", linkedrequest_b)
-        if "foiministryrequestid" in linkedrequest_b and linkedrequest_b["foiministryrequestid"] is not None:
-            print("min two way")
-            current_linkedrequests = FOIMinistryRequest().get_linkedrequests(linkedrequest_b["foiministryrequestid"])
+        foiministryrequestid = linkedrequest_b["foiministryrequestid"]
+        rawrequestid = linkedrequest_b["rawrequestid"]
+        if foiministryrequestid is not None:
+            current_linkedrequests = self.__get_current_linkedrequests(None, foiministryrequestid)
             updated_linkedrequests = self.__update_linkedrequest_list(linkedrequest_a, current_linkedrequests)
-            return FOIMinistryRequest().update_linkedrequests(linkedrequest_b["foiministryrequestid"], json.dumps(updated_linkedrequests), user)
-        if "rawrequestid" in linkedrequest_b and linkedrequest_b["rawrequestid"] is not None:
-            print("raw two way")
-            current_linkedrequests = FOIRawRequest().get_linkedrequests(linkedrequest_b["rawrequestid"])
+            if self.__valid_udpate(current_linkedrequests, updated_linkedrequests):
+                return FOIMinistryRequest().update_linkedrequests(foiministryrequestid, json.dumps(updated_linkedrequests), user)
+        else:
+            current_linkedrequests = self.__get_current_linkedrequests(rawrequestid, None)
             updated_linkedrequests = self.__update_linkedrequest_list(linkedrequest_a, current_linkedrequests)
-            return FOIRawRequest().update_linkedrequests(linkedrequest_b["rawrequestid"], json.dumps(updated_linkedrequests), user)
+            if self.__valid_udpate(current_linkedrequests, updated_linkedrequests):
+                return FOIRawRequest().update_linkedrequests(rawrequestid, json.dumps(updated_linkedrequests), user)
 
     def __update_linkedrequest_list(self, linkedrequest_toremove, linkedrequests):
         # Remove linkedrequest_b from linkedrequest_a's array of linkedrequests if linkedrequest_b's axisrequestid exists as a key in the array
         linkedrequest_toremove_axisid = linkedrequest_toremove["axisrequestid"]
-        if len(linkedrequests) > 0:
-            print("REMOVE")
-            return [linkedrequest for linkedrequest in linkedrequests if linkedrequest_toremove_axisid not in linkedrequest]
-        return linkedrequests
+        return [linkedrequest for linkedrequest in linkedrequests if linkedrequest_toremove_axisid not in linkedrequest]
+    
+    def __valid_udpate(self, current_linkedrequests, updated_linkedrequests):
+        return (current_linkedrequests is not None and len(current_linkedrequests) > 0) and (len(current_linkedrequests) != len(updated_linkedrequests))
+    
+    def __get_current_linkedrequests(self, requestid, foiministryrequestid):
+        if foiministryrequestid is not None:
+            return FOIMinistryRequest().get_linkedrequests(foiministryrequestid)
+        return FOIRawRequest().get_linkedrequests(requestid)
