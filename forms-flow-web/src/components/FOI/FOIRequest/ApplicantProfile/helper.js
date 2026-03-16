c5d7586a5e40d7e4a721b2b508c837afbe5233d9
Hundal <aman.hundal@gov.bc.ca>
2026-03-16 15:22:52 -0700

    validation logic fix + linkedrequest fix

--- forms-flow-web/src/components/FOI/FOIRequest/ApplicantProfile/helper.js
+++ forms-flow-web/src/components/FOI/FOIRequest/ApplicantProfile/helper.js
@@ -29,21 +29,21 @@ export const validateApplicantProfileFields = (applicantProfile) => {
     (applicantProfile?.lastName !== "" && applicantProfile?.lastName !== null && applicantProfile?.lastName?.length <= 50) &&
     (applicantProfile?.category !== "" && applicantProfile?.category !== null);
   const charFieldLengthInvalid = 
-    applicantProfile?.address?.length > 120 &&
-    applicantProfile?.city?.length > 120 &&
-    applicantProfile?.province?.length > 120 &&
-    applicantProfile?.country?.length > 120 &&
-    applicantProfile?.postal?.length > 6 &&
-    applicantProfile?.middleName?.length > 50 &&
-    applicantProfile?.businessName?.length > 255 &&
-    applicantProfile?.addressSecondary?.length > 120 &&
-    applicantProfile?.phonePrimary?.length > 50 &&
-    applicantProfile?.phoneSecondary?.length > 50 &&
-    applicantProfile?.workPhonePrimary?.length > 50 &&
-    applicantProfile?.workPhoneSecondary?.length > 50 &&
-    applicantProfile?.additionalPersonalInfoValid?.alsoKnownAs?.length > 250 &&
-    applicantProfile?.additionalPersonalInfoValid?.personalHealthNumber?.length > 50 &&
-    applicantProfile?.publicServiceEmployeeNumber?.length > 50 &&
+    applicantProfile?.address?.length > 120 ||
+    applicantProfile?.city?.length > 120 ||
+    applicantProfile?.province?.length > 120 ||
+    applicantProfile?.country?.length > 120 ||
+    applicantProfile?.postal?.length > 6 ||
+    applicantProfile?.middleName?.length > 50 ||
+    applicantProfile?.businessName?.length > 255 ||
+    applicantProfile?.addressSecondary?.length > 120 ||
+    applicantProfile?.phonePrimary?.length > 50 ||
+    applicantProfile?.phoneSecondary?.length > 50 ||
+    applicantProfile?.workPhonePrimary?.length > 50 ||
+    applicantProfile?.workPhoneSecondary?.length > 50 ||
+    applicantProfile?.additionalPersonalInfoValid?.alsoKnownAs?.length > 250 ||
+    applicantProfile?.additionalPersonalInfoValid?.personalHealthNumber?.length > 50 ||
+    applicantProfile?.publicServiceEmployeeNumber?.length > 50 ||
     applicantProfile?.correctionalServiceNumber?.length > 50;
 
   let contactInvalid = false;
